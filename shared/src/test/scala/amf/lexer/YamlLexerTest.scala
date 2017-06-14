fedab0c6c78a71171d7a32d3662ce4b3be2dafc9
Hernan Najles <hernan.najles@mulesoft.com>
2017-06-14 14:01:54 -0300

    add yamllexer tests

--- shared/src/test/scala/amf/lexer/YamlLexerTest.scala
+++ shared/src/test/scala/amf/lexer/YamlLexerTest.scala
@@ -2,7 +2,7 @@ package amf.lexer
 
 import amf.common.ListAssertions
 import amf.yaml.YamlToken._
-import amf.yaml.{YamlLexer, YamlToken}
+import amf.yaml.YamlLexer
 import org.scalatest.FunSuite
 
 /**
@@ -11,7 +11,7 @@ import org.scalatest.FunSuite
 
 class YamlLexerTest extends FunSuite with ListAssertions{
 
-  test("Simple key value parse test"){
+  test("Simple mapping parse test"){
     val input= "a: 1"
     val actual: List[(Token,String)] = YamlLexer(input).lex()
     println(actual.toString)
@@ -21,12 +21,193 @@ class YamlLexerTest extends FunSuite with ListAssertions{
     assert(actual,expected)
   }
 
-  test("Simple key value with white space parse test"){
+  test("Simple single quoted mapping parse test"){
+    val input= "'a': 1"
+    val actual: List[(Token,String)] = YamlLexer(input).lex()
+    println(actual.toString)
+    val expected = List((StartMap,": "), (StringToken,"'a'"),
+      (StringToken,"1"),(EndMap,""),(Eof,""))
+
+    assert(actual,expected)
+  }
+
+  test("Simple doble quoted mapping parse test"){
+    val input= "\"a\": 1"
+    val actual: List[(Token,String)] = YamlLexer(input).lex()
+    println(actual.toString)
+    val expected = List((StartMap,": "), (StringToken,"\"a\""),
+      (StringToken,"1"),(EndMap,""),(Eof,""))
+
+    assert(actual,expected)
+  }
+
+  test("Simple mapping with white space parses test"){
     val input= "a : 1"
     val actual: List[(Token,String)] = YamlLexer(input).lex()
     println(actual.toString)
     val expected = List((StartMap,": "), (StringToken,"a "),
       (StringToken,"1"),(EndMap,""),(Eof,""))
+    assert(actual,expected)
+  }
+
+  test("Simple doble quoted mapping with white space parses test"){
+    val input= "\"a\" : 1"
+    val actual: List[(Token,String)] = YamlLexer(input).lex()
+    println(actual.toString)
+    val expected = List((StartMap,": "), (StringToken,"\"a\" "),
+      (StringToken,"1"),(EndMap,""),(Eof,""))
+    assert(actual,expected)
+  }
+  //TODO: with { tokens, the lexer not respond
+  ignore("Simple flow mapping with white space parses test"){
+    val input= "a : {1,2}"
+    val actual: List[(Token,String)] = YamlLexer(input).lex()
+    println(actual.toString)
+    val expected = List((StartMap,": "), (StringToken,"a "),
+        (StartMap,"{"),(StringToken,"1"),(Comma,","),(StringToken,"2"),
+        (EndMap,"}"),(EndMap,""),(Eof,""))
+    assert(actual,expected)
+  }
+
+  ignore("flow sequence parse test"){
+    val input= "a: [1,2,3]"
+    val actual: List[(Token,String)] = YamlLexer(input).lex()
+    println(actual.toString)
+    val expected = List((StartMap,": "), (StringToken,"a"),
+      (StartSequence,"["),
+        (StringToken,"1"),(Comma,","),(StringToken,"2"),(Comma,","),(StringToken,"3"),
+      (EndSequence,"]"),
+      (EndMap,""),(Eof,""))
+
+    assert(actual,expected)
+  }
+
+  test("Two items mapping"){
+    val input= "a : 1 \nb: 2"
+    val actual: List[(Token,String)] = YamlLexer(input).lex()
+    println(actual.toString)
+    val expected = List((StartMap,": "),
+      (StringToken,"a "), (StringToken,"1 "),(WhiteSpace,"\\n"),
+      (Comma,": "),
+      (StringToken,"b"),(StringToken,"2"),(EndMap,""),(Eof,""))
+
+    assert(actual,expected)
+  }
+
+  test("Two items sequence"){
+    val input= "- a\n- b"
+    val actual: List[(Token,String)] = YamlLexer(input).lex()
+    println(actual.toString)
+    val expected = List((StartSequence,"- "),
+      (StringToken,"a"),(WhiteSpace,"\\n"),
+      (Comma,"- "), (StringToken,"b"),(EndSequence,""),(Eof,""))
+
+    assert(actual,expected)
+  }
+
+  ignore("sequence of mappings"){
+    val input= "- a: 1\n- b: 2"
+    val actual: List[(Token,String)] = YamlLexer(input).lex()
+    println(actual.toString)
+    val expected = List((StartSequence,"- "),
+        (StartMap,": "),(StringToken,"a"),(StringToken,1),(WhiteSpace,"\\n"),(EndMap,""),
+        (Comma,"- "),(StartMap,": "), (StringToken,"b"),(StringToken,"2"),(EndMap,""),
+      (EndSequence,""),(Eof,""))
+
+    assert(actual,expected)
+  }
+
+  test("mapping of sequences"){
+    val input=
+      """a:
+        | - 1
+        | - 2
+        |b:
+        | - 3""".stripMargin
+    val actual: List[(Token,String)] = YamlLexer(input).lex()
+    println(actual.toString)
+    val expected = List((StartMap,":\\n"),(StringToken,"a"),(WhiteSpace," "),
+        (StartSequence,"- "),(StringToken,"1"),(WhiteSpace,"\\n"),(WhiteSpace," "),
+        (Comma,"- "),(StringToken,"2"),(WhiteSpace,"\\n"),(EndSequence,":\\n"),
+      (Comma,""),(StringToken,"b"),(WhiteSpace," "),
+        (StartSequence,"- "),(StringToken,"3"),(EndSequence,""),
+      (EndMap,""),(Eof,""))
+
+    assert(actual,expected)
+  }
+
+  ignore("sequence of  sequences"){
+    val input=
+      """-
+        | - 1
+        | - 2
+        |-
+        | - 3""".stripMargin
+    val actual: List[(Token,String)] = YamlLexer(input).lex()
+    println(actual.toString)
+    val expected = List((StartSequence,"-"),(WhiteSpace,"\\n "),
+      (StartSequence,"- "),(StringToken,"1"),(WhiteSpace,"\\n"),(WhiteSpace," "),
+      (Comma,"- "),(StringToken,"2"),(WhiteSpace,"\\n"),(EndSequence,"\\n"),
+      (Comma,"-"),(WhiteSpace,"\\n "),
+      (StartSequence,"- "),(StringToken,"3"),(EndSequence,""),
+      (EndSequence,""),(Eof,""))
+
+    assert(actual,expected)
+  }
+
+  test("mapping of mappings"){
+    val input=
+      """q:
+        | z: 1
+        | x: 2
+        |w:
+        | c: 3""".stripMargin
+    val actual: List[(Token,String)] = YamlLexer(input).lex()
+    println(actual.toString)
+    val expected = List(
+      (StartMap,":\\n"),(StringToken,"q"),(WhiteSpace," "),
+        (StartMap,": "),(StringToken,"z"),(StringToken,"1"),(WhiteSpace,"\\n"),(WhiteSpace," "),
+        (Comma,": "),(StringToken,"x"),(StringToken,"2"),(WhiteSpace,"\\n"),(EndMap,":\\n"),
+      (Comma,""),(StringToken,"w"),(WhiteSpace," "),
+        (StartMap,": "),(StringToken,"c"),(StringToken,"3"),(EndMap,""),
+      (EndMap,""),(Eof,""))
+
+    assert(actual,expected)
+  }
+
+  test("mixed types sequence"){
+    val input=
+      """- a: 1
+        |  b: 2
+        |- z""".stripMargin
+    val actual: List[(Token,String)] = YamlLexer(input).lex()
+    println(actual.toString)
+    val expected = List(
+      (StartSequence,"- "),
+        (StartMap,": "),(StringToken,"a"),(StringToken,"1"),(WhiteSpace,"\\n"),(WhiteSpace,"  "),
+        (Comma,": "),(StringToken,"b"),(StringToken,"2"),(WhiteSpace,"\\n"),(EndMap,""),
+      (Comma,"- "),(StringToken,"z"),
+       (EndSequence,""),(Eof,""))
+
+    assert(actual,expected)
+  }
+
+  test("mixed types mapping"){
+    val input=
+      """a: 1
+        |b:
+        | - 2
+        | - 3""".stripMargin
+    val actual: List[(Token,String)] = YamlLexer(input).lex()
+    println(actual.toString)
+    val expected = List(
+      (StartMap,": "),(StringToken,"a"),(StringToken,"1"),(WhiteSpace,"\\n"),
+      (Comma,":\\n"),(StringToken,"b"),(WhiteSpace," "),
+        (StartSequence,"- "),(StringToken,"2"),(WhiteSpace,"\\n"),(WhiteSpace," "),
+        (Comma,"- "),(StringToken,"3"),
+        (EndSequence,""),
+      (EndMap,""),(Eof,""))
+
     assert(actual,expected)
   }
 
