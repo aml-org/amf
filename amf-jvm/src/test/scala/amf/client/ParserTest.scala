bc72813f5f72829d97bac9381e31af5903513d41
Antonio Garrote <antoniogarrote@gmail.com>
2017-11-01 13:51:26 +0000

    Fixing order checking types in JSON-LD documents being parsed

--- amf-jvm/src/test/scala/amf/client/ParserTest.scala
+++ amf-jvm/src/test/scala/amf/client/ParserTest.scala
@@ -154,6 +154,34 @@ class ParserTest extends AsyncFunSuite with PlatformSecrets with PairsAMFUnitFix
 
   }
 
+  test("Dialects regeneration tests with external JSON-LD") {
+    platform.dialectsRegistry.registerDialect("file://shared/src/test/resources/vocabularies/amc2/dialect.raml") map { parsedDialect =>
+      new RamlParser().parseFileAsync("file://shared/src/test/resources/vocabularies/amc2/example.raml", platform).get
+    } map { _ =>
+      val externalJsonld= "[{\"@id\":\"file:///tmp/amc/example.raml\",\"http://raml.org/vocabularies/document#references\":[{\"@id\":\"file:///tmp/amc/reusable-jmx-publisher.raml\",\"http://raml.org/vocabularies/document#encodes\":[{\"@id\":\"file:///tmp/amc/reusable-jmx-publisher.raml#\",\"http://mulesoft.com/vocabularies/mule-runtime#timeUnit\":[{\"@value\":\"SECONDS\"}],\"http://mulesoft.com/vocabularies/mule-runtime#frequency\":[{\"@value\":100}],\"http://mulesoft.com/vocabularies/mule-runtime#enabledComponent\":[{\"@value\":true}],\"http://mulesoft.com/vocabularies/mule-runtime#componentId\":[{\"@value\":\"mule.agent.jmx.publisher.service\"}],\"@type\":[\"http://mulesoft.com/vocabularies/mule-runtime#JmxPublisherService\"]}],\"@type\":[\"http://raml.org/vocabularies/document#Unit\",\"http://raml.org/vocabularies/document#Fragment\",\"http://raml.org/vocabularies/document#DialectNode\"]}],\"http://raml.org/vocabularies/document#encodes\":[{\"@id\":\"file:///tmp/amc/example.raml#\",\"http://mulesoft.com/vocabularies/mule-runtime#transport\":[{\"@id\":\"file:///tmp/amc/example.raml#websocket.transport\",\"http://mulesoft.com/vocabularies/mule-runtime#keystore\":[{\"@id\":\"file:///tmp/amc/example.raml#websocket.transport/security\",\"http://mulesoft.com/vocabularies/mule-runtime#keyStorePassword\":[{\"@value\":\"exampleNs/123\"}],\"http://mulesoft.com/vocabularies/mule-runtime#keyAliasPassword\":[{\"@value\":\"exampleNs/124\"}],\"http://mulesoft.com/vocabularies/mule-runtime#keyAlias\":[{\"@value\":\"agent\"}],\"@type\":[\"http://mulesoft.com/vocabularies/mule-runtime#KeyStore\"]}],\"http://mulesoft.com/vocabularies/mule-runtime#componentId\":[{\"@value\":\"websocket.transport\"}],\"@type\":[\"http://mulesoft.com/vocabularies/mule-runtime#WebSocketsTransport\"]}],\"http://mulesoft.com/vocabularies/mule-runtime#supportsService\":[{\"@id\":\"file:///tmp/amc/example.raml#mule.agent.jmx.publisher.service\",\"http://mulesoft.com/vocabularies/mule-runtime#timeUnit\":[{\"@value\":\"SECONDS\"}],\"http://mulesoft.com/vocabularies/mule-runtime#frequency\":[{\"@value\":100}],\"http://mulesoft.com/vocabularies/mule-runtime#enabledComponent\":[{\"@value\":true}],\"http://mulesoft.com/vocabularies/mule-runtime#componentId\":[{\"@value\":\"mule.agent.jmx.publisher.service\"}],\"@type\":[\"http://mulesoft.com/vocabularies/mule-runtime#JmxPublisherService\"]}],\"@type\":[\"http://mulesoft.com/vocabularies/mule-runtime#Agent\"]}],\"@type\":[\"http://raml.org/vocabularies/document#Unit\",\"http://raml.org/vocabularies/document#Module\",\"http://raml.org/vocabularies/document#Fragment\",\"http://raml.org/vocabularies/document#Document\"]}]"
+      new AmfParser().parseStringAsync(externalJsonld, platform).get
+    } map { parsedModel =>
+      new RamlGenerator().generateString(parsedModel).get
+    } map { generatedRaml =>
+      val expected =
+        """#%Mule Agent Configuration 0.1
+          |transports:
+          |  websocket.transport:
+          |    security:
+          |      keyStorePassword: exampleNs/123
+          |      keyStoreAlias: agent
+          |      keyStoreAliasPassword: exampleNs/124
+          |services:
+          |  mule.agent.jmx.publisher.service:
+          |    enabled: true
+          |    frequency: 100
+          |    frequencyTimeUnit: SECONDS
+          |""".stripMargin
+      assert(generatedRaml == expected)
+    }
+
+  }
+
   def assertModule(actual: Module, expected: Module): Assertion = {
     actual should be(expected)
     succeed
