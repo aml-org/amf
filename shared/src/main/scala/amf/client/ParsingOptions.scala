b91eb3de8d0a6b08c1bcdfa8d99f639a57585106
Antonio Garrote <antoniogarrote@gmail.com>
2017-11-16 18:32:34 +0000

    APIMF-249 Parser never stops on parser-side errors

--- shared/src/main/scala/amf/client/ParsingOptions.scala
+++ shared/src/main/scala/amf/client/ParsingOptions.scala
@@ -1,14 +1,6 @@
 package amf.client
 
 class ParsingOptions {
-  private var exceptionOnFailedValidation = true
-
-  def withoutExceptionOnFailedValidation(shouldThrow: Boolean): ParsingOptions = {
-    exceptionOnFailedValidation = shouldThrow
-    this
-  }
-
-  def isWithExceptionOnFailedValidation = exceptionOnFailedValidation
 }
 
 object ParsingOptions {
