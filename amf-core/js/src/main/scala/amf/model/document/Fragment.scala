305862bf5622dd3f9416128cff4a07cead418b5f
Antonio Garrote <antoniogarrote@gmail.com>
2017-11-29 23:54:34 +0000

    APIMF-161 fixing error with duplicated Fragment wrappers

--- amf-core/js/src/main/scala/amf/model/document/Fragment.scala
+++ amf-core/js/src/main/scala/amf/model/document/Fragment.scala
@@ -5,7 +5,7 @@ import amf.core.model.document
 import scala.scalajs.js.annotation.JSExportAll
 
 @JSExportAll
-case class Fragment(private[amf] val fragment: document.Fragment) extends BaseUnit with EncodesModel {
+class Fragment(private[amf] val fragment: document.Fragment) extends BaseUnit with EncodesModel {
 
   override private[amf] val element = fragment
 }
--- amf-webapi/js/src/main/scala/amf/model/document/Fragment.scala
+++ amf-webapi/js/src/main/scala/amf/model/document/Fragment.scala
@@ -1,7 +1,6 @@
 package amf.model.document
 
 import amf.core.model.document.{Fragment => CoreFragment}
-import amf.core.unsafe.PlatformSecrets
 import amf.plugins.document.webapi.model.{AnnotationTypeDeclarationFragment => CoreAnnotationTypeDeclarationFragment, DataTypeFragment => CoreDataTypeFragment, DocumentationItemFragment => CoreDocumentationItemFragment, ExternalFragment => CoreExternalFragment, NamedExampleFragment => CoreNamedExampleFragment, ResourceTypeFragment => CoreResourceTypeFragment, SecuritySchemeFragment => CoreSecuritySchemeFragment, TraitFragment => CoreTraitFragment}
 
 import scala.scalajs.js.annotation.JSExportAll
@@ -10,55 +9,50 @@ import scala.scalajs.js.annotation.JSExportAll
   * JS Fragment model class
   */
 @JSExportAll
-abstract class Fragment(private[amf] val fragment: CoreFragment) extends BaseUnit with EncodesModel {
-
-  override private[amf] val element = fragment
-
-}
-
-object Fragment extends PlatformSecrets {
-  // TODO: This is missing for the dialects @modularization
-  // case d: CoreDialectFragment                   => DialectFragment(d)
-  def apply(fragment: CoreFragment): Fragment = platform.wrap(fragment)
-}
-
 case class DocumentationItem(private[amf] val documentationItem: CoreDocumentationItemFragment)
   extends Fragment(documentationItem) {
   def this() = this(CoreDocumentationItemFragment())
 
 }
 
+@JSExportAll
 case class DataType(private[amf] val dataType: CoreDataTypeFragment) extends Fragment(dataType) {
   def this() = this(CoreDataTypeFragment())
 }
 
+@JSExportAll
 case class NamedExample(private[amf] val namedExample: CoreNamedExampleFragment)
   extends Fragment(namedExample) {
   def this() = this(CoreNamedExampleFragment())
 }
 
+@JSExportAll
 case class ExternalFragment(private[amf] val ef: CoreExternalFragment) extends Fragment(ef) {
   def this() = this(CoreExternalFragment())
 }
 
+@JSExportAll
 case class ResourceTypeFragment(private[amf] val resourceTypeFragment: CoreResourceTypeFragment)
   extends Fragment(resourceTypeFragment) {
 
   def this() = this(CoreResourceTypeFragment())
 }
 
+@JSExportAll
 case class TraitFragment(private[amf] val traitFragment: CoreTraitFragment)
   extends Fragment(traitFragment) {
 
   def this() = this(CoreTraitFragment())
 }
 
+@JSExportAll
 case class AnnotationTypeDeclaration(
                                       private[amf] val annotationTypeDeclaration: CoreAnnotationTypeDeclarationFragment)
   extends Fragment(annotationTypeDeclaration) {
   def this() = this(CoreAnnotationTypeDeclarationFragment())
 }
 
+@JSExportAll
 case class SecuritySchemeFragment(private[amf] val extensionFragment: CoreSecuritySchemeFragment)
   extends Fragment(extensionFragment) {
 
