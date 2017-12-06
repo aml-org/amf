7ff02da49f03b7b803232f8353b14ac7fd33e3f7
Antonio Garrote <antoniogarrote@gmail.com>
2017-12-06 20:48:56 +0000

    Requiring with absolute path shacl js

--- amf-client/js/build-scripts/buildjs.sh
+++ amf-client/js/build-scripts/buildjs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo '#!/usr/bin/env node' > amf.js
-echo 'SHACLValidator = require("./src/main/resources/shacl_node.js")' >> amf.js
+echo 'SHACLValidator = require(__dirname + "/src/main/resources/shacl_node.js")' >> amf.js
 cat ./target/artifact/amf-client-module.js >> amf.js
 chmod a+x amf.js
