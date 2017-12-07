7f600030ac472d2f6f9d8ce3b71a0955a158a52d
Martín Gutierrez <martin.gutierrez@mulesoft.com>
2017-12-07 11:48:59 -0300

    Change shacl publish and amf publish for npm

--- amf-client/js/build-scripts/buildjs.sh
+++ amf-client/js/build-scripts/buildjs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo '#!/usr/bin/env node' > amf.js
-echo 'SHACLValidator = require(__dirname + "/src/main/resources/shacl_node.js")' >> amf.js
+echo 'SHACLValidator = require("@mulesoft/amf-shacl-node")' >> amf.js
 cat ./target/artifact/amf-client-module.js >> amf.js
 chmod a+x amf.js
