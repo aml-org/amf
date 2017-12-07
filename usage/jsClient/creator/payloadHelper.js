cc94c97732a9202f8168a465866bf4ea17a94a90
Alejandro Loose <alejandro.loose@mulesoft.com>
2017-12-07 10:01:42 -0300

    Delete usage (moved to amf-examples repo)

--- usage/jsClient/creator/payloadHelper.js
+++
@@ -1,7 +0,0 @@
-exports.payloadHelper = {
-
-    populate200Playlists: function (payload) {
-        payload.withMediaType("application/json")
-            .withSchema("string");
-    }
-}
--- usage/jsClient/creator/responseHelper.js
+++
@@ -1,8 +0,0 @@
-var PayloadHelper = require('./payloadHelper.js').payloadHelper;
-
-exports.responseHelper = {
-
-    populate200Playlists: function (response) {
-        PayloadHelper.populate200Playlists(response.withPayload())
-    }
-}
