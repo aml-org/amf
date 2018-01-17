a340f521a3243037927f18d5f019d2cba80453b4
Martín Gutierrez <martin.gutierrez@mulesoft.com>
2018-01-17 14:23:19 -0300

    Clean resolution.

--- amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/resolution/pipelines/OasResolutionPipeline.scala
+++ amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/resolution/pipelines/OasResolutionPipeline.scala
@@ -2,23 +2,13 @@ package amf.plugins.document.webapi.resolution.pipelines
 
 import amf.ProfileNames
 import amf.core.model.document.BaseUnit
-import amf.plugins.domain.webapi.resolution.stages.{ExamplesResolutionStage, MediaTypeResolutionStage, ParametersNormalizationStage}
+import amf.plugins.domain.webapi.resolution.stages.{
+  ExamplesResolutionStage,
+  MediaTypeResolutionStage,
+  ParametersNormalizationStage
+}
 
 class OasResolutionPipeline extends AmfResolutionPipeline {
-
   override val parameters = new ParametersNormalizationStage(ProfileNames.OAS)
   override val mediaTypes = new MediaTypeResolutionStage(ProfileNames.OAS)
-  override val examples   = new ExamplesResolutionStage(ProfileNames.OAS)
-
-  override def resolve[T <: BaseUnit](model: T): T = {
-    withModel(model) { () =>
-      commonSteps()
-      step(parameters)
-      step(mediaTypes)
-      step(examples)
-      step(cleanRefs)
-      step(cleanDecls)
-    }
-  }
-
 }
--- amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/resolution/pipelines/Raml10ResolutionPipeline.scala
+++ amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/resolution/pipelines/Raml10ResolutionPipeline.scala
@@ -2,23 +2,14 @@ package amf.plugins.document.webapi.resolution.pipelines
 
 import amf.ProfileNames
 import amf.core.model.document.BaseUnit
-import amf.plugins.domain.webapi.resolution.stages.{ExamplesResolutionStage, MediaTypeResolutionStage, ParametersNormalizationStage}
+import amf.plugins.domain.webapi.resolution.stages.{
+  ExamplesResolutionStage,
+  MediaTypeResolutionStage,
+  ParametersNormalizationStage
+}
 
 class Raml10ResolutionPipeline extends AmfResolutionPipeline {
-
   override val parameters = new ParametersNormalizationStage(ProfileNames.RAML)
   override val mediaTypes = new MediaTypeResolutionStage(ProfileNames.RAML)
   override val examples   = new ExamplesResolutionStage(ProfileNames.RAML)
-
-  override def resolve[T <: BaseUnit](model: T): T = {
-    withModel(model) { () =>
-      commonSteps()
-      step(parameters)
-      step(mediaTypes)
-      step(examples)
-      step(cleanRefs)
-      step(cleanDecls)
-    }
-  }
-
 }
