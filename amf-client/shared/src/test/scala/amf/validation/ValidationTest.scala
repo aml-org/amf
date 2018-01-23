8d234b803a3a4fcdfa7b76a8a4c8d24006e38a71
Martín Gutierrez <martin.gutierrez@mulesoft.com>
2018-01-23 11:41:43 -0300

    APIMF-384 Add test of resource type fragment.

--- amf-client/shared/src/test/scala/amf/validation/ValidationTest.scala
+++ amf-client/shared/src/test/scala/amf/validation/ValidationTest.scala
@@ -733,6 +733,17 @@ class ValidationTest extends AsyncFunSuite with PlatformSecrets {
     }
   }
 
+  test("Spec resource type fragment") {
+    val validation = Validation(platform)
+    for {
+      doc <- AMFCompiler(validationsPath + "resource_types/fragment.raml", platform, RamlYamlHint, validation)
+        .build()
+      report <- validation.validate(doc, ProfileNames.RAML)
+    } yield {
+      assert(report.conforms)
+    }
+  }
+
   test("08 Validation") {
     val validation = Validation(platform)
     for {
