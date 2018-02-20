c62d3f34136e6eda37f9fd5c6b9660830dc802a9
Martín Gutierrez <martin.gutierrez@mulesoft.com>
2018-02-20 18:29:33 -0300

    APIMF-470 ResourceTypes and Traits parsing method.

--- amf-webapi/shared/src/main/scala/amf/plugins/domain/webapi/models/templates/ResourceType.scala
+++ amf-webapi/shared/src/main/scala/amf/plugins/domain/webapi/models/templates/ResourceType.scala
@@ -1,14 +1,22 @@
 package amf.plugins.domain.webapi.models.templates
 
+import amf.ProfileNames
+import amf.core.metamodel.domain.templates.AbstractDeclarationModel
+import amf.core.model.document.BaseUnit
 import amf.core.model.domain.templates.AbstractDeclaration
 import amf.core.parser.{Annotations, Fields}
 import amf.plugins.domain.webapi.metamodel.templates.ResourceTypeModel
+import amf.plugins.domain.webapi.models.EndPoint
+import amf.plugins.domain.webapi.resolution.ExtendsHelper
 import org.yaml.model.YPart
 
 case class ResourceType(fields: Fields, annotations: Annotations) extends AbstractDeclaration(fields, annotations) {
   override def linkCopy(): ResourceType = ResourceType().withId(id)
 
-  override def meta = ResourceTypeModel
+  override def meta: AbstractDeclarationModel = ResourceTypeModel
+
+  def asEndpoint[T <: BaseUnit](unit: T, profile: String = ProfileNames.RAML): EndPoint =
+    ExtendsHelper.asEndpoint(unit, profile, dataNode, name)
 }
 
 object ResourceType {
