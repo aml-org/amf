af41729c13ad09d2ec5ddff24d856b656498e84f
Antonio Garrote <antoniogarrote@gmail.com>
2018-03-09 13:17:28 -0800

    Adding support for resolution of dialect instances

--- amf-vocabularies/shared/src/main/scala/amf/plugins/document/vocabularies2/model/document/DialectInstance.scala
+++ amf-vocabularies/shared/src/main/scala/amf/plugins/document/vocabularies2/model/document/DialectInstance.scala
@@ -2,10 +2,11 @@ package amf.plugins.document.vocabularies2.model.document
 
 import amf.core.metamodel.Obj
 import amf.core.model.document.{BaseUnit, DeclaresModel, EncodesModel}
-import amf.core.model.domain.DomainElement
+import amf.core.model.domain.{AmfObject, DomainElement, Linkable}
 import amf.core.parser.{Annotations, Fields}
 import amf.plugins.document.vocabularies2.metamodel.document.{DialectInstanceFragmentModel, DialectInstanceLibraryModel, DialectInstanceModel}
 import amf.plugins.document.vocabularies2.metamodel.document.DialectInstanceModel._
+import amf.plugins.document.vocabularies2.model.domain.DialectDomainElement
 
 case class DialectInstance(fields: Fields, annotations: Annotations) extends BaseUnit with DeclaresModel with EncodesModel  {
 
@@ -21,6 +22,84 @@ case class DialectInstance(fields: Fields, annotations: Annotations) extends Bas
   def definedBy(): String = fields(DefinedBy)
   def withDefinedBy(dialectId: String) = set(DefinedBy, dialectId)
 
+  override def transform(selector: (DomainElement) => Boolean,
+                transformation: (DomainElement, Boolean) => Option[DomainElement]): BaseUnit = {
+    val domainElementAdapter = (o: AmfObject) => {
+      o match {
+        case e: DomainElement => selector(e)
+        case _                => false
+      }
+    }
+    val transformationAdapter = (o: AmfObject, isCycle: Boolean) => {
+      o match {
+        case e: DomainElement => transformation(e, isCycle)
+        case _                => Some(o)
+      }
+    }
+    transformByCondition(this, domainElementAdapter, transformationAdapter)
+    this
+  }
+
+  override protected def transformByCondition(element: AmfObject,
+                                              predicate: (AmfObject) => Boolean,
+                                              transformation: (AmfObject, Boolean) => Option[AmfObject],
+                                              cycles: Set[String] = Set.empty): AmfObject = {
+    if (!cycles.contains(element.id)) {
+      // not visited yet
+      if (predicate(element)) { // matches predicate, we transform
+        transformation(element, false).orNull
+      } else {
+        // not matches the predicate, we traverse
+
+        element match {
+          case dataNode: DialectDomainElement =>
+            dataNode.objectProperties.foreach {
+              case (prop, value) =>
+                Option(transformByCondition(value, predicate, transformation, cycles + element.id)) match {
+                  case Some(transformed: DialectDomainElement) =>
+                    dataNode.objectProperties.put(prop, transformed)
+                    dataNode
+                  case _ =>
+                    dataNode.objectProperties.remove(prop)
+                    dataNode
+                }
+            }
+            dataNode.objectCollectionProperties.foreach {
+              case (prop, values: Seq[DialectDomainElement]) =>
+                val newValues = values.map { value =>
+                  Option(transformByCondition(value, predicate, transformation, cycles + element.id)) match {
+                    case Some(transformed: DialectDomainElement) => Some(transformed)
+                    case _                                       => None
+                  }
+                } collect { case Some(x) => x }
+                if (newValues.isEmpty) {
+                  dataNode.objectCollectionProperties.remove(prop)
+                } else {
+                  dataNode.objectCollectionProperties.put(prop, newValues)
+                }
+                dataNode
+              case _ => dataNode
+            }
+
+          case other => super.transformByCondition(other, predicate, transformation, cycles)
+        }
+        element
+      }
+
+    } else
+      element match {
+        // target of the link has been traversed, we still visit the link in case a transformer wants to
+        // transform links/references, but we will not traverse to avoid loops
+        case linkable: Linkable if linkable.isLink =>
+          if (predicate(element)) {
+            transformation(element, true).orNull // passing the cycle boolean flat!
+          } else {
+            element
+          }
+        // traversed and not visited
+        case _ => element
+      }
+  }
 }
 
 object DialectInstance {
