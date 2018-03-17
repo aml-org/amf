6ed4e2294a7dd027b7d859241c6e3e5d245adc3e
Hernan Najles <hernan.najles@mulesoft.com>
2018-03-17 12:38:09 -0300

    APIMF-599 add anyshape model fields to union model. Now we dump the examples in jsonld for this shape

--- amf-webapi/shared/src/main/scala/amf/plugins/domain/shapes/metamodel/UnionShapeModel.scala
+++ amf-webapi/shared/src/main/scala/amf/plugins/domain/shapes/metamodel/UnionShapeModel.scala
@@ -10,7 +10,7 @@ object UnionShapeModel extends AnyShapeModel {
 
   val AnyOf = Field(Array(ShapeModel), Namespace.Shapes + "anyOf")
 
-  override def fields: List[Field] = List(AnyOf) ++ ShapeModel.fields ++ DomainElementModel.fields
+  override def fields: List[Field] = List(AnyOf) ++ AnyShapeModel.fields ++ DomainElementModel.fields
 
   override val `type`: List[ValueType] =
     List(Namespace.Shapes + "UnionShape", Namespace.Shacl + "Shape", Namespace.Shapes + "Shape")
