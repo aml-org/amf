a391a90f3d910bed2fbaa0c2cf1aa2061a255225
Antonio Garrote <antoniogarrote@gmail.com>
2018-03-21 09:15:07 -0700

    APIMF-535 fixing problems with 0.8 docs containing header and external fragments

--- amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/WebApiDocuments.scala
+++ amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/WebApiDocuments.scala
@@ -1,7 +1,3 @@
 package amf.plugins.document.webapi
 
-import amf.plugins.document.webapi.annotations._
-import amf.plugins.document.webapi.metamodel.FragmentsTypesModels._
-import amf.plugins.document.webapi.metamodel.{ExtensionModel, OverlayModel}
-
 trait WebApiDocuments {}
