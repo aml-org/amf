eb08ed7a644a6dcdf60d3699696e4d04e1f0190e
Antonio Garrote <antoniogarrote@gmail.com>
2018-03-24 10:46:18 -0700

    APIMF-563 testing all resolution tests with the 'editing' pipeline

--- amf-core/shared/src/main/scala/amf/core/services/RuntimeResolver.scala
+++ amf-core/shared/src/main/scala/amf/core/services/RuntimeResolver.scala
@@ -6,14 +6,14 @@ import amf.core.registries.AMFPluginsRegistry
 
 object RuntimeResolver {
 
-  def resolve(vendor: String, unit: BaseUnit): BaseUnit = {
+  def resolve(vendor: String, unit: BaseUnit, pipelineId: String): BaseUnit = {
     var plugin = AMFPluginsRegistry.documentPluginForID(vendor) match {
       case Some(documentPlugin) => Some(documentPlugin)
       case None                 => AMFPluginsRegistry.documentPluginForVendor(vendor).headOption
     }
 
     plugin match {
-      case Some(documentPlugin: AMFDocumentPlugin) => documentPlugin.resolve(unit)
+      case Some(documentPlugin: AMFDocumentPlugin) => documentPlugin.resolve(unit, pipelineId)
       case None =>
         throw new Exception(s"Cannot find domain plugin for vendor $vendor to resolve unit ${unit.location}")
     }
