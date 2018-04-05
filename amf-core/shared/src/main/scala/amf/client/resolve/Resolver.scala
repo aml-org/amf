2d06ba41825b31aa800ad7d8f93ea4d5aa876892
Antonio Garrote <antoniogarrote@gmail.com>
2018-04-04 17:21:56 -0700

    Fixing problems resolving from wrappers.

--- amf-core/shared/src/main/scala/amf/client/resolve/Resolver.scala
+++ amf-core/shared/src/main/scala/amf/client/resolve/Resolver.scala
@@ -1,7 +1,9 @@
 package amf.client.resolve
 
+import amf.client.convert.CoreClientConverters
 import amf.client.convert.CoreClientConverters._
 import amf.client.model.document.BaseUnit
+import amf.core.model.document.{BaseUnit => InternalBaseUnit}
 import amf.core.resolution.pipelines.ResolutionPipeline
 import amf.core.services.RuntimeResolver
 
@@ -9,6 +11,13 @@ import scala.scalajs.js.annotation.JSExportAll
 
 @JSExportAll
 class Resolver(vendor: String) {
-  def resolve(unit: BaseUnit): BaseUnit = RuntimeResolver.resolve(vendor, unit, ResolutionPipeline.DEFAULT_PIPELINE)
-  def resolve(unit: BaseUnit, pipelineId: String) = RuntimeResolver.resolve(vendor, unit, pipelineId)
+  def resolve(unit: BaseUnit): BaseUnit = {
+    convert(RuntimeResolver.resolve(vendor, unit._internal, ResolutionPipeline.DEFAULT_PIPELINE))
+  }
+
+  def resolve(unit: BaseUnit, pipelineId: String) = convert(RuntimeResolver.resolve(vendor, unit._internal, pipelineId))
+
+  protected def convert(unit: InternalBaseUnit) = {
+    CoreClientConverters.asClient(unit)
+  }
 }
