4c9305bb4fc510cb846eddfbded4a89a7d7aac7e
Antonio Garrote <antoniogarrote@gmail.com>
2018-04-19 12:02:16 -0700

    APIMF-421 Checking invalid media types in response

--- amf-client/shared/src/test/scala/amf/validation/ValidationTest.scala
+++ amf-client/shared/src/test/scala/amf/validation/ValidationTest.scala
@@ -1623,6 +1623,17 @@ class ValidationTest extends AsyncFunSuite with PlatformSecrets {
     }
   }
 
+  test("Invalid type example 4 test") {
+    for {
+      validation <- Validation(platform)
+      doc        <- AMFCompiler(validationsPath + "invalidex4.raml", platform, OasYamlHint, validation).build()
+      report     <- validation.validate(doc, ProfileNames.AMF)
+    } yield {
+      assert(!report.conforms)
+      assert(report.results.count(_.level == SeverityLevels.VIOLATION) == 1)
+    }
+  }
+
   test("Valid type example 1 test") {
     for {
       validation <- Validation(platform)
