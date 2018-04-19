f9abf36598817444ac48e55ca08c892be8eb586b
Antonio Garrote <antoniogarrote@gmail.com>
2018-04-19 10:42:17 -0700

    APIMF-584 Cannot override default types in type declarations

--- amf-client/shared/src/test/scala/amf/validation/ValidationTest.scala
+++ amf-client/shared/src/test/scala/amf/validation/ValidationTest.scala
@@ -1600,4 +1600,15 @@ class ValidationTest extends AsyncFunSuite with PlatformSecrets {
       assert(report.results.count(_.level == SeverityLevels.VIOLATION) == 1)
     }
   }
+
+  test("Invalid type example 2 test") {
+    for {
+      validation <- Validation(platform)
+      doc        <- AMFCompiler(validationsPath + "invalidex2.raml", platform, OasYamlHint, validation).build()
+      report     <- validation.validate(doc, ProfileNames.AMF)
+    } yield {
+      assert(!report.conforms)
+      assert(report.results.count(_.level == SeverityLevels.VIOLATION) == 1)
+    }
+  }
 }
