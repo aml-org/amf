2fec0964e21b2c67f1a23223cdac52d9d85de081
Antonio Garrote <antoniogarrote@gmail.com>
2018-05-30 10:39:15 -0700

    APIMF-790 transforming exception resolving wrong includes in resolution into proper validation errors

--- amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/parser/spec/raml/Raml08DocumentParser.scala
+++ amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/parser/spec/raml/Raml08DocumentParser.scala
@@ -57,6 +57,9 @@ case class Raml08DocumentParser(root: Root)(implicit override val ctx: RamlWebAp
       val entries = entry.value.tagType match {
         case YType.Seq => entry.value.as[Seq[YMap]].flatMap(m => m.entries)
         case YType.Map => entry.value.as[YMap].entries
+        case t         =>
+          ctx.violation(parent, s"Invalid node $t in abstract declaration", entry.value)
+          Nil
       }
       entries.foreach { entry =>
         ctx.declarations += AbstractDeclarationParser(producer(entry), parent, entry).parse()
