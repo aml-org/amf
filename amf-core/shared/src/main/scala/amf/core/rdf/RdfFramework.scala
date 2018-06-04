e74a34cf5cdfd6afd8efbe7297f08dbf97a43b64
Antonio Garrote <antoniogarrote@gmail.com>
2018-06-04 16:50:11 -0700

    KG-114 exporting parsed units as RDF native models

--- amf-core/shared/src/main/scala/amf/core/rdf/RdfFramework.scala
+++ amf-core/shared/src/main/scala/amf/core/rdf/RdfFramework.scala
@@ -1,7 +1,16 @@
 package amf.core.rdf
 
+import amf.core.emitter.RenderOptions
+import amf.core.model.document.BaseUnit
+
 trait RdfFramework {
 
   def emptyRdfModel(): RdfModel
-  
+
+  def unitToRdfModel(unit: BaseUnit, options: RenderOptions): RdfModel = {
+    val model = emptyRdfModel()
+    new RdfModelEmitter(model).emit(unit, options)
+    model
+  }
+
 }
