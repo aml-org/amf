cbebe7b90ed2d1b0fc978b0bc41910ef8850329c
Martín Gutierrez <martin.gutierrez@mulesoft.com>
2018-07-16 11:32:25 -0300

    APIMF-956 - Fix js publication.

--- amf-client/js/build-scripts/buildjs.sh
+++ amf-client/js/build-scripts/buildjs.sh
@@ -3,6 +3,7 @@
 cd ./amf-client/js
 
 echo 'SHACLValidator = require("amf-shacl-node")' > amf.js
+echo 'Ajv = require("ajv")' >> amf.js
 cat ./target/artifact/amf-client-module.js >> amf.js
 chmod a+x amf.js
 
