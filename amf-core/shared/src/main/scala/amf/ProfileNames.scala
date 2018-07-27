1e3906965577ca77c11c7e4b6330b28b719e209b
Loose <alejandro.loose@mulesoft.com>
2018-07-27 11:32:12 -0300

    APIMF-975: added an enum of datatypes namespaces to the client

--- amf-core/shared/src/main/scala/amf/ProfileNames.scala
+++ amf-core/shared/src/main/scala/amf/ProfileNames.scala
@@ -1,5 +1,7 @@
 package amf
 
+import amf.core.vocabulary.Namespace
+
 import scala.scalajs.js.annotation.{JSExportAll, JSExportTopLevel}
 
 @JSExportAll
@@ -74,4 +76,27 @@ object OASStyle extends MessageStyle {
 }
 object AMFStyle extends MessageStyle {
   override def profileName: ProfileName = AMFProfile
+}
+
+@JSExportAll
+@JSExportTopLevel("DataTypes")
+object DataTypes {
+  val String: String       = (Namespace.Xsd + "string").iri()
+  val Integer: String      = (Namespace.Xsd + "integer").iri()
+  val Number: String       = (Namespace.Shapes + "number").iri()
+  val Long: String         = (Namespace.Xsd + "long").iri()
+  val Double: String       = (Namespace.Xsd + "double").iri()
+  val Float: String        = (Namespace.Xsd + "float").iri()
+  val Decimal: String      = (Namespace.Xsd + "decimal").iri()
+  val Boolean: String      = (Namespace.Xsd + "boolean").iri()
+  val Date: String         = (Namespace.Xsd + "date").iri()
+  val Time: String         = (Namespace.Xsd + "time").iri()
+  val DateTime: String     = (Namespace.Xsd + "dateTime").iri()
+  val DateTimeOnly: String = (Namespace.Shapes + "dateTimeOnly").iri()
+  val File: String         = (Namespace.Shapes + "file").iri()
+  val Byte: String         = (Namespace.Xsd + "byte").iri()
+  val Binary: String       = (Namespace.Xsd + "base64Binary").iri()
+  val Password: String     = (Namespace.Shapes + "password").iri()
+  val Any: String          = (Namespace.Xsd + "anyType").iri()
+  val Nil: String          = (Namespace.Xsd + "nil").iri()
 }
