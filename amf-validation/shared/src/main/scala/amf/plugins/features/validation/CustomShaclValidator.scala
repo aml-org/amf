e5e0eefd158d3ac5923c29f9eceda6556e14c5c4
Hernan Najles <hernan.najles@mulesoft.com>
2018-08-16 10:56:35 -0300

    More prints to try to guess why an amf scalar is not instance of amf element in match)

--- amf-validation/shared/src/main/scala/amf/plugins/features/validation/CustomShaclValidator.scala
+++ amf-validation/shared/src/main/scala/amf/plugins/features/validation/CustomShaclValidator.scala
@@ -175,9 +175,13 @@ class CustomShaclValidator(model: BaseUnit, validations: EffectiveValidations, o
         case Some(f) =>
           Option(element.fields.getValue(f)) match {
             case Some(value) if value.value.isInstanceOf[AmfScalar] =>
-              if (fromMinLenght)
+              if (fromMinLenght) {
+
                 println(
                   "scalar value found amf scalar to scala: " + amfScalarToScala(value.value.asInstanceOf[AmfScalar]))
+                println("value: " + value)
+                println("value value: " + value.value)
+              }
               Some((value.annotations, value.value, Some(amfScalarToScala(value.value.asInstanceOf[AmfScalar]))))
             case Some(value) =>
               if (fromMinLenght) println("NOn scalar value found, ")
@@ -445,7 +449,13 @@ class CustomShaclValidator(model: BaseUnit, validations: EffectiveValidations, o
                        propertyConstraint: PropertyConstraint,
                        parentElement: DomainElement) = {
     println("Validation min count")
-    extractPropertyValue(propertyConstraint, parentElement, true) match {
+    val extracted = extractPropertyValue(propertyConstraint, parentElement, true)
+    if (extracted.isDefined) {
+      println("Amf element: " + extracted.get._2)
+      println("Is instance of amfscalar: " + extracted.get._2.isInstanceOf[AmfScalar])
+    }
+
+    extracted match {
       case Some((_, arr: AmfArray, _)) =>
         println("Some array found ")
         if (!(arr.values.length >= propertyConstraint.minCount.get.toInt)) {
