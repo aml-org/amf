a81f9d811e0bd519f206dd0e53c4883f53653cad
Martín Gutierrez <martin.gutierrez@mulesoft.com>
2018-10-01 16:38:05 -0300

    APIMF-1136 - Don't explode when using a tag in uses facet (ignore it).

--- amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/parser/spec/declaration/LibraryLocationParser.scala
+++ amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/parser/spec/declaration/LibraryLocationParser.scala
@@ -3,17 +3,16 @@ package amf.plugins.document.webapi.parser.spec.declaration
 import amf.core.parser.ParserContext
 import org.yaml.model.{YMapEntry, YScalar, YType}
 
-case class LibraryLocationParser(e: YMapEntry, ctx: ParserContext) {
+case class LibraryLocationParser(e: YMapEntry) {
 
-  def parse(): Option[String] = {
+  def parse()(implicit ctx: ParserContext): Option[String] = {
     e.value.tagType match {
-      case YType.Include                      => Some(e.value.as[YScalar].text)
       case YType.Null | YType.Map | YType.Seq => None
-      case _                                  => Some(e.value)
+      case _                                  => Some(e.value.as[YScalar].text) // TODO should we validate the tag?
     }
   }
 }
 
 object LibraryLocationParser {
-  def apply(e: YMapEntry, ctx: ParserContext): Option[String] = new LibraryLocationParser(e, ctx).parse()
+  def apply(e: YMapEntry, ctx: ParserContext): Option[String] = new LibraryLocationParser(e).parse()(ctx)
 }
