6af6fbf5ad400d032be1ed70748716abe993da09
Pedro Colunga <pedro.colunga@mulesoft.com>
2018-11-27 23:26:03 -0300

    Added RAML validation to fail tests.

--- amf-client/shared/src/test/scala/amf/emit/CompatibilityCycleTest.scala
+++ amf-client/shared/src/test/scala/amf/emit/CompatibilityCycleTest.scala
@@ -1,14 +1,18 @@
 package amf.emit
 
+import amf.ProfileNames
 import amf.core.model.document.BaseUnit
 import amf.core.remote._
+import amf.core.validation.AMFValidationReport
+import amf.facades.Validation
 import amf.io.FunSuiteCycleTests
 import amf.plugins.document.webapi.resolution.pipelines.RamlCompatibilityPipeline
 import org.mulesoft.common.io.AsyncFile
+import org.scalatest.Matchers
 
 import scala.concurrent.Future
 
-class CompatibilityCycleTest extends FunSuiteCycleTests {
+class CompatibilityCycleTest extends FunSuiteCycleTests with Matchers {
 
   override val basePath = "amf-client/shared/src/test/resources/compatibility/"
 
@@ -25,17 +29,23 @@ class CompatibilityCycleTest extends FunSuiteCycleTests {
         origin   <- build(c, None, useAmfJsonldSerialisation = true)
         rendered <- render(origin, c, useAmfJsonldSerialization = true)
         tmp      <- writeTemporaryFile(path)(rendered)
-        _        <- validate(tmp, RamlYamlHint)
+        report   <- validate(tmp, RamlYamlHint)
       } yield {
-        succeed
+        report.toString should include("Conforms? true")
       }
     }
   }
 
-  private def validate(source: AsyncFile, hint: Hint): Future[BaseUnit] = {
+  private def validate(source: AsyncFile, hint: Hint): Future[AMFValidationReport] = {
     println(s"About to validate ${source.path}")
-    val config = CycleConfig(source.path, source.path, hint, hint.vendor, "", None)
-    build(config, None, useAmfJsonldSerialisation = true)
+    Validation(platform)
+      .map(_.withEnabledValidation(false))
+      .flatMap(validation => {
+        val config = CycleConfig(source.path, source.path, hint, hint.vendor, "", None)
+        build(config, Some(validation), useAmfJsonldSerialisation = true).flatMap(unit => {
+          validation.validate(unit, ProfileNames.RAML)
+        })
+      })
   }
 
   override def transform(unit: BaseUnit, config: CycleConfig): BaseUnit = config.target match {
