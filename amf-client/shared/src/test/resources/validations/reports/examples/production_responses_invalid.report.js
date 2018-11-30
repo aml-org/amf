b1ee72a49ce85b1284146f8449afa02239dd64b1
Nicolas Schejtman <nschejtman93@gmail.com>
2018-11-30 14:27:44 -0300

    Fixed js tests

--- amf-client/shared/src/test/resources/validations/reports/examples/production_responses_invalid.report.js
+++ amf-client/shared/src/test/resources/validations/reports/examples/production_responses_invalid.report.js
@@ -5,10 +5,10 @@ Number of results: 1
 
 Level: Violation
 
-- Source: http://a.ml/vocabularies/amf/parser#missing-operation-status-code
-  Message: RAML Responses must have a valid status code
+- Source: http://a.ml/vocabularies/amf/parser#closed-shape
+  Message: Property application/json not supported in a RAML 1.0 response node
   Level: Violation
-  Target: file://amf-client/shared/src/test/resources/validations/production/responses-invalid.raml#/web-api/end-points/%2Ftest%2F%7Bid%7D/patch
+  Target: file://amf-client/shared/src/test/resources/validations/production/responses-invalid.raml#/web-api/end-points/%2Ftest%2F%7Bid%7D/patch/body
   Property: 
-  Position: Some(LexicalInformation([(11,16)-(14,26)]))
+  Position: Some(LexicalInformation([(13,12)-(14,26)]))
   Location: file://amf-client/shared/src/test/resources/validations/production/responses-invalid.raml
