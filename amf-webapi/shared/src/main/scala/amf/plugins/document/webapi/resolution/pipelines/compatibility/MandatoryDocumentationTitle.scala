ba96b6333184d1a78e1be4d7a2ced7f3ba1daa1c
Martin Gutierrez <martin.gutierrez@mulesoft.com>
2018-12-11 18:33:12 -0300

    Compatibility url to title.

--- amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/resolution/pipelines/compatibility/MandatoryDocumentationTitle.scala
+++ amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/resolution/pipelines/compatibility/MandatoryDocumentationTitle.scala
@@ -2,6 +2,7 @@ package amf.plugins.document.webapi.resolution.pipelines.compatibility
 import amf.core.model.document.{BaseUnit, Document}
 import amf.core.parser.ErrorHandler
 import amf.core.resolution.stages.ResolutionStage
+import amf.plugins.domain.shapes.metamodel.CreativeWorkModel
 import amf.plugins.domain.webapi.models.{Tag, WebApi}
 
 class MandatoryDocumentationTitle()(override implicit val errorHandler: ErrorHandler) extends ResolutionStage {
@@ -14,47 +15,48 @@ class MandatoryDocumentationTitle()(override implicit val errorHandler: ErrorHan
         try {
           ensureDocumentationTitle(d.encodes.asInstanceOf[WebApi])
         } catch {
-          case e: Throwable => // ignore: we don't want this to break anything
+          case _: Throwable => // ignore: we don't want this to break anything
         }
         model
       case _ => model
     }
   }
 
-  protected def ensureDocumentationTitle(api: WebApi) = {
+  private def ensureDocumentationTitle(api: WebApi): Unit = {
     if (missingTitle(api)) {
       addMissingTitle(api)
     }
+
     api.documentations.foreach { documentation =>
       if (documentation.title.isNullOrEmpty) {
-        documentation.withTitle("")
+        documentation.url.option() match {
+          case Some(url) => documentation.withTitle(url)
+          case _         => documentation.withTitle("generated")
+        }
       }
+      documentation.fields.removeField(CreativeWorkModel.Url)
     }
+
     api.tags.foreach { tag =>
       ensureDocumentationTitleInTag(tag)
     }
   }
 
-  protected def missingTitle(webApi: WebApi): Boolean = webApi.name.option().isEmpty
+  private def missingTitle(webApi: WebApi): Boolean = webApi.name.option().isEmpty
 
-
-  protected def addMissingTitle(webApi: WebApi): Unit = {
+  private def addMissingTitle(webApi: WebApi): Unit = {
     if (webApi.name.isNullOrEmpty) {
-      webApi.withName("")
+      webApi.withName("generated")
     }
-
   }
 
-
-  protected def ensureDocumentationTitleInTag(tag: Tag): Unit = {
+  private def ensureDocumentationTitleInTag(tag: Tag): Unit = {
     if (Option(tag.documentation).isDefined) {
       val documentationTitle = tag.name.option().getOrElse {
         tagCounter += 1
-        s"Tag ${tagCounter} documentation"
+        s"Tag $tagCounter documentation"
       }
       tag.documentation.withTitle(documentationTitle)
     }
   }
-
-
 }
