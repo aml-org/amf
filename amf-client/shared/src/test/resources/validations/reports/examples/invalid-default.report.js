3b00751797572ec5d710662e32a298d56f3e15c5
Pedro Colunga <pedro.colunga@mulesoft.com>
2018-12-12 10:47:57 -0300

    Fix js test.

--- amf-client/shared/src/test/resources/validations/reports/examples/invalid-default.report.js
+++ amf-client/shared/src/test/resources/validations/reports/examples/invalid-default.report.js
@@ -8,7 +8,7 @@ Level: Violation
 - Source: http://a.ml/vocabularies/amf/parser#example-validation-error
   Message: should match pattern "^[a-z]*$"
   Level: Violation
-  Target: file://amf-client/shared/src/test/resources/validations/examples/invalid-default.raml#/declarations/types/scalar/name/scalar_1
+  Target: file://amf-client/shared/src/test/resources/validations/examples/invalid-default.raml#/declarations/types/scalar/name/default/scalar_1
   Property: 
   Position: Some(LexicalInformation([(7,13)-(7,20)]))
   Location: file://amf-client/shared/src/test/resources/validations/examples/invalid-default.raml
