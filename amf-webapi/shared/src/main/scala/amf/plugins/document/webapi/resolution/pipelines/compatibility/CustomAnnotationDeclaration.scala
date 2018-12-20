cbe39fd6e3d3776a1bc45898d9cc635b1905b960
Nicolas Schejtman <nschejtman93@gmail.com>
2018-12-20 16:08:42 -0300

    Refactor CustomAnnotationDeclaration & add new annotations to WellKnownAnnotation

--- amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/resolution/pipelines/compatibility/CustomAnnotationDeclaration.scala
+++ amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/resolution/pipelines/compatibility/CustomAnnotationDeclaration.scala
@@ -1,23 +1,16 @@
 package amf.plugins.document.webapi.resolution.pipelines.compatibility
 
-import amf.core.metamodel.{Field, Obj}
 import amf.core.model.document.{BaseUnit, DeclaresModel}
-import amf.core.model.domain.DomainElement
 import amf.core.model.domain.extensions.CustomDomainProperty
-import amf.core.parser.{Annotations, ErrorHandler, Fields}
+import amf.core.parser.ErrorHandler
 import amf.core.resolution.stages.ResolutionStage
-import amf.core.utils.Strings
+import amf.plugins.document.webapi.parser.spec.common.WellKnownAnnotation
 import amf.plugins.domain.shapes.models.AnyShape
 
 class CustomAnnotationDeclaration()(override implicit val errorHandler: ErrorHandler) extends ResolutionStage {
   override def resolve[T <: BaseUnit](model: T): T = {
     try {
-      val annotationsTypes: Seq[DomainElement] = Seq("url", "format", "exclusiveMaximum", "exclusiveMinimum", "minimum",
-        "maximum", "format", "tuple", "collectionFormat", "additionalProperties", "dependencies", "readOnly",
-        "externalDocs", "format", "recursive", "pattern", "multipleOf", "collectionFormat", "payloads",
-        "baseUriParameters", "oasDeprecated", "summary", "tags", "consumes", "produces", "binding", "flow",
-        "parameters", "defaultResponse", "examples", "serverDescription", "servers", "responses", "termsOfService",
-        "contact", "license", "and", "or", "xor", "not", "xone")
+      val annotationsTypes = WellKnownAnnotation.ramlKnownAnnotations
         .map(name => CustomDomainProperty().withName(s"amf-$name").withSchema(AnyShape()))
 
       model match {
