7a54c9e3004be9064fefeb5bd2bc7367d4047745
Martin Gutierrez <martin.gutierrez@mulesoft.com>
2019-04-29 16:57:24 -0300

    APIMF-1472 - Keep inner schema name when its name is autogenerated.

--- amf-client/shared/src/test/scala/amf/resolution/EditingResolutionTest.scala
+++ amf-client/shared/src/test/scala/amf/resolution/EditingResolutionTest.scala
@@ -200,6 +200,10 @@ class EditingResolutionTest extends FunSuiteCycleTests {
     )
   }
 
+  test("Keep schema name in body link schema") {
+    cycle("body-link-name.raml", "body-link-name.jsonld", RamlYamlHint, Amf, validationsPath + "body-link-name/")
+  }
+
   /*
   test("Exchange experience API resolution to Amf") {
     cycle("api.v1.raml", "api.v1.resolved.jsonld", RamlYamlHint, Amf, productionPath + "exchange-experience-api-1.0.1-raml/")
