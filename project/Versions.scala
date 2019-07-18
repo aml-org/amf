848587985424d0a8021cd804774cdad168f429e1
Hernan Najles <hernan.naj@gmail.com>
2019-07-18 11:10:00 -0300

    Hack in Versions to work with source mode at als project

--- project/Versions.scala
+++ project/Versions.scala
@@ -7,8 +7,12 @@ import scala.collection.JavaConverters._
 
 object Versions {
   lazy val versions: Map[String, String] = {
-    val props = new Properties()
-    props.load(new FileInputStream(Path("versions.properties").asFile))
+    val props        = new Properties()
+    val absolutePath = Path("").asFile.getAbsolutePath
+    val versionsFile =
+      if (absolutePath.endsWith("als")) Path("../amf/versions.properties").asFile
+      else Path("versions.properties").asFile
+    props.load(new FileInputStream(versionsFile))
     props.entrySet().asScala.map(e => e.getKey.toString -> e.getValue.toString).toMap
   }
 }
