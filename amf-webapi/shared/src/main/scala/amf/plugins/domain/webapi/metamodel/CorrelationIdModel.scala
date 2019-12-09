b460441ff0180ee96b47ee49d9fb2483364d96fc
Martin Gutierrez <martin.gutierrez@mulesoft.com>
2019-12-09 11:57:37 -0300

    Move correlation id to core

--- amf-webapi/shared/src/main/scala/amf/plugins/domain/webapi/metamodel/CorrelationIdModel.scala
+++ amf-webapi/shared/src/main/scala/amf/plugins/domain/webapi/metamodel/CorrelationIdModel.scala
@@ -1,27 +1,27 @@
 package amf.plugins.domain.webapi.metamodel
+
 import amf.core.metamodel.Field
 import amf.core.metamodel.Type.Str
 import amf.core.metamodel.domain.common.DescriptionField
 import amf.core.metamodel.domain.{DomainElementModel, ModelDoc, ModelVocabularies}
-import amf.core.vocabulary.Namespace.ApiContract
+import amf.core.vocabulary.Namespace.Core
 import amf.core.vocabulary.ValueType
 
 object CorrelationIdModel extends DomainElementModel with DescriptionField {
 
-  val Location = Field(Str,
-                       ApiContract + "location",
-                       ModelDoc(ModelVocabularies.ApiContract,
-                                "location",
-                                "structural location of the information where the id is located"))
+  val Location = Field(
+    Str,
+    Core + "location",
+    ModelDoc(ModelVocabularies.Core, "location", "structural location of the information where the id is located"))
 
-  override val `type`: List[ValueType] = ApiContract + "CorrelationId" :: DomainElementModel.`type`
+  override val `type`: List[ValueType] = Core + "CorrelationId" :: DomainElementModel.`type`
 
   override val fields: List[Field] = Description :: Location :: DomainElementModel.fields
 
   override def modelInstance = ???
 
   override val doc: ModelDoc = ModelDoc(
-    ModelVocabularies.ApiContract,
+    ModelVocabularies.Core,
     "CorrelationId",
     "Model defining an identifier that can used for message tracing and correlation"
   )
