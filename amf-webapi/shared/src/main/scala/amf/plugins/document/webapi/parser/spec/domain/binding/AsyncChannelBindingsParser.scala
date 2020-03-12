8d0c7c97b0b6bdafa41016174ca671e0fcc9d971
Hernan Najles <hernan.naj+github@gmail.com>
2020-03-12 14:12:54 -0300

    Add lexical info for queue and route key fields

--- amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/parser/spec/domain/binding/AsyncChannelBindingsParser.scala
+++ amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/parser/spec/domain/binding/AsyncChannelBindingsParser.scala
@@ -70,7 +70,7 @@ case class AsyncChannelBindingsParser(entryLike: YMapEntryLike, parent: String)(
     ctx.closedShape(binding.id, map, "amqpIsExchangeChannelBinding")
     map.key(
       "exchange", { entry =>
-        val exchange    = Amqp091ChannelExchange(Annotations(entry)).adopted(binding.id)
+        val exchange    = Amqp091ChannelExchange(Annotations(entry.key)).adopted(binding.id)
         val exchangeMap = entry.value.as[YMap]
 
         exchangeMap.key("name", Amqp091ChannelExchangeModel.Name in exchange) // TODO validate maxlength 255
@@ -82,7 +82,7 @@ case class AsyncChannelBindingsParser(entryLike: YMapEntryLike, parent: String)(
 
         ctx.closedShape(exchange.id, exchangeMap, "amqpExchangeChannelBinding")
 
-        binding.set(Amqp091ChannelBindingModel.Exchange, exchange)
+        binding.set(Amqp091ChannelBindingModel.Exchange, exchange, Annotations(entry))
       }
     )
   }
@@ -91,7 +91,7 @@ case class AsyncChannelBindingsParser(entryLike: YMapEntryLike, parent: String)(
     ctx.closedShape(binding.id, map, "amqpIsQueueChannelBinding")
     map.key(
       "queue", { entry =>
-        val queue    = Amqp091Queue(Annotations(entry)).adopted(binding.id)
+        val queue    = Amqp091Queue(Annotations(entry.value)).adopted(binding.id)
         val queueMap = entry.value.as[YMap]
 
         queueMap.key("name", Amqp091QueueModel.Name in queue) // TODO validate maxlength 255
@@ -103,7 +103,7 @@ case class AsyncChannelBindingsParser(entryLike: YMapEntryLike, parent: String)(
 
         ctx.closedShape(queue.id, queueMap, "amqpQueueChannelBinding")
 
-        binding.set(Amqp091ChannelBindingModel.Queue, queue)
+        binding.set(Amqp091ChannelBindingModel.Queue, queue, Annotations(entry))
       }
     )
   }
