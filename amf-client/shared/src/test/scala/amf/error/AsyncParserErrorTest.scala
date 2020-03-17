ceabab93fe9dc083702cd29095cbd27e49332583
Tomas Fernandez <tfernandez@mulesoft.com>
2020-03-17 12:09:59 -0300

    Added IllegalTypeHandler to YMapEntryLike

---
+++ amf-client/shared/src/test/scala/amf/error/AsyncParserErrorTest.scala
@@ -0,0 +1,28 @@
+package amf.error
+
+import amf.core.model.document.BaseUnit
+import amf.core.parser.Range
+import amf.core.parser.errorhandler.ParserErrorHandler
+import amf.core.remote.AsyncYamlHint
+import amf.facades.AMFCompiler
+
+import scala.concurrent.Future
+
+class AsyncParserErrorTest extends ParserErrorTest {
+
+  override protected val basePath: String = "file://amf-client/shared/src/test/resources/parser-results/async"
+
+  override protected def build(eh: ParserErrorHandler, file: String): Future[BaseUnit] =
+    AMFCompiler(file, platform, AsyncYamlHint, eh = eh).build()
+
+  test("Parameter is not YMap") {
+    validate(
+      "/error/invalid-single-parameter-parse.yaml",
+      violation => {
+        violation.level should be("Violation")
+        violation.message should be("YAML map expected")
+        violation.position.map(_.range) should be(Some(Range((8, 14), (8, 27))))
+      }
+    )
+  }
+}
