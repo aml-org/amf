2efb472067de88337a35e9bea43a9593f7be8b38
Martin Gutierrez <martin.gutierrez@mulesoft.com>
2020-05-06 15:43:53 -0300

    Remove WebApiAnnotations as it is not being used.

--- amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/annotations/Annotations.scala
+++ amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/annotations/Annotations.scala
@@ -1,11 +1,7 @@
 package amf.plugins.document.webapi.annotations
 
-import amf.core.annotations._
 import amf.core.model.domain._
 import amf.core.parser.Range
-import amf.core.remote._
-import amf.plugins.domain.shapes.annotations.ParsedFromTypeExpression
-import amf.plugins.domain.webapi.annotations.ParentEndPoint
 
 case class ParsedJSONSchema(rawText: String) extends EternalSerializedAnnotation {
   override val name: String  = "parsed-json-schema"
@@ -127,16 +123,6 @@ object LocalLinkPath extends AnnotationGraphLoader {
 
 case class InlineDefinition() extends Annotation
 
-/*
-case class DomainElementReference(name: String, ref: Option[DomainEntity]) extends SerializableAnnotation {
-  override val value: String = name
-}
-
-object DomainElementReference extends AnnotationGraphLoader {
-  override def unparse(annotatedValue: String, objects: Map[String, AmfElement]) = ???
-}
- */
-
 case class EndPointBodyParameter() extends Annotation
 
 case class DefaultPayload() extends Annotation
@@ -157,49 +143,3 @@ case class ReferencedElement(parsedUrl: String, referenced: DomainElement) exten
 case class Inferred() extends Annotation
 
 case class CollectionFormatFromItems() extends Annotation
-
-object WebApiAnnotations {
-
-  private def sourceVendor(value: String, objects: Map[String, AmfElement]) = {
-    value match {
-      case Vendor(vendor) => SourceVendor(vendor)
-      case _              => SourceVendor(Vendor(value))
-    }
-  }
-
-  private def parentEndPoint(value: String, objects: Map[String, AmfElement]) = {
-    ParentEndPoint.unparse(value, objects).get
-  }
-
-  private def singleValueArray(value: String, objects: Map[String, AmfElement]) = {
-    SingleValueArray()
-  }
-
-  private def aliases(value: String, objects: Map[String, AmfElement]) = {
-    Aliases.unparse(value, objects)
-  }
-
-  private def parsedJsonSchema(value: String, objects: Map[String, AmfElement]) = {
-    ParsedJSONSchema(value)
-  }
-
-  private def parsedRamlDatatype(value: String, objects: Map[String, AmfElement]) = {
-    ParsedRamlDatatype(value)
-  }
-
-  private def declaredElement(value: String, objects: Map[String, AmfElement]) = {
-    DeclaredElement()
-  }
-
-  private def typeExpression(value: String, objects: Map[String, AmfElement]) = {
-    ParsedFromTypeExpression(value)
-  }
-
-  private def synthesizedField(value: String, objects: Map[String, AmfElement]) = {
-    SynthesizedField()
-  }
-
-  private def lexical(value: String, objects: Map[String, AmfElement]) = {
-    LexicalInformation(Range.apply(value))
-  }
-}
