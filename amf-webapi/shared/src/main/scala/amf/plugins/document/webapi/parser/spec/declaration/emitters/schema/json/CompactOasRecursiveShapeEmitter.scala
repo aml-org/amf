11df0f80bd7242828614e8cfbae6ab9596575565
Agustin Bettati <bettatiagustin@gmail.com>
2020-05-18 15:17:51 -0300

    APIMF-2033: Handle missing closures with compact emission queue

--- amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/parser/spec/declaration/emitters/schema/json/CompactOasRecursiveShapeEmitter.scala
+++ amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/parser/spec/declaration/emitters/schema/json/CompactOasRecursiveShapeEmitter.scala
@@ -12,9 +12,14 @@ class CompactOasRecursiveShapeEmitter(recursive: RecursiveShape,
     extends OasRecursiveShapeEmitter(recursive, ordering, schemaPath) {
 
   override def emit(b: EntryBuilder): Unit = {
-    val label = recursive.fixpoint.option().flatMap { fixpoint =>
-      spec.definitionsQueue.labelOfShape(fixpoint)
-    }
+    val label = recursive.fixpoint
+      .option()
+      .flatMap { fixpoint =>
+        spec.definitionsQueue.labelOfShape(fixpoint)
+      }
+      .orElse {
+        recursive.fixpointTarget.map(spec.definitionsQueue.enqueue)
+      }
     label match {
       case Some(name) => b.entry("$ref", s"#${spec.schemasDeclarationsPath}$name")
       case None       => super.emit(b) // default to normal behaviour
