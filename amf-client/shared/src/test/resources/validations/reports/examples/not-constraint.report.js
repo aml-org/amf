c7c028aee3f0866170462716d96d6b7ceb6d728b
Tomas Fernandez <tfernandez@mulesoft.com>
2020-07-30 14:33:54 -0300

    APIMF-2311: fixed duplicate ids for oas 3 unnamed examples defined in several responses

--- amf-client/shared/src/test/resources/validations/reports/examples/not-constraint.report.js
+++ amf-client/shared/src/test/resources/validations/reports/examples/not-constraint.report.js
@@ -8,7 +8,7 @@ Level: Warning
 - Source: http://a.ml/vocabularies/amf/validation#example-validation-error
   Message: should NOT be valid
   Level: Warning
-  Target: file://amf-client/shared/src/test/resources/validations/oas3/not-constraint.json#/web-api/end-points/%2Ftest/get/200/application%2Fjson/any/schema/example/invalid
-  Property: file://amf-client/shared/src/test/resources/validations/oas3/not-constraint.json#/web-api/end-points/%2Ftest/get/200/application%2Fjson/any/schema/example/invalid
+  Target: file://amf-client/shared/src/test/resources/validations/oas3/not-constraint.json#/web-api/end-points/%2Ftest/get/200/application%2Fjson/example/invalid
+  Property: file://amf-client/shared/src/test/resources/validations/oas3/not-constraint.json#/web-api/end-points/%2Ftest/get/200/application%2Fjson/example/invalid
   Position: Some(LexicalInformation([(25,29)-(25,42)]))
   Location: file://amf-client/shared/src/test/resources/validations/oas3/not-constraint.json
