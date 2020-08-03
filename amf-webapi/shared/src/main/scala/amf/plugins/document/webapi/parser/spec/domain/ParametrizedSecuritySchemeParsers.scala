3f0bf7404f50ba89d244c02fd7920f8385feb543
Agustin Bettati <bettatiagustin@gmail.com>
2020-08-03 15:27:32 -0300

    APIMF-2321: warning required fields for OAuth 2.0 grant types

--- amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/parser/spec/domain/ParametrizedSecuritySchemeParsers.scala
+++ amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/parser/spec/domain/ParametrizedSecuritySchemeParsers.scala
@@ -3,25 +3,32 @@ package amf.plugins.document.webapi.parser.spec.domain
 import amf.core.annotations.NullSecurity
 import amf.core.model.domain.DomainElement
 import amf.core.parser.{Annotations, _}
-import amf.core.utils.{Lazy, AmfStrings}
-import amf.plugins.document.webapi.contexts.WebApiContext
+import amf.core.utils.{AmfStrings, Lazy}
+import amf.plugins.document.webapi.contexts.parser.raml.RamlWebApiContext
+import amf.plugins.document.webapi.parser.spec.{SpecField, SpecNode}
 import amf.plugins.document.webapi.parser.spec.common.WellKnownAnnotation.isRamlAnnotation
 import amf.plugins.document.webapi.parser.spec.common._
 import amf.plugins.document.webapi.vocabulary.VocabularyMappings
 import amf.plugins.domain.webapi.metamodel.security._
 import amf.plugins.domain.webapi.models.security._
-import amf.validations.ParserSideValidations.{UnknownScopeErrorSpecification, UnknownSecuritySchemeErrorSpecification}
+import amf.validations.ParserSideValidations.{
+  MissingRequiredFieldForGrantType,
+  UnknownScopeErrorSpecification,
+  UnknownSecuritySchemeErrorSpecification
+}
 import org.yaml.model._
+
+import scala.collection.mutable
 
 object RamlParametrizedSecuritySchemeParser {
   def parse(producer: String => ParametrizedSecurityScheme)(node: YNode)(
-      implicit ctx: WebApiContext): ParametrizedSecurityScheme = {
+      implicit ctx: RamlWebApiContext): ParametrizedSecurityScheme = {
     RamlParametrizedSecuritySchemeParser(node, producer).parse()
   }
 }
 
 case class RamlParametrizedSecuritySchemeParser(node: YNode, producer: String => ParametrizedSecurityScheme)(
-    implicit ctx: WebApiContext) {
+    implicit ctx: RamlWebApiContext) {
   def parse(): ParametrizedSecurityScheme = node.tagType match {
     case YType.Null => producer("null").add(Annotations(node) += NullSecurity())
     case YType.Map =>
@@ -76,13 +83,13 @@ case class RamlParametrizedSecuritySchemeParser(node: YNode, producer: String =>
 }
 
 object RamlSecuritySettingsParser {
-  def parse(scheme: SecurityScheme)(node: YNode)(implicit ctx: WebApiContext): Settings = {
-    RamlSecuritySettingsParser(node.as[YMap], scheme.`type`.value(), scheme).parse()
+  def parse(scheme: SecurityScheme)(node: YNode)(implicit ctx: RamlWebApiContext): Settings = {
+    ctx.factory.securitySettingsParser(node.as[YMap], scheme.`type`.value(), scheme).parse()
   }
 }
 
 case class RamlSecuritySettingsParser(map: YMap, `type`: String, scheme: DomainElement with WithSettings)(
-    implicit val ctx: WebApiContext)
+    implicit val ctx: RamlWebApiContext)
     extends SpecParserOps {
   def parse(): Settings = {
     val result = `type` match {
@@ -120,7 +127,7 @@ case class RamlSecuritySettingsParser(map: YMap, `type`: String, scheme: DomainE
     dynamicSettings(settings, "name", "in")
   }
 
-  private def oauth2() = {
+  protected def oauth2(): OAuth2Settings = {
     val settings = scheme.withOAuth2Settings()
     val flow     = new Lazy[OAuth2Flow](() => OAuth2Flow(map).adopted(settings.id))
 
@@ -161,6 +168,7 @@ case class RamlSecuritySettingsParser(map: YMap, `type`: String, scheme: DomainE
     flow.option.foreach(f => settings.setArray(OAuth2SettingsModel.Flows, Seq(f)))
 
     dynamicSettings(settings, "authorizationUri", "accessTokenUri", "authorizationGrants", "scopes")
+    settings
   }
 
   private def oauth1() = {
@@ -177,4 +185,48 @@ case class RamlSecuritySettingsParser(map: YMap, `type`: String, scheme: DomainE
   private def isValidScope(maybeFlow: Option[OAuth2Flow], scope: String): Boolean =
     maybeFlow.exists(flow => flow.scopes.isEmpty || flow.scopes.map(_.name.value()).contains(scope))
 
+}
+
+class Raml10SecuritySettingsParser(map: YMap, `type`: String, scheme: DomainElement with WithSettings)(
+    implicit override val ctx: RamlWebApiContext)
+    extends RamlSecuritySettingsParser(map, `type`, scheme) {
+
+  override protected def oauth2(): OAuth2Settings = {
+    val settings = super.oauth2()
+    validateRequiredFields(settings)
+    settings
+  }
+
+  private def validateRequiredFields(settings: OAuth2Settings): Unit = {
+    val grants            = settings.authorizationGrants.flatMap(_.option())
+    val requiredFields    = requiredFieldsWithGrant(grants)
+    val keys: Seq[String] = map.entries.map(ctx.getEntryKey)
+    requiredFields.foreach {
+      case (requiredField, grant) =>
+        if (!keys.contains(requiredField.name))
+          ctx.eh.warning(MissingRequiredFieldForGrantType,
+                         settings.id,
+                         s"'${requiredField.name}' is required when '$grant' grant type is used",
+                         map)
+    }
+  }
+
+  private val oauth2SpecFields = Map(
+    "authorization_code" -> SpecNode(requiredFields = Set(SpecField("accessTokenUri"), SpecField("authorizationUri"))),
+    "password"           -> SpecNode(requiredFields = Set(SpecField("accessTokenUri"))),
+    "client_credentials" -> SpecNode(requiredFields = Set(SpecField("accessTokenUri"))),
+    "implicit"           -> SpecNode(requiredFields = Set(SpecField("authorizationUri")))
+  )
+
+  private def requiredFieldsWithGrant(grants: Seq[String]): mutable.Map[SpecField, String] = {
+    val requiredFields = mutable.Map.empty[SpecField, String]
+    grants.foreach { grantType =>
+      oauth2SpecFields.get(grantType).map(_.requiredFields).map { fieldsForGrant =>
+        fieldsForGrant.foreach { requiredField =>
+          requiredFields += requiredField -> grantType
+        }
+      }
+    }
+    requiredFields
+  }
 }
