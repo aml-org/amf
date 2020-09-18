1b75109d8907a93eb2e065f55a5ededc66e17711
Nicolas Schejtman <nschejtman93@gmail.com>
2020-09-18 11:38:44 -0300

    Create new amf-custom-validation module

---
+++ amf-custom-validation/shared/src/main/scala/amf/plugins/features/validation/custom/model/Model.scala
@@ -0,0 +1,364 @@
+package amf.plugins.features.validation.custom.model
+
+import amf.ProfileName
+import amf.core.validation.SeverityLevels
+import amf.core.validation.core._
+import amf.core.validation.model.PropertyPathParser
+import amf.core.vocabulary.Namespace
+import amf.plugins.document.vocabularies.model.domain.DialectDomainElement
+
+import scala.collection.mutable
+
+trait DialectWrapper {
+
+  def expand(value: String, prefixes: mutable.Map[String, String]): String = {
+    val valuePrefix = value.split("\\.").head
+    prefixes.get(valuePrefix) match {
+      case Some(uri) => value.replace(valuePrefix + ".", uri)
+      case _         => value
+    }
+  }
+
+  def extractString(node: DialectDomainElement, property: String): Option[String] = {
+    node.definedBy
+      .propertiesMapping()
+      .find(_.name().value() == property)
+      .flatMap(p => node.literalProperty(p.toField))
+      .map(_.toString)
+  }
+
+  def extractInt(node: DialectDomainElement, property: String): Option[Int] = {
+    node.definedBy
+      .propertiesMapping()
+      .find(_.name().value() == property)
+      .flatMap(p => node.literalProperty(p.toField))
+      .map(_.asInstanceOf[Int])
+  }
+
+  def extractStrings(node: DialectDomainElement, property: String): Seq[String] = {
+    node.definedBy
+      .propertiesMapping()
+      .find(_.name().value() == property)
+      .map(p => node.literalProperties(p.toField).map(_.toString))
+      .getOrElse(Nil)
+  }
+
+  def mapEntities[T](node: DialectDomainElement, property: String, f: DialectDomainElement => T): Seq[T] = {
+    node.definedBy
+      .propertiesMapping()
+      .find(_.name().value() == property)
+      .map(p => node.objectCollectionProperty(p.toField).map(f))
+      .getOrElse(Nil)
+  }
+
+  def mapIndexedEntities[T](node: DialectDomainElement,
+                            property: String,
+                            f: (DialectDomainElement, Int) => T): Seq[T] = {
+    node.definedBy.propertiesMapping().find(_.name().value() == property).map { p =>
+      node.objectCollectionProperty(p.toField).zipWithIndex.map {
+        case (dialectDomainElement, i) =>
+          f(dialectDomainElement, i)
+      }
+    } getOrElse (Nil)
+  }
+
+  def mapEntity[T](node: DialectDomainElement, property: String, f: DialectDomainElement => T): Option[T] =
+    node.definedBy
+      .propertiesMapping()
+      .find(_.name().value() == property)
+      .flatMap(p => node.objectProperty(p.toField).map(f))
+
+  def prefixes(node: DialectDomainElement): mutable.Map[String, String] = {
+    val prefixMap: mutable.Map[String, String] = mutable.HashMap()
+    node.definedBy.propertiesMapping().find(_.name().value() == "prefixes").map(_.toField) match {
+      case Some(prefixesProperty) =>
+        node.objectCollectionProperty(prefixesProperty).foreach { prefixEntity =>
+          val prefix    = extractString(prefixEntity, "prefix").getOrElse("")
+          val prefixUri = extractString(prefixEntity, "uri").getOrElse("")
+          prefixMap.put(prefix, prefixUri)
+        }
+      case _ =>
+    }
+    Namespace.ns.foreach {
+      case (p, v) =>
+        prefixMap.get(p) match {
+          case Some(x) => // ignore
+          case _       => prefixMap.put(p, v.base)
+        }
+    }
+    prefixMap
+  }
+
+  def mandatory[T](message: String, x: Option[T]): T = x match {
+    case Some(e) => e
+    case None    => throw new Exception(s"Missing mandatory property '$message'")
+  }
+
+}
+
+object ParsedFunctionConstraint extends DialectWrapper {
+  def apply(node: DialectDomainElement): FunctionConstraint = {
+    FunctionConstraint(
+      message = extractString(node, "message"),
+      code = extractString(node, "code"),
+      libraries = extractStrings(node, "libraries"),
+      functionName = extractString(node, "functionName"),
+    )
+  }
+}
+
+object ParsedQueryConstraint extends DialectWrapper {
+
+  def apply(query: String, prefixes: mutable.Map[String, String]): QueryConstraint = {
+    val ns = prefixes.toMap
+    QueryConstraint(
+      prefixes = ns,
+      query = query
+    )
+  }
+
+}
+
+object ParsedPropertyConstraint extends DialectWrapper {
+  val validationNs = "http://a.ml/vocabularies/amf-validation#"
+
+  def apply(node: DialectDomainElement,
+            prefixes: mutable.Map[String, String],
+            nameForNestedValidation: String,
+            message: Option[String] = None,
+            nested: String): (PropertyConstraint, Seq[ValidationSpecification]) = {
+    val name = mandatory("name in property constraint", extractString(node, "name"))
+    val path = PropertyPathParser(name, s => expand(s, prefixes))
+
+    val (nestedNode, nestedConstraints) = mapEntity(
+      node,
+      "nested",
+      ParsedValidationSpecification(_,
+                                    prefixes,
+                                    Some(s"${nameForNestedValidation}_nested_node"),
+                                    message,
+                                    None,
+                                    Some(nested))).map { case (v, nested) => (Seq(v), nested) } getOrElse ((Nil, Nil))
+    val atLeastTuple = mapEntity(
+      node,
+      "atLeast",
+      ParsedQualifiedValidationSpecification(_,
+                                             prefixes,
+                                             Some(s"${nameForNestedValidation}_${name}_atLeast_"),
+                                             message,
+                                             None,
+                                             Some(nested)))
+    val atMostTuple = mapEntity(
+      node,
+      "atMost",
+      ParsedQualifiedValidationSpecification(_,
+                                             prefixes,
+                                             Some(s"${nameForNestedValidation}_${name}_atLeast_"),
+                                             message,
+                                             None,
+                                             Some(nested)))
+
+    val property = PropertyConstraint(
+      ramlPropertyId = expand(mandatory("ramlID in property constraint", Some(name)), prefixes),
+      name = name,
+      path = Some(path),
+      message = extractString(node, "message"),
+      pattern = extractString(node, "pattern"),
+      maxCount = extractString(node, "maxCount"),
+      minCount = extractString(node, "minCount"),
+      maxLength = extractString(node, "maxLength"),
+      minLength = extractString(node, "minLength"),
+      maxExclusive = extractString(node, "maxExclusive"),
+      minExclusive = extractString(node, "minExclusive"),
+      maxInclusive = extractString(node, "maxInclusive"),
+      minInclusive = extractString(node, "minInclusive"),
+      in = extractStrings(node, "in"),
+      node = nestedNode.headOption.map(_.id),
+      equalToProperty = extractString(node, "equalsToProperty").map(s => expand(s, prefixes)),
+      disjointWithProperty = extractString(node, "disjointWithProperty").map(s => expand(s, prefixes)),
+      lessThanProperty = extractString(node, "lessThanProperty").map(s => expand(s, prefixes)),
+      lessThanOrEqualsToProperty = extractString(node, "lessThanOrEqualsToProperty").map(s => expand(s, prefixes)),
+//      `class`             = extractString(node, "class").map(s => Seq(expand(s, prefixes))).getOrElse(Nil),
+      atLeast = atLeastTuple.map { t =>
+        (t._1, t._2.id)
+      },
+      atMost = atMostTuple.map { t =>
+        (t._1, t._2.id)
+      },
+    )
+
+    val atLeastAcc = atLeastTuple.map(t => Seq(t._2)).getOrElse(Nil) ++ atLeastTuple.map(_._3).getOrElse(Nil)
+    val atLMostAcc = atMostTuple.map(t => Seq(t._2)).getOrElse(Nil) ++ atMostTuple.map(_._3).getOrElse(Nil)
+
+    (property, nestedNode ++ nestedConstraints ++ atLeastAcc ++ atLMostAcc)
+  }
+}
+object ParsedQualifiedValidationSpecification extends DialectWrapper {
+
+  def apply(node: DialectDomainElement,
+            prefixes: mutable.Map[String, String],
+            nameForNestedValidation: Option[String] = None,
+            message: Option[String] = None,
+            targetClassForNestedValidation: Option[Seq[String]] = None,
+            nested: Option[String] = None): (Int, ValidationSpecification, Seq[ValidationSpecification]) = {
+    val count: Int = mandatory("count missing in qualified validation specification", extractInt(node, "count"))
+    val (validation, nestedValidations) = mapEntity(
+      node,
+      "validation",
+      ParsedValidationSpecification(_, prefixes, nameForNestedValidation, message, None, nested)).get
+
+    (count, validation, nestedValidations)
+  }
+}
+
+object ParsedValidationSpecification extends DialectWrapper {
+
+  def apply(node: DialectDomainElement,
+            prefixes: mutable.Map[String, String],
+            nameForNestedValidation: Option[String] = None,
+            message: Option[String] = None,
+            targetClassForNestedValidation: Option[Seq[String]] = None,
+            nested: Option[String] = None): (ValidationSpecification, Seq[ValidationSpecification]) = {
+    val name: String =
+      nameForNestedValidation.getOrElse(mandatory("name in validation specification", extractString(node, "name")))
+    val targetClasses: Seq[String] =
+      targetClassForNestedValidation.getOrElse(extractStrings(node, "targetClass").map(expand(_, prefixes)))
+    val finalMessage: String =
+      message.getOrElse(extractString(node, "message").getOrElse(s"Unsatisfied constraint ${name}"))
+    val finalNested: Option[String] = Some(nested.getOrElse(name))
+
+    val propsTuples: Seq[(PropertyConstraint, Seq[ValidationSpecification])] =
+      mapIndexedEntities(node, "propertyConstraints", {
+        case (e, i) =>
+          ParsedPropertyConstraint(e, prefixes, name + s"_prop${i}", Some(finalMessage), finalNested.get)
+      })
+    val propertyConstraints = propsTuples.map(_._1)
+    val nestedProperties    = propsTuples.flatMap(_._2)
+
+    val additionalPropertyConstraints: Seq[PropertyConstraint] =
+      extractStrings(node, "classConstraints").map(s => expand(s, prefixes)).zipWithIndex.map {
+        case (id, i) =>
+          PropertyConstraint(
+            ramlPropertyId = expand("rdf.type", prefixes),
+            name = s"rdf.type_$i",
+            path = Some(PropertyPathParser("rdf.type", s => expand(s, prefixes))),
+            value = Some(expand(id, prefixes))
+          )
+      }
+
+    // optional query constraint
+    val queryConstraint = extractString(node, "query").map { query =>
+      ParsedQueryConstraint(query, prefixes)
+    }
+
+    // the base validation
+    val base = ValidationSpecification(
+      name = name,
+      message = finalMessage,
+      targetClass = targetClasses,
+      propertyConstraints = propertyConstraints ++ additionalPropertyConstraints,
+      functionConstraint = mapEntity(node, "functionConstraint", ParsedFunctionConstraint.apply),
+      nested = nested,
+      query = queryConstraint
+    )
+
+    // compute nested shapes
+    val (andConstraints, nestedAndConstraints) = collectNestedValidations(
+      mapIndexedEntities(
+        node,
+        "and", {
+          case (n: DialectDomainElement, i: Int) =>
+            ParsedValidationSpecification(n, prefixes, Some(s"${name}_and_${i}"), Some(finalMessage), None, finalNested)
+        }
+      ))
+    val (orConstraints, nestedOrConstraints) = collectNestedValidations(
+      mapIndexedEntities(
+        node,
+        "or", {
+          case (n: DialectDomainElement, i: Int) =>
+            ParsedValidationSpecification(n, prefixes, Some(s"${name}_or_${i}"), Some(finalMessage), None, finalNested)
+        }
+      ))
+    val (xoneConstraints, xoneNestedConstraints) = collectNestedValidations(
+      mapIndexedEntities(
+        node,
+        "xone", {
+          case (n: DialectDomainElement, i: Int) =>
+            ParsedValidationSpecification(n,
+                                          prefixes,
+                                          Some(s"${name}_xone_${i}"),
+                                          Some(finalMessage),
+                                          None,
+                                          finalNested)
+        }
+      ))
+
+    val (notConstraints, notNestedConstraints) = mapEntity(
+      node,
+      "not",
+      ParsedValidationSpecification(_, prefixes, Some(s"${name}_not"), Some(finalMessage), None, finalNested)).map {
+      case (v, nested) => (Seq(v), nested)
+    } getOrElse ((Nil, Nil))
+
+    val allNested = nestedAndConstraints ++ nestedOrConstraints ++ xoneNestedConstraints ++ notNestedConstraints ++ nestedProperties
+
+    val finalValidation = base.copy(
+      andConstraints = andConstraints.map(_.id),
+      unionConstraints = orConstraints.map(_.id),
+      xoneConstraints = xoneConstraints.map(_.id),
+      notConstraint = notConstraints.headOption.map(_.id)
+    )
+    (finalValidation, andConstraints ++ orConstraints ++ xoneConstraints ++ notConstraints ++ allNested)
+  }
+
+  protected def collectNestedValidations(
+      nestedValidations: Seq[(ValidationSpecification, Seq[ValidationSpecification])])
+    : (Seq[ValidationSpecification], Seq[ValidationSpecification]) = {
+    nestedValidations.foldLeft((Seq[ValidationSpecification](), Seq[ValidationSpecification]())) {
+      case ((validationsAcc, nestedAcc), (validation, nested)) =>
+        (validationsAcc ++ Seq(validation), nestedAcc ++ nested)
+    }
+  }
+}
+
+object ParsedValidationProfile extends DialectWrapper {
+
+  def apply(node: DialectDomainElement): ValidationProfile = {
+    val prfx = prefixes(node)
+    val severityMapping = SeverityMapping()
+      .set(extractStrings(node, "violation"), SeverityLevels.VIOLATION)
+      .set(extractStrings(node, "info"), SeverityLevels.INFO)
+      .set(extractStrings(node, "warning"), SeverityLevels.WARNING)
+      .disable(extractStrings(node, "disabled"))
+    ValidationProfile(
+      name = ProfileName(mandatory("profile in validation profile", extractString(node, "profile"))),
+      baseProfile = extractString(node, "extends").map(ProfileName.apply),
+      severities = severityMapping,
+      validations =
+        collectValidations(mapEntities(node, "validations", (v) => ParsedValidationSpecification(deref(v), prfx))),
+      prefixes = prfx
+    )
+  }
+
+  protected def deref(v: DialectDomainElement): DialectDomainElement = {
+    v.linkTarget match {
+      case Some(linked: DialectDomainElement) =>
+        val nameFieldEntry = v.fields.fields().find(_.field.value.name == "name").get
+        val nameValue      = nameFieldEntry.scalar.value.asInstanceOf[String]
+        linked.set(nameFieldEntry.field, nameValue)
+        linked
+      case _ =>
+        v
+
+    }
+  }
+
+  protected def collectValidations(
+      nestedValidations: Seq[(ValidationSpecification, Seq[ValidationSpecification])]): Seq[ValidationSpecification] = {
+    nestedValidations.foldLeft(Seq[ValidationSpecification]()) {
+      case (validationsAcc, (validation, nested)) =>
+        validationsAcc ++ Seq(validation) ++ nested
+    }
+  }
+
+}
--- build.sbt
+++ build.sbt
@@ -17,25 +17,23 @@ publish := {}
 
 jsEnv := new org.scalajs.jsenv.nodejs.NodeJSEnv()
 
-lazy val sonarUrl = sys.env.getOrElse("SONAR_SERVER_URL", "Not found url.")
+lazy val sonarUrl   = sys.env.getOrElse("SONAR_SERVER_URL", "Not found url.")
 lazy val sonarToken = sys.env.getOrElse("SONAR_SERVER_TOKEN", "Not found token.")
-lazy val branch = sys.env.getOrElse("BRANCH_NAME", "develop")
+lazy val branch     = sys.env.getOrElse("BRANCH_NAME", "develop")
 
 //enablePlugins(ScalaJSBundlerPlugin)
 
 sonarProperties ++= Map(
-  "sonar.login" -> sonarToken,
-  "sonar.projectKey" -> "mulesoft.amf",
-  "sonar.projectName" -> "AMF",
-  "sonar.projectVersion" -> versions("amf.webapi"),
-
-  "sonar.sourceEncoding" -> "UTF-8",
-  "sonar.github.repository" -> "mulesoft/amf",
-  "sonar.branch.name" -> branch,
-
+  "sonar.login"                      -> sonarToken,
+  "sonar.projectKey"                 -> "mulesoft.amf",
+  "sonar.projectName"                -> "AMF",
+  "sonar.projectVersion"             -> versions("amf.webapi"),
+  "sonar.sourceEncoding"             -> "UTF-8",
+  "sonar.github.repository"          -> "mulesoft/amf",
+  "sonar.branch.name"                -> branch,
   "sonar.scala.coverage.reportPaths" -> "amf-client/jvm/target/scala-2.12/scoverage-report/scoverage.xml,amf-webapi/jvm/target/scala-2.12/scoverage-report/scoverage.xml,amf-validation/jvm/target/scala-2.12/scoverage-report/scoverage.xml",
-  "sonar.sources" -> "amf-client/shared/src/main/scala,amf-webapi/shared/src/main/scala,amf-validation/shared/src/main/scala",
-  "sonar.tests" -> "amf-client/shared/src/test/scala"
+  "sonar.sources"                    -> "amf-client/shared/src/main/scala,amf-webapi/shared/src/main/scala,amf-validation/shared/src/main/scala",
+  "sonar.tests"                      -> "amf-client/shared/src/test/scala"
 )
 
 val settings = Common.settings ++ Common.publish ++ Seq(
@@ -60,10 +58,9 @@ lazy val workspaceDirectory: File =
 val amfAmlVersion = versions("amf.aml")
 
 lazy val amfAmlJVMRef = ProjectRef(workspaceDirectory / "amf-aml", "amlJVM")
-lazy val amfAmlJSRef = ProjectRef(workspaceDirectory / "amf-aml", "amlJS")
+lazy val amfAmlJSRef  = ProjectRef(workspaceDirectory / "amf-aml", "amlJS")
 lazy val amfAmlLibJVM = "com.github.amlorg" %% "amf-aml" % amfAmlVersion
-lazy val amfAmlLibJS = "com.github.amlorg" %% "amf-aml_sjs0.6" % amfAmlVersion
-
+lazy val amfAmlLibJS  = "com.github.amlorg" %% "amf-aml_sjs0.6" % amfAmlVersion
 
 lazy val defaultProfilesGenerationTask = TaskKey[Unit](
   "defaultValidationProfilesGeneration",
@@ -73,16 +70,17 @@ lazy val defaultProfilesGenerationTask = TaskKey[Unit](
   * AMF-WebAPI
   * ********************************************* */
 lazy val webapi = crossProject(JSPlatform, JVMPlatform)
-  .settings(Seq(
-    name := "amf-webapi"
-  ))
+  .settings(
+    Seq(
+      name := "amf-webapi"
+    ))
   .in(file("./amf-webapi"))
   .settings(settings)
   .jvmSettings(
-    libraryDependencies += "org.scala-js"           %% "scalajs-stubs"          % scalaJSVersion % "provided",
-    libraryDependencies += "org.scala-lang.modules" % "scala-java8-compat_2.12" % "0.8.0",
-    libraryDependencies += "org.json4s"             %% "json4s-native"         % "3.5.4",
-    libraryDependencies += "com.github.everit-org.json-schema" % "org.everit.json.schema" % "1.9.2",
+    libraryDependencies += "org.scala-js"                      %% "scalajs-stubs"          % scalaJSVersion % "provided",
+    libraryDependencies += "org.scala-lang.modules"            % "scala-java8-compat_2.12" % "0.8.0",
+    libraryDependencies += "org.json4s"                        %% "json4s-native"          % "3.5.4",
+    libraryDependencies += "com.github.everit-org.json-schema" % "org.everit.json.schema"  % "1.9.2",
     artifactPath in (Compile, packageDoc) := baseDirectory.value / "target" / "artifact" / "amf-webapi-javadoc.jar",
     mappings in (Compile, packageBin) += file("amf-webapi.versions") -> "amf-webapi.versions"
   )
@@ -91,38 +89,48 @@ lazy val webapi = crossProject(JSPlatform, JVMPlatform)
     scalaJSModuleKind := ModuleKind.CommonJSModule,
     artifactPath in (Compile, fullOptJS) := baseDirectory.value / "target" / "artifact" / "amf-webapi-module.js",
     scalacOptions += "-P:scalajs:suppressExportDeprecations"
-  ).disablePlugins(SonarPlugin)
+  )
+  .disablePlugins(SonarPlugin)
 
 lazy val webapiJVM = webapi.jvm.in(file("./amf-webapi/jvm")).sourceDependency(amfAmlJVMRef, amfAmlLibJVM)
-lazy val webapiJS  = webapi.js.in(file("./amf-webapi/js")).sourceDependency(amfAmlJSRef, amfAmlLibJS).disablePlugins(SonarPlugin)
+lazy val webapiJS =
+  webapi.js.in(file("./amf-webapi/js")).sourceDependency(amfAmlJSRef, amfAmlLibJS).disablePlugins(SonarPlugin)
 
 /** **********************************************
-  * AMF-Validation
+  * AMF-Custom-Validation
   * ********************************************* */
-lazy val validation = crossProject(JSPlatform, JVMPlatform)
-  .settings(Seq(
-    name := "amf-validation"
-  ))
-  .in(file("./amf-validation"))
+lazy val customValidation = crossProject(JSPlatform, JVMPlatform)
+  .settings(
+    Seq(
+      name := "amf-custom-validation"
+    ))
+  .in(file("./amf-custom-validation"))
   .settings(settings)
   .jvmSettings(
-    libraryDependencies += "org.scala-js"           %% "scalajs-stubs"          % scalaJSVersion % "provided",
-    libraryDependencies += "org.scala-lang.modules" % "scala-java8-compat_2.12" % "0.8.0",
-    libraryDependencies += "org.json4s"             %% "json4s-native"          % "3.5.4",
-    libraryDependencies += "org.apache.jena"        % "apache-jena-libs"        % "3.14.0" pomOnly(),
-    libraryDependencies += "org.apache.jena"        % "jena-shacl"              % "3.14.0",
-    libraryDependencies += "org.apache.commons" % "commons-compress" % "1.19",
-    libraryDependencies += "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.8",
-    artifactPath in (Compile, packageDoc) := baseDirectory.value / "target" / "artifact" / "amf-validation-javadoc.jar"
+    libraryDependencies += "org.scala-js"               %% "scalajs-stubs"          % scalaJSVersion % "provided",
+    libraryDependencies += "org.scala-lang.modules"     % "scala-java8-compat_2.12" % "0.8.0",
+    libraryDependencies += "org.json4s"                 %% "json4s-native"          % "3.5.4",
+    libraryDependencies += "org.apache.jena"            % "apache-jena-libs"        % "3.14.0" pomOnly (),
+    libraryDependencies += "org.apache.jena"            % "jena-shacl"              % "3.14.0",
+    libraryDependencies += "org.apache.commons"         % "commons-compress"        % "1.19",
+    libraryDependencies += "com.fasterxml.jackson.core" % "jackson-databind"        % "2.9.8",
+    artifactPath in (Compile, packageDoc) := baseDirectory.value / "target" / "artifact" / "amf-custom-validation-javadoc.jar"
   )
   .jsSettings(
+    jsDependencies += ProvidedJS / "shacl.js",
+    jsDependencies += ProvidedJS / "ajv.min.js",
     libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.2",
     scalaJSModuleKind := ModuleKind.CommonJSModule,
-    artifactPath in (Compile, fullOptJS) := baseDirectory.value / "target" / "artifact" / "amf-validation-module.js"
-  ).disablePlugins(SonarPlugin)
+    artifactPath in (Compile, fullOptJS) := baseDirectory.value / "target" / "artifact" / "amf-custom-validation-module.js"
+  )
+  .disablePlugins(SonarPlugin)
 
-lazy val validationJVM = validation.jvm.in(file("./amf-validation/jvm")).sourceDependency(amfAmlJVMRef, amfAmlLibJVM)
-lazy val validationJS  = validation.js.in(file("./amf-validation/js")).sourceDependency(amfAmlJSRef, amfAmlLibJS).disablePlugins(SonarPlugin)
+lazy val customValidationJVM =
+  customValidation.jvm.in(file("./amf-custom-validation/jvm")).sourceDependency(amfAmlJVMRef, amfAmlLibJVM)
+lazy val customValidationJS = customValidation.js
+  .in(file("./amf-custom-validation/js"))
+  .sourceDependency(amfAmlJSRef, amfAmlLibJS)
+  .disablePlugins(SonarPlugin)
 
 /** **********************************************
   * AMF Client
@@ -130,17 +138,17 @@ lazy val validationJS  = validation.js.in(file("./amf-validation/js")).sourceDep
 lazy val client = crossProject(JSPlatform, JVMPlatform)
   .settings(name := "amf-client")
   .settings(fullRunTask(defaultProfilesGenerationTask, Compile, "amf.tasks.validations.ValidationProfileExporter"))
-  .dependsOn( webapi, validation)
+  .dependsOn(webapi, customValidation)
   .in(file("./amf-client"))
   .settings(settings)
   .jvmSettings(
-    libraryDependencies += "org.scala-js"           %% "scalajs-stubs"          % scalaJSVersion % "provided",
-    libraryDependencies += "org.reflections" % "reflections" % "0.9.12",
-    libraryDependencies += "org.scala-lang.modules" % "scala-java8-compat_2.12" % "0.8.0",
-    libraryDependencies += "org.json4s"             %% "json4s-native"          % "3.5.4",
-    libraryDependencies += "org.topbraid"           % "shacl"                   % "1.3.0",
-    libraryDependencies += "org.apache.commons" % "commons-compress" % "1.18",
-    libraryDependencies += "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.8",
+    libraryDependencies += "org.scala-js"               %% "scalajs-stubs"          % scalaJSVersion % "provided",
+    libraryDependencies += "org.reflections"            % "reflections"             % "0.9.12",
+    libraryDependencies += "org.scala-lang.modules"     % "scala-java8-compat_2.12" % "0.8.0",
+    libraryDependencies += "org.json4s"                 %% "json4s-native"          % "3.5.4",
+    libraryDependencies += "org.topbraid"               % "shacl"                   % "1.3.0",
+    libraryDependencies += "org.apache.commons"         % "commons-compress"        % "1.18",
+    libraryDependencies += "com.fasterxml.jackson.core" % "jackson-databind"        % "2.9.8",
     mainClass in Compile := Some("amf.Main"),
     packageOptions in (Compile, packageBin) += Package.ManifestAttributes("Automatic-Module-Name" → "org.mule.amf"),
     mappings in (Compile, packageBin) += file("amf-webapi.versions") -> "amf-webapi.versions",
@@ -166,33 +174,15 @@ lazy val client = crossProject(JSPlatform, JVMPlatform)
     }
   )
   .jsSettings(
-    jsDependencies += ProvidedJS / "shacl.js",
-    jsDependencies += ProvidedJS / "ajv.min.js",
     libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.2",
     scalaJSModuleKind := ModuleKind.CommonJSModule,
     artifactPath in (Compile, fullOptJS) := baseDirectory.value / "target" / "artifact" / "amf-client-module.js"
-  ).disablePlugins(SonarPlugin)
+  )
+  .disablePlugins(SonarPlugin)
 
 lazy val clientJVM = client.jvm.in(file("./amf-client/jvm")).sourceDependency(amfAmlJVMRef, amfAmlLibJVM)
-lazy val clientJS  = client.js.in(file("./amf-client/js")).sourceDependency(amfAmlJSRef, amfAmlLibJS).disablePlugins(SonarPlugin)
-
-/** **********************************************
-  * AMF Tools
-  ********************************************** */
-//lazy val tools = crossProject(JVMPlatform)
-//  .settings(Seq(
-//    name := "amf-tools",
-//    fullRunTask(defaultProfilesGenerationTask, Compile, "amf.tasks.validations.ValidationProfileExporter")))
-//  .dependsOn(core, webapi, vocabularies, validation, client % "compile->compile;test->test")
-//  .in(file("./amf-tools"))
-//  .settings(settings)
-//  .jvmSettings(
-//    mainClass in Compile := Some("amf.VocabularyExporter"),
-//    mainClass in assembly := Some("amf.VocabularyExporter"),
-//    assemblyOutputPath in assembly := file(s"./amf-${version.value}.jar")
-//  )
-//
-//lazy val toolsJVM = tools.jvm.in(file("./amf-tools/jvm"))
+lazy val clientJS =
+  client.js.in(file("./amf-client/js")).sourceDependency(amfAmlJSRef, amfAmlLibJS).disablePlugins(SonarPlugin)
 
 // Tasks
 
@@ -210,7 +200,6 @@ addCommandAlias(
 /** **********************************************
   * AMF Runner
   ********************************************** */
-
 lazy val amfRunner = project
   .in(file("./amf-runner"))
   .dependsOn(clientJVM)
