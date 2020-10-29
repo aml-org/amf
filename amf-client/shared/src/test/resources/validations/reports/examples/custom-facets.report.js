987604f2ed68cc5d0074f8fbcd78224af219361a
Nicolas Schejtman <nschejtman93@gmail.com>
2020-10-29 16:31:10 -0300

    APIMF-2556: fix tests
      Fix closed shape validation id in goldens
      Fix included dialects/vocabularie in RAML examples

--- amf-client/shared/src/test/resources/validations/reports/examples/custom-facets.report.js
+++ amf-client/shared/src/test/resources/validations/reports/examples/custom-facets.report.js
@@ -13,7 +13,7 @@ Level: Violation
   Position: Some(LexicalInformation([(13,0)-(15,0)]))
   Location: file://amf-client/shared/src/test/resources/validations/facets/custom-facets.raml
 
-- Source: http://a.ml/vocabularies/amf/aml#closed-shape
+- Source: http://a.ml/vocabularies/amf/parser#closed-shape
   Message: Property 'thisIsWrong' not supported in a RAML 1.0 shape node
   Level: Violation
   Target: file://amf-client/shared/src/test/resources/validations/facets/custom-facets.raml#/declarations/types/scalar/ErroneousType
@@ -21,7 +21,7 @@ Level: Violation
   Position: Some(LexicalInformation([(14,4)-(15,0)]))
   Location: file://amf-client/shared/src/test/resources/validations/facets/custom-facets.raml
 
-- Source: http://a.ml/vocabularies/amf/aml#closed-shape
+- Source: http://a.ml/vocabularies/amf/parser#closed-shape
   Message: Property 'noHolidays' not supported in a RAML 1.0 union node
   Level: Violation
   Target: file://amf-client/shared/src/test/resources/validations/facets/custom-facets.raml#/declarations/types/union/Incorrect1
@@ -29,7 +29,7 @@ Level: Violation
   Position: Some(LexicalInformation([(22,4)-(23,0)]))
   Location: file://amf-client/shared/src/test/resources/validations/facets/custom-facets.raml
 
-- Source: http://a.ml/vocabularies/amf/aml#closed-shape
+- Source: http://a.ml/vocabularies/amf/parser#closed-shape
   Message: Property 'f' not supported in a RAML 1.0 union node
   Level: Violation
   Target: file://amf-client/shared/src/test/resources/validations/facets/custom-facets.raml#/declarations/types/union/Incorrect2
@@ -37,7 +37,7 @@ Level: Violation
   Position: Some(LexicalInformation([(25,4)-(26,0)]))
   Location: file://amf-client/shared/src/test/resources/validations/facets/custom-facets.raml
 
-- Source: http://a.ml/vocabularies/amf/aml#closed-shape
+- Source: http://a.ml/vocabularies/amf/parser#closed-shape
   Message: Property 'error' not supported in a RAML 1.0 union node
   Level: Violation
   Target: file://amf-client/shared/src/test/resources/validations/facets/custom-facets.raml#/declarations/types/union/Incorrect3
