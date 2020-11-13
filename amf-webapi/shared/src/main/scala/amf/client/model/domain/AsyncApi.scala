61656fe55eaec7fe1d7de0f3ef9a367f56bd5b1a
Hernan Najles <hernan.naj+github@gmail.com>
2020-11-13 10:28:40 -0300

    POC

--- amf-webapi/shared/src/main/scala/amf/client/model/domain/AsyncApi.scala
+++ amf-webapi/shared/src/main/scala/amf/client/model/domain/AsyncApi.scala
@@ -4,7 +4,7 @@ import scala.scalajs.js.annotation.{JSExportAll, JSExportTopLevel}
 import amf.plugins.domain.webapi.models.api.{AsyncApi => InternalAsyncApi}
 
 @JSExportAll
-case class AsyncApi(override private[amf] val _internal: InternalAsyncApi) extends Api(_internal) {
+case class AsyncApi(override private[amf] val _internal: InternalAsyncApi) extends Api[AsyncApi](_internal) {
 
   @JSExportTopLevel("model.domain.AsyncApi")
   def this() = this(InternalAsyncApi())
--- amf-webapi/shared/src/main/scala/amf/client/model/domain/WebApi.scala
+++ amf-webapi/shared/src/main/scala/amf/client/model/domain/WebApi.scala
@@ -4,7 +4,7 @@ import scala.scalajs.js.annotation.{JSExportAll, JSExportTopLevel}
 import amf.plugins.domain.webapi.models.api.{WebApi => InternalWebApi}
 
 @JSExportAll
-case class WebApi(override private[amf] val _internal: InternalWebApi) extends Api(_internal) {
+case class WebApi(override private[amf] val _internal: InternalWebApi) extends Api[WebApi](_internal) {
 
   @JSExportTopLevel("model.domain.WebApi")
   def this() = this(InternalWebApi())
