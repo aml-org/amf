a0856fa2d634798a9808527431c553b58fee6333
Agustin Bettati <bettatiagustin@gmail.com>
2020-11-17 16:14:19 -0300

    use instance collector for annotation removal stage iterator

--- amf-webapi/shared/src/main/scala/amf/plugins/domain/webapi/resolution/stages/AnnotationRemovalStage.scala
+++ amf-webapi/shared/src/main/scala/amf/plugins/domain/webapi/resolution/stages/AnnotationRemovalStage.scala
@@ -4,13 +4,15 @@ import amf.core.errorhandling.ErrorHandler
 import amf.core.model.document.{BaseUnit, Document, FieldsFilter}
 import amf.core.model.domain.Annotation
 import amf.core.resolution.stages.ResolutionStage
+import amf.core.traversal.iterator.{DomainElementIterator, IdCollector, InstanceCollector}
 import amf.plugins.document.webapi.annotations.ExternalReferenceUrl
 
 class AnnotationRemovalStage()(override implicit val errorHandler: ErrorHandler) extends ResolutionStage() {
 
   override def resolve[T <: BaseUnit](model: T): T = model match {
     case d: Document =>
-      d.iterator(fieldsFilter = FieldsFilter.All).foreach(_.annotations.reject(eliminationCriteria))
+      d.iterator(fieldsFilter = FieldsFilter.All, visited = InstanceCollector())
+        .foreach(_.annotations.reject(eliminationCriteria))
       model
     case _ => model
   }
