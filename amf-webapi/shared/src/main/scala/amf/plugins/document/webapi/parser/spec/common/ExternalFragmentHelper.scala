94c82e18d552c6afd4a314a0b670991dc9489343
Agustin Bettati <bettatiagustin@gmail.com>
2021-01-08 12:57:15 -0300

    APIMF-2712: ensure the parsed node that is found corresponds to the content being searched

--- amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/parser/spec/common/ExternalFragmentHelper.scala
+++ amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/parser/spec/common/ExternalFragmentHelper.scala
@@ -1,10 +1,10 @@
 package amf.plugins.document.webapi.parser.spec.common
 
+import amf.core.model.StrField
 import amf.core.model.domain.ExternalDomainElement
 import amf.plugins.document.webapi.contexts.WebApiContext
-import org.yaml.model.YNode
+import org.yaml.model.{YNode, YScalar}
 import org.yaml.model.YNode.MutRef
-import org.yaml.parser.JsonParser
 
 object ExternalFragmentHelper {
 
@@ -15,9 +15,14 @@ object ExternalFragmentHelper {
       .map(_.encoded)
       .flatMap {
         case e: ExternalDomainElement =>
-          e.parsed
+          // TODO: leaves room for improvement, content nested within the parsed external domain element can be searched
+          if (sameContent(e.raw, contentNode)) e.parsed else None
         case _ => None
       }
+  }
+
+  private def sameContent(str: StrField, contentNode: YNode): Boolean = {
+    contentNode.asOption[YScalar].exists(_.text == str.value())
   }
 
   private def locationOfNode(contentNode: YNode): String = {
