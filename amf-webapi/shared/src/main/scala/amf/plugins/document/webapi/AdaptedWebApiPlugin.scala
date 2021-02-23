14a32639f4a468d44c140ae078af9d7f453ee80d
Agustin Bettati <bettatiagustin@gmail.com>
2021-02-23 16:52:45 -0300

    APIMF-2877: (MOD) renaming to AMF convention

--- amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/AdaptedWebApiPlugin.scala
+++ amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/AdaptedWebApiPlugin.scala
@@ -1,12 +1,12 @@
 package amf.plugins.document.webapi
 
-import amf.client.`new`.amfcore.plugins.AmfParsePluginAdapter
+import amf.client.`new`.amfcore.plugins.AMFParsePluginAdapter
 
-object Async20ParsePlugin              extends AmfParsePluginAdapter(Async20Plugin)
-object Oas20ParsePlugin                extends AmfParsePluginAdapter(Oas20Plugin)
-object Oas30ParsePlugin                extends AmfParsePluginAdapter(Oas30Plugin)
-object Raml08ParsePlugin               extends AmfParsePluginAdapter(Raml08Plugin)
-object Raml10ParsePlugin               extends AmfParsePluginAdapter(Raml10Plugin)
-object PayloadParsePlugin              extends AmfParsePluginAdapter(PayloadPlugin)
-object JsonSchemaParsePlugin           extends AmfParsePluginAdapter(JsonSchemaPlugin)
-object ExternalJsonYamlRefsParsePlugin extends AmfParsePluginAdapter(ExternalJsonYamlRefsPlugin)
+object Async20ParsePlugin              extends AMFParsePluginAdapter(Async20Plugin)
+object Oas20ParsePlugin                extends AMFParsePluginAdapter(Oas20Plugin)
+object Oas30ParsePlugin                extends AMFParsePluginAdapter(Oas30Plugin)
+object Raml08ParsePlugin               extends AMFParsePluginAdapter(Raml08Plugin)
+object Raml10ParsePlugin               extends AMFParsePluginAdapter(Raml10Plugin)
+object PayloadParsePlugin              extends AMFParsePluginAdapter(PayloadPlugin)
+object JsonSchemaParsePlugin           extends AMFParsePluginAdapter(JsonSchemaPlugin)
+object ExternalJsonYamlRefsParsePlugin extends AMFParsePluginAdapter(ExternalJsonYamlRefsPlugin)
