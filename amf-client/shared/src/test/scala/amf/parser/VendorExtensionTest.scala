5c7ad8d671e20eb74d7aa88b50ff0ea6fc3b06c2
Antonio Garrote <antoniogarrote@gmail.com>
2021-02-26 22:55:04 -0800

    Adding support for extension validation

--- amf-client/shared/src/test/scala/amf/parser/VendorExtensionTest.scala
+++ amf-client/shared/src/test/scala/amf/parser/VendorExtensionTest.scala
@@ -1,12 +1,15 @@
 package amf.parser
 
 import _root_.org.scalatest.Matchers
+import amf.Raml10Profile
 import amf.client.AMF
 import amf.client.convert.{CoreClientConverters, NativeOps}
 import amf.client.parse.DefaultParserErrorHandler
 import amf.client.render._
 import amf.core.{AMFCompiler, CompilerContextBuilder}
-import amf.core.remote.{Amf, Aml, Raml10}
+import amf.core.remote.{Amf, Aml, Oas20, Raml10}
+import amf.facades.Validation
+import amf.internal.environment.Environment
 import amf.io.{FileAssertionTest, MultiJsonldAsyncFunSuite}
 
 import scala.concurrent.ExecutionContext
@@ -15,7 +18,7 @@ import scala.concurrent.ExecutionContext
 trait VendorExtensionTest extends MultiJsonldAsyncFunSuite with Matchers with NativeOps with FileAssertionTest {
   override implicit val executionContext: ExecutionContext = ExecutionContext.Implicits.global
 
-  multiGoldenTest("HERE_HERE Parse vendor extension correctly",
+  multiGoldenTest("Parse vendor extension correctly",
     "file://amf-client/shared/src/test/resources/vendor-extensions/api.%s") { config =>
     val apiPath = "file://amf-client/shared/src/test/resources/vendor-extensions/api.raml"
     val extensionPath = "file://amf-client/shared/src/test/resources/vendor-extensions/extension.yaml"
@@ -45,25 +48,52 @@ trait VendorExtensionTest extends MultiJsonldAsyncFunSuite with Matchers with Na
     }
   }
 
-  multiGoldenTest("HERE_HERE Generate vendor extension correctly", "api.%s") { config =>
+  multiGoldenTest("Parse vendor extension correctly for the connectivity dialect",
+    "file://amf-client/shared/src/test/resources/vendor-extensions/slack.%s") { config =>
+    val apiPath = "file://amf-client/shared/src/test/resources/vendor-extensions/slack.yaml"
+    val extensionPath = "file://amf-client/shared/src/test/resources/vendor-extensions/connector.yaml"
+    val golden  = config.golden
 
-    val directory = "file://amf-client/shared/src/test/resources/vendor-extensions/"
-    val target = s"api.raml"
-    val extensionPath = s"${directory}extension.yaml"
+    // TODO migrate to render options converter
+    var options = new RenderOptions().withCompactUris.withSourceMaps.withPrettyPrint
+    if (config.renderOptions.isFlattenedJsonLd) {
+      options = options.withFlattenedJsonLd
+    } else {
+      options = options.withoutFlattenedJsonLd
+    }
+
     val context =
       new CompilerContextBuilder(extensionPath, platform, DefaultParserErrorHandler.withRun()).build()
-
-    val options = new RenderOptions().withCompactUris.withSourceMaps.withPrettyPrint
 
     for {
       _      <- AMF.init().asFuture
       _      <- new AMFCompiler(context, None, Some(Aml.name)).build()
-      parsed <- amf.Core.parser(Amf.name, "application/ld+json").parseFileAsync(s"${directory}${config.golden}").asFuture
-      raml <- amf.Core.generator("RAML 1.0", "application/yaml").generateString(parsed, options).asFuture
-      actual <- writeTemporaryFile(config.golden)(raml)
-      r      <- assertDifferences(actual, s"${directory}${target}")
+      parsed <- amf.Core.parser(Oas20.name, "application/yaml").parseFileAsync(apiPath).asFuture
+      //      resolved <- Future.successful(amf.Core.resolver(Raml10.name).resolve(parsed, ResolutionPipeline.DEFAULT_PIPELINE))
+      jsonLd <- amf.Core.generator("AMF Graph", "application/ld+json").generateString(parsed, options).asFuture
+      actual <- writeTemporaryFile(golden)(jsonLd)
+      r      <- assertDifferences(actual, golden)
     } yield {
       r
     }
   }
+
+  multiGoldenTest("Validate the extension", "api.%s") { config =>
+    val apiPath = "file://amf-client/shared/src/test/resources/vendor-extensions/api.invalid.raml"
+    val directory = "file://amf-client/shared/src/test/resources/vendor-extensions/"
+    val extensionPath = s"${directory}extension.yaml"
+    val context =
+      new CompilerContextBuilder(extensionPath, platform, DefaultParserErrorHandler.withRun()).build()
+
+    for {
+      _          <- AMF.init().asFuture
+      validation <- Validation(platform)
+      _          <- new AMFCompiler(context, None, Some(Aml.name)).build()
+      parsed     <- amf.Core.parser(Raml10.name, "application/yaml").parseFileAsync(apiPath).asFuture
+      report     <- validation.validate(parsed._internal, Raml10Profile)
+    } yield {
+      assert(!report.conforms)
+    }
+  }
+
 }
