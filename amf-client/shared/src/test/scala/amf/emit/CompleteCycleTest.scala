14baaf8f477d82e1410f372d8a4a51673b9ad8fa
Tomas Fernandez <tfernandez@mulesoft.com>
2021-04-20 09:37:15 -0300

    fixed tests

--- amf-client/shared/src/test/scala/amf/emit/CompleteCycleTest.scala
+++ amf-client/shared/src/test/scala/amf/emit/CompleteCycleTest.scala
@@ -54,10 +54,6 @@ class CompleteCycleTest extends FunSuiteCycleTests {
     cycle("complete.json", config.golden, OasJsonHint, target = Amf, renderOptions = Some(config.renderOptions))
   }
 
-  multiGoldenTest("Endpoints oas to amf test", "endpoints.json.%s") { config =>
-    cycle("endpoints.json", config.golden, OasJsonHint, target = Amf, renderOptions = Some(config.renderOptions))
-  }
-
   multiGoldenTest("Complete with formData parameter references oas to amf test", "formDataParameters.%s") { config =>
     cycle("formDataParameters.json",
           config.golden,
@@ -347,6 +343,10 @@ class CompleteCycleTest extends FunSuiteCycleTests {
 
   test("Endpoints raml to oas test") {
     cycle("endpoints.raml", "endpoints.json", RamlYamlHint, target = Oas)
+  }
+
+  multiGoldenTest("Endpoints oas to amf test", "endpoints.json.%s") { config =>
+    cycle("endpoints.json", config.golden, OasJsonHint, target = Amf, renderOptions = Some(config.renderOptions))
   }
 
   test("Endpoints oas to raml test") {
