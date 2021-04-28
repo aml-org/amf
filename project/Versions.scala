b7f9ad46214545422e633d6aee948954e1740aad
Nicolas Schejtman <nschejtman93@gmail.com>
2021-04-28 16:53:03 -0300

    Fetch amf-webapi.versions file from AMF directory with source mode

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,19 +1,13 @@
-import java.io.FileInputStream
+import sbt._
+
+import java.io.{File, FileInputStream}
 import java.util.Properties
-
-import sbt.Path
-
 import scala.collection.JavaConverters._
 
 object Versions {
   lazy val versions: Map[String, String] = {
-    val props        = new Properties()
-    val absolutePath = Path("").asFile.getAbsolutePath
-    val versionsFile =
-      if (absolutePath.endsWith("als") || absolutePath.endsWith("amf-runner") || absolutePath.endsWith("amf-TCKutor") || absolutePath
-            .endsWith("amf-metadata"))
-        Path("../amf/amf-webapi.versions").asFile
-      else Path("amf-webapi.versions").asFile
+    val props              = new Properties()
+    val versionsFile: File = Common.workspaceDirectory / "amf" / "amf-webapi.versions"
     props.load(new FileInputStream(versionsFile))
     props.entrySet().asScala.map(e => e.getKey.toString -> e.getValue.toString).toMap
   }
