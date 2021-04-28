a257c181cd7014e5d19048203118de857013cf9f
Nicolas Schejtman <nschejtman93@gmail.com>
2021-04-28 16:53:03 -0300

    FIX: Fetch amf-webapi.versions file from AMF directory with source mode

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,8 +6,13 @@ import scala.collection.JavaConverters._
 
 object Versions {
   lazy val versions: Map[String, String] = {
-    val props              = new Properties()
-    val versionsFile: File = Common.workspaceDirectory / "amf" / "amf-webapi.versions"
+    val props             = new Properties()
+    val sourceModeEnabled = java.lang.Boolean.getBoolean("sbt.sourcemode")
+    val versionsFile = if (sourceModeEnabled) {
+      Common.workspaceDirectory / "amf" / "amf-webapi.versions"
+    } else {
+      file("amf-webapi.versions")
+    }
     props.load(new FileInputStream(versionsFile))
     props.entrySet().asScala.map(e => e.getKey.toString -> e.getValue.toString).toMap
   }
