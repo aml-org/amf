e954b24a469387be340d8a6302390b11988b3b3c
arielmirra <amirra@mulesoft.com>
2021-05-10 18:52:51 -0300

    APIMF-3020: add globalThis to buildjs.sh script
    - add globalThis. to SHACLValidator and Ajv declarations

--- amf-cli/js/build-scripts/buildjs.sh
+++ amf-cli/js/build-scripts/buildjs.sh
@@ -2,8 +2,8 @@
 
 # cd ./amf-cli/js
 
-echo 'SHACLValidator = require("amf-shacl-node")' > amf.js
-echo 'Ajv = require("ajv")' >> amf.js
+echo 'globalThis.SHACLValidator = require("amf-shacl-node")' > amf.js
+echo 'globalThis.Ajv = require("ajv")' >> amf.js
 cat ./target/artifact/amf-client-module.js >> amf.js
 chmod a+x amf.js
 
