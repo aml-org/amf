3b46f118d7eb3363d872da2a240f7f5ff868d20a
Tomas Fernandez <tfernandez@mulesoft.com>
2021-05-28 11:12:45 -0300

    Cleanup: changes due to amf-aml adoption.

--- amf-client/shared/src/main/scala/amf/facades/Validation.scala
+++ amf-client/shared/src/main/scala/amf/facades/Validation.scala
@@ -1,25 +1,20 @@
 package amf.facades
 
 import amf.client.execution.BaseExecutionEnvironment
-import amf.client.remod.amfcore.plugins.validate.ValidationConfiguration
-import amf.core.errorhandling.AMFErrorHandler
-import amf.core.model.document.BaseUnit
 import amf.core.remote.Platform
-import amf.core.services.RuntimeValidator
 import amf.core.unsafe.PlatformSecrets
-import amf.core.validation.{AMFValidationReport, EffectiveValidations}
+import amf.core.validation.EffectiveValidations
+import amf.core.validation.core.ValidationSpecification
 import amf.plugins.document.graph.AMFGraphPlugin
-import amf.plugins.document.vocabularies.AMLPlugin
-import amf.plugins.document.vocabularies.model.document.Dialect
 import amf.plugins.document.webapi.validation.PayloadValidatorPlugin
 import amf.plugins.document.webapi.{Oas20Plugin, PayloadPlugin, Raml08Plugin, Raml10Plugin, _}
 import amf.plugins.domain.VocabulariesRegister
 import amf.plugins.domain.shapes.DataShapesDomainPlugin
 import amf.plugins.domain.webapi.APIDomainPlugin
 import amf.plugins.features.validation.CoreValidations
-import amf.plugins.features.validation.custom.AMFValidatorPlugin
+import amf.plugins.features.validation.emitters.ShaclJsonLdShapeGraphEmitter
 import amf.plugins.syntax.SYamlSyntaxPlugin
-import amf.validation.{DialectValidations, ValidationDialectText}
+import amf.validation.DialectValidations
 import amf.validations._
 import amf.{ProfileName, Raml10Profile}
 
@@ -37,7 +32,6 @@ class Validation(platform: Platform) {
 //    platform.registerValidations(ShapePayloadValidations.validations, ShapePayloadValidations.levels)
 //    platform.registerValidations(ShapeParserSideValidations.validations, ShapeParserSideValidations.levels)
 
-    amf.core.AMF.registerPlugin(AMFValidatorPlugin)
     amf.core.AMF.registerPlugin(PayloadValidatorPlugin)
     // Remod registering
     VocabulariesRegister.register(platform)
@@ -50,48 +44,17 @@ class Validation(platform: Platform) {
       amf.core.registries.AMFPluginsRegistry.registerDocumentPlugin(Async20Plugin)
       amf.core.registries.AMFPluginsRegistry.registerDocumentPlugin(PayloadPlugin)
       amf.core.registries.AMFPluginsRegistry.registerDocumentPlugin(AMFGraphPlugin)
-      amf.core.registries.AMFPluginsRegistry.registerDocumentPlugin(AMLPlugin)
       amf.core.registries.AMFPluginsRegistry.registerDocumentPlugin(JsonSchemaPlugin)
       amf.core.registries.AMFPluginsRegistry.registerDomainPlugin(APIDomainPlugin)
       amf.core.registries.AMFPluginsRegistry.registerDomainPlugin(DataShapesDomainPlugin)
     }
   }
 
-  lazy val validator: AMFValidatorPlugin.type =
-    RuntimeValidator.validatorOption.get.asInstanceOf[AMFValidatorPlugin.type]
-  //
-
-  val url = "http://a.ml/dialects/profile.raml"
-
-  /**
-    * Loads the validation dialect from the provided URL
-    */
-  def loadValidationDialect(): Future[Dialect] = {
-    AMLPlugin().registry.registerDialect(url, ValidationDialectText.text)
-    /*
-    platform.dialectsRegistry.get("%Validation Profile 1.0") match {
-      case Some(dialect) => Promise().success(dialect).future
-      case None          => platform.dialectsRegistry.registerDialect(url, ValidationDialectText.text)
-    }
-   */
-  }
-
-  def loadValidationProfile(validationProfilePath: String, errorHandler: AMFErrorHandler): Future[ProfileName] = {
-    validator.loadValidationProfile(validationProfilePath, errorHandler = errorHandler)
-  }
-
-  def validate(model: BaseUnit,
-               profileName: ProfileName,
-               configuration: ValidationConfiguration,
-               resolved: Boolean = false): Future[AMFValidationReport] = {
-
-    validator.validate(model, profileName, resolved, configuration)
-  }
-
-  def computeValidations(profileName: ProfileName): EffectiveValidations = validator.computeValidations(profileName)
-
   def shapesGraph(validations: EffectiveValidations, profileName: ProfileName = Raml10Profile): String =
-    validator.shapesGraph(validations, profileName)
+    new ShaclJsonLdShapeGraphEmitter(profileName).emit(customValidations(validations.effective.values.toSeq))
+
+  def customValidations(validations: Seq[ValidationSpecification]): Seq[ValidationSpecification] =
+    validations.filter(s => !s.isParserSide)
 }
 
 object Validation extends PlatformSecrets {
