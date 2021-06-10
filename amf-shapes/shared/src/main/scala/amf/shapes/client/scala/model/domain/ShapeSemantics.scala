13811479461fc730293c586ce6090bb62e588f9b
Antonio Garrote <antoniogarrote@gmail.com>
2021-06-10 07:34:54 -0700

    Adding support for mandatory attributes transformations

--- amf-shapes/shared/src/main/scala/amf/shapes/client/scala/model/domain/ShapeSemantics.scala
+++ amf-shapes/shared/src/main/scala/amf/shapes/client/scala/model/domain/ShapeSemantics.scala
@@ -67,7 +67,7 @@ class CuriePrefix(override val fields: Fields, val annotations: Annotations)
   def withAlias(alias: String): this.type = set(CuriePrefixModel.Alias, AmfScalar(alias, Annotations()))
   def alias: StrField = fields.field(CuriePrefixModel.Alias)
 
-  override def meta: Obj = DefaultVocabularyModel
+  override def meta: Obj = CuriePrefixModel
 
   override def componentId: String = "/" + iri.value().urlComponentEncoded
 }
@@ -153,7 +153,7 @@ class SemanticContext(override val fields: Fields, val annotations: Annotations)
     }
 
     newContext.withCuries(curies.map { curie =>
-      CuriePrefix().withId(curie.id).withAlias(curie.alias.value()).withId(curie.iri.value())
+      CuriePrefix().withId(curie.id).withAlias(curie.alias.value()).withIri(curie.iri.value())
     })
 
     base.flatMap(_.iri.option()).foreach { iri =>
@@ -165,7 +165,7 @@ class SemanticContext(override val fields: Fields, val annotations: Annotations)
     })
 
     newContext.withMapping(mapping.map { m =>
-      val newMapping = ContextMapping().withId(m.id)
+      val newMapping = ContextMapping().withId(m.id).withAlias(m.alias.value())
       m.iri.option().foreach { iri =>
         newMapping.withIri(expand(iri))
       }
@@ -203,8 +203,8 @@ class SemanticContext(override val fields: Fields, val annotations: Annotations)
     merged.mapping.foreach { mapping =>
       accTypings(mapping.alias.value()) = mapping
     }
-    toMerge.curies.foreach { mapping =>
-      acc(mapping.alias.value()) = mapping
+    toMerge.mapping.foreach { mapping =>
+      accTypings(mapping.alias.value()) = mapping
     }
     merged.withMapping(accTypings.values.toList)
 
