35a884995b415428c49b998dfa089b7407159cc7
Antonio Garrote <antoniogarrote@gmail.com>
2021-06-10 07:34:54 -0700

    Adding support for mandatory attributes transformations

--- amf-shapes/shared/src/main/scala/amf/plugins/parser/dialect/PropertyShapeTransformer.scala
+++ amf-shapes/shared/src/main/scala/amf/plugins/parser/dialect/PropertyShapeTransformer.scala
@@ -1,7 +1,7 @@
 package amf.plugins.parser.dialect
 
 import amf.core.model.domain.extensions.PropertyShape
-import amf.core.parser.{Annotations, Fields}
+import amf.core.parser.{Annotations, Fields, ScalarNode}
 import amf.plugins.document.vocabularies.model.domain.PropertyMapping
 import amf.plugins.domain.shapes.models.{ArrayShape, ScalarShape}
 
@@ -10,6 +10,12 @@ case class PropertyShapeTransformer(property: PropertyShape, ctx: ShapeTransform
   val propertyMapping = PropertyMapping(Fields(),Annotations(property.annotations))
     .withId(property.id)
     .withName(property.name.value())
+
+  def checkMandatoriness() = {
+    property.minCount.option().foreach { minCount =>
+      propertyMapping.withMinCount(minCount)
+    }
+  }
 
   def transform(): PropertyMapping = {
     property.range match {
@@ -22,15 +28,33 @@ case class PropertyShapeTransformer(property: PropertyShape, ctx: ShapeTransform
         }
       }
     }
+    checkMandatoriness()
+    checkSemantics()
     propertyMapping
   }
 
   private def transformScalarProperty(scalar: ScalarShape) = {
+    // datatype
     val scalarRangeDatatype = sanitizeScalarRange(scalar.dataType.value())
     propertyMapping.withLiteralRange(scalarRangeDatatype)
+    // pattern
     scalar.pattern.option().foreach { pattern =>
       propertyMapping.withPattern(pattern)
     }
+    scalar.minimum.option().foreach { minimum =>
+      propertyMapping.withMinimum(minimum)
+    }
+    scalar.maximum.option().foreach { maximum =>
+      propertyMapping.withMaximum(maximum)
+    }
+    /*
+    scalar.values.filter {
+      case _: ScalarNode => true
+      case _             => false // @TODO: advanced types of enums
+    } map { s =>
+
+    }
+     */
   }
 
   private def sanitizeScalarRange(xsd: String): String = {
@@ -40,11 +64,19 @@ case class PropertyShapeTransformer(property: PropertyShape, ctx: ShapeTransform
       xsd;
     }
   }
-/*
-  def checkSemantics(): Unit = {
-    ctx.semantics.mapping.foreach {
 
+  def checkSemantics(): Unit = {
+    ctx.semantics.mapping.foreach { semanticMapping =>
+      val alias = semanticMapping.alias.value()
+      if (propertyMapping.name().value() == alias) {
+        semanticMapping.iri.option().foreach { iri =>
+          propertyMapping.withNodePropertyMapping(iri)
+        }
+        semanticMapping.coercion.option().foreach { iri =>
+          propertyMapping.withLiteralRange(iri)
+        }
+      }
     }
   }
-*/
+
 }
