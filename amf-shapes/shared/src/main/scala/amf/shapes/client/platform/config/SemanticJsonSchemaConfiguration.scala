ec9a2e13c88508c11eeae4e7270a532d4555058a
Hernan Najles <hernan.naj+github@gmail.com>
2021-07-20 09:34:10 -0300

    Add platform semantic json schema configuration

---
+++ amf-shapes/shared/src/main/scala/amf/shapes/client/platform/config/SemanticJsonSchemaConfiguration.scala
@@ -0,0 +1,14 @@
+package amf.shapes.client.platform.config
+
+import amf.aml.client.platform.AMLConfiguration
+import amf.aml.internal.convert.VocabulariesClientConverter._
+import amf.shapes.client.scala.config.{SemanticJsonSchemaConfiguration => InternalSemanticJsonSchemaConfiguration}
+
+import scala.scalajs.js.annotation.{JSExportAll, JSExportTopLevel}
+
+@JSExportAll
+@JSExportTopLevel("SemanticJsonSchemaConfiguration")
+object SemanticJsonSchemaConfiguration {
+
+  def predefined(): AMLConfiguration = InternalSemanticJsonSchemaConfiguration.predefined()
+}
