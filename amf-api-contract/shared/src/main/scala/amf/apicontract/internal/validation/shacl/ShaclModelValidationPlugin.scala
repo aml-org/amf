d000b4216432439fb9aeea6dc0247d9177dd91a5
Tomas Fernandez <tfernandez@mulesoft.com>
2021-07-29 17:25:15 -0300

    Fixed apiContract compilation due to full shacl validator removal

--- amf-api-contract/shared/src/main/scala/amf/apicontract/internal/validation/shacl/ShaclModelValidationPlugin.scala
+++ amf-api-contract/shared/src/main/scala/amf/apicontract/internal/validation/shacl/ShaclModelValidationPlugin.scala
@@ -4,32 +4,33 @@ import amf.apicontract.internal.validation.plugin.BaseApiValidationPlugin
 import amf.core.client.common.validation.ProfileName
 import amf.core.client.scala.model.document.BaseUnit
 import amf.core.client.scala.validation.AMFValidationReport
-import amf.core.internal.plugins.validation.ValidationOptions
+import amf.core.internal.plugins.validation.{ValidationInfo, ValidationOptions}
 import amf.core.internal.validation.ShaclReportAdaptation
-import amf.core.internal.validation.core.ShaclValidationOptions
-import amf.validation.internal.shacl.ShaclValidator
+import amf.validation.internal.shacl.custom.CustomShaclValidator
+import amf.validation.internal.shacl.custom.CustomShaclValidator.CustomShaclFunctions
 
 import scala.concurrent.{ExecutionContext, Future}
 
-trait ShaclValidationPlugin extends BaseApiValidationPlugin with ShaclReportAdaptation {
+object ShaclModelValidationPlugin extends BaseApiValidationPlugin with ShaclReportAdaptation {
+
+  override val id: String = this.getClass.getSimpleName
 
   override protected def specificValidate(unit: BaseUnit, profile: ProfileName, options: ValidationOptions)(
       implicit executionContext: ExecutionContext): Future[AMFValidationReport] = {
     validateWithShacl(unit, profile, options)
   }
 
-  protected def validator(options: ShaclValidationOptions): ShaclValidator
-
   private def validateWithShacl(unit: BaseUnit, profile: ProfileName, options: ValidationOptions)(
       implicit executionContext: ExecutionContext): Future[AMFValidationReport] = {
 
-    val shaclOptions = DefaultShaclOptions(options.config.amfConfig.listeners.toSeq)
-      .withMessageStyle(profile.messageStyle)
+    val validator = new CustomShaclValidator(functions, profile.messageStyle)
 
-    validator(shaclOptions)
+    validator
       .validate(unit, options.effectiveValidations.effective.values.toSeq)
       .map { report =>
         adaptToAmfReport(unit, profile, report, options.effectiveValidations)
       }
   }
+
+  private val functions: CustomShaclFunctions = CustomShaclFunctions.functions
 }
