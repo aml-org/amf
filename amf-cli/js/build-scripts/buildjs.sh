8e1885643be4f62eb9a3432a6aebb2a266151cdf
Tomas Fernandez <tfernandez@mulesoft.com>
2021-08-04 12:49:21 -0300

    Removed shacl.js from published npm package

--- amf-cli/js/build-scripts/buildjs.sh
+++ amf-cli/js/build-scripts/buildjs.sh
@@ -2,7 +2,6 @@
 
 cd ./amf-cli/js || exit
 
-echo 'globalThis.SHACLValidator = require("amf-shacl-node")' > amf.js || exit
 echo 'globalThis.Ajv = require("ajv")' >> amf.js || exit
 cat ./target/artifact/amf-client-module.js >> amf.js || exit
 chmod a+x amf.js
