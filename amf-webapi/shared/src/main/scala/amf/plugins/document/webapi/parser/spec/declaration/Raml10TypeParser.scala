4823048d8c312bed0ff7872d357dab2fc1b7bceb
arielmirra <amirra@mulesoft.com>
2021-08-05 16:46:32 -0300

    APIMF-2975: nillable properties now accept required facet

--- amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/parser/spec/declaration/Raml10TypeParser.scala
+++ amf-webapi/shared/src/main/scala/amf/plugins/document/webapi/parser/spec/declaration/Raml10TypeParser.scala
@@ -581,8 +581,7 @@ sealed abstract class RamlTypeParser(entryOrNode: YMapEntryLike,
           }
         }
         val toParse = YMapEntry(YNode(name), YMap(newEntries, newEntries.headOption.map(_.sourceName).getOrElse("")))
-        val parsed =
-          ctx.factory.typeParser(toParse, s => s.withId(union.id), typeInfo.isAnnotation, defaultType).parse().get
+        val parsed  = Raml10TypeParser(toParse, s => s.withId(union.id), typeInfo).parse().get
         parsed.inherits.head match {
           case union: UnionShape =>
             union.anyOf.tail.head match {
