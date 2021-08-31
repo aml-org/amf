20c72a7550ced84ed2202b504df53159e3387403
Tomas Fernandez <tfernandez@mulesoft.com>
2021-08-31 12:18:34 -0300

    Moved shape rendering methods up to the BaseShapeConfiguration

--- amf-shapes/shared/src/main/scala/amf/shapes/client/platform/ShapesElementClient.scala
+++ amf-shapes/shared/src/main/scala/amf/shapes/client/platform/ShapesElementClient.scala
@@ -1,12 +1,7 @@
 package amf.shapes.client.platform
 
-import amf.core.client.platform.model.domain.DomainElement
-import amf.core.client.platform.render.AMFElementRenderer
-import amf.shapes.client.platform.model.domain.{AnyShape, Example}
-import amf.shapes.client.platform.render.{JsonSchemaShapeRenderer, RamlShapeRenderer}
 import amf.shapes.client.scala.{ShapesElementClient => InternalShapesElementClient}
 import amf.shapes.internal.convert.ShapeClientConverters._
-import org.yaml.builder.DocBuilder
 
 import scala.scalajs.js.annotation.JSExportAll
 
@@ -19,12 +14,4 @@ class ShapesElementClient private[amf] (private val _internal: InternalShapesEle
   }
 
   override def getConfiguration(): ShapesConfiguration = _internal.getConfiguration
-
-  def toJsonSchema(element: AnyShape): String                    = _internal.toJsonSchema(element)
-  def buildJsonSchema(element: AnyShape): String                 = _internal.buildJsonSchema(element)
-  def toRamlDatatype(element: AnyShape): String                  = _internal.toRamlDatatype(element)
-  def renderExample(example: Example, mediaType: String): String = _internal.renderExample(example, mediaType)
-
-  override def renderToBuilder[T](element: DomainElement, builder: DocBuilder[T]): Unit =
-    AMFElementRenderer.renderToBuilder(element, builder, getConfiguration())
 }
