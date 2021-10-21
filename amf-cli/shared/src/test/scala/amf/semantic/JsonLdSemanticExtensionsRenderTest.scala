25f59a61c97a583d303e7f20c218591c273b84ef
Tomas Fernandez <tfernandez@mulesoft.com>
2021-10-21 17:00:55 -0300

    Test fix: Removed 'await' helper as it messed up JS bundle

--- amf-cli/shared/src/test/scala/amf/semantic/JsonLdSemanticExtensionsRenderTest.scala
+++ amf-cli/shared/src/test/scala/amf/semantic/JsonLdSemanticExtensionsRenderTest.scala
@@ -1,45 +1,46 @@
 package amf.semantic
 
-import amf.apicontract.client.scala.{AMFConfiguration, APIConfiguration}
+import amf.apicontract.client.scala.{
+  AMFConfiguration,
+  APIConfiguration,
+  AsyncAPIConfiguration,
+  OASConfiguration,
+  RAMLConfiguration
+}
 import amf.core.client.scala.config.RenderOptions
 import amf.core.client.scala.errorhandling.{AMFErrorHandler, UnhandledErrorHandler}
 import amf.core.client.scala.model.document.BaseUnit
 import amf.core.internal.remote._
 import amf.io.FunSuiteCycleTests
 
+import scala.concurrent.Future
+
 class JsonLdSemanticExtensionsRenderTest extends FunSuiteCycleTests {
+
   override def basePath: String = "amf-cli/shared/src/test/resources/semantic/"
 
   test("Render flattened semantic extensions to JSON-LD in a RAML 1.0 spec") {
-    cycle("api.raml",
-          golden = "instance.raml.jsonld",
-          Raml10YamlHint,
-          AmfJsonHint,
-          renderOptions = Some(RenderOptions().withPrettyPrint.withCompactUris))
+    getConfig("dialect.yaml", RAMLConfiguration.RAML10()).flatMap { config =>
+      cycle("api.raml", golden = "instance.raml.jsonld", Raml10YamlHint, AmfJsonHint, amfConfig = Some(config))
+    }
   }
 
   test("Render flattened semantic extensions to JSON-LD in a OAS 2.0 spec") {
-    cycle("api.oas20.yaml",
-          golden = "instance.oas20.jsonld",
-          Oas20YamlHint,
-          AmfJsonHint,
-          renderOptions = Some(RenderOptions().withPrettyPrint.withCompactUris))
+    getConfig("dialect.yaml", OASConfiguration.OAS20()).flatMap { config =>
+      cycle("api.oas20.yaml", golden = "instance.oas20.jsonld", Oas20YamlHint, AmfJsonHint, amfConfig = Some(config))
+    }
   }
 
   test("Render flattened semantic extensions to JSON-LD in a OAS 3.0 spec") {
-    cycle("api.oas30.yaml",
-          golden = "instance.oas30.jsonld",
-          Oas30YamlHint,
-          AmfJsonHint,
-          renderOptions = Some(RenderOptions().withPrettyPrint.withCompactUris))
+    getConfig("dialect.yaml", OASConfiguration.OAS30()).flatMap { config =>
+      cycle("api.oas30.yaml", golden = "instance.oas30.jsonld", Oas30YamlHint, AmfJsonHint, amfConfig = Some(config))
+    }
   }
 
   test("Render flattened semantic extensions to JSON-LD in a ASYNC 2.0 spec") {
-    cycle("api.async.yaml",
-          golden = "instance.async.jsonld",
-          Async20YamlHint,
-          AmfJsonHint,
-          renderOptions = Some(RenderOptions().withPrettyPrint.withCompactUris))
+    getConfig("dialect.yaml", AsyncAPIConfiguration.Async20()).flatMap { config =>
+      cycle("api.async.yaml", golden = "instance.async.jsonld", Async20YamlHint, AmfJsonHint, amfConfig = Some(config))
+    }
   }
 
   /** Method for transforming parsed unit. Override if necessary. */
@@ -47,22 +48,11 @@ class JsonLdSemanticExtensionsRenderTest extends FunSuiteCycleTests {
     APIConfiguration.fromSpec(unit.sourceSpec.get).baseUnitClient().transform(unit).baseUnit
   }
 
-  override protected def buildConfig(options: Option[RenderOptions], eh: Option[AMFErrorHandler]): AMFConfiguration = {
-    getConfig("dialect.yaml")
-  }
-
-  override protected def buildConfig(from: AMFConfiguration,
-                                     options: Option[RenderOptions],
-                                     eh: Option[AMFErrorHandler]): AMFConfiguration = {
-    getConfig("dialect.yaml", from)
-  }
-
-  private def getConfig(dialect: String, baseConfig: AMFConfiguration = APIConfiguration.API()): AMFConfiguration = {
-    val config = baseConfig
+  private def getConfig(dialect: String,
+                        baseConfig: AMFConfiguration = APIConfiguration.API()): Future[AMFConfiguration] = {
+    baseConfig
       .withRenderOptions(RenderOptions().withPrettyPrint.withCompactUris)
       .withErrorHandlerProvider(() => UnhandledErrorHandler)
       .withDialect(s"file://$basePath" + dialect)
-
-    await { config }
   }
 }
--- amf-cli/shared/src/test/scala/amf/semantic/semantic.scala
+++
@@ -1,9 +0,0 @@
-package amf
-
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
-
-package object semantic {
-
-  def await[T](future: => Future[T]): T = Await.result(future, Duration.Inf)
-}
