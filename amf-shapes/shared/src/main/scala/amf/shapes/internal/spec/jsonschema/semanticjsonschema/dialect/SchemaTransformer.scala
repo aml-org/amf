8684f127c3cf3931cc46b08fdc52454d87f4a1b2
Loose <alejandro.loose@mulesoft.com>
2021-10-28 17:47:11 -0300

    Added tests, fixed some ID issues (WIP), changed generated dialect name

--- amf-shapes/shared/src/main/scala/amf/shapes/internal/spec/jsonschema/semanticjsonschema/dialect/SchemaTransformer.scala
+++ amf-shapes/shared/src/main/scala/amf/shapes/internal/spec/jsonschema/semanticjsonschema/dialect/SchemaTransformer.scala
@@ -6,17 +6,19 @@ import amf.shapes.client.scala.model.domain.AnyShape
 
 import scala.collection.mutable
 
-case class TransformationResult(encoded: Either[NodeMapping, UnionNodeMapping], declared: Seq[DomainElement], externals: mutable.Map[String,String])
+case class TransformationResult(encoded: Either[NodeMapping, UnionNodeMapping],
+                                declared: Seq[DomainElement],
+                                externals: mutable.Map[String, String])
 
 case class SchemaTransformer(shape: AnyShape) {
 
   def transform(): TransformationResult = {
-    val ctx = ShapeTransformationContext()
-    val transformed = ShapeTransformer(shape,ctx).transform()
-    val declared = ctx.transformed()
+    val ctx         = ShapeTransformationContext()
+    val transformed = ShapeTransformation(shape, ctx).transform()
+    val declared    = ctx.transformed()
     transformed match {
       case nm: NodeMapping       => TransformationResult(Left(nm), declared, ctx.externals)
-      case unm: UnionNodeMapping =>TransformationResult(Right(unm), declared, ctx.externals)
+      case unm: UnionNodeMapping => TransformationResult(Right(unm), declared, ctx.externals)
       case _                     => throw new Exception(s"Unknown transformed shape ${transformed}")
     }
   }
