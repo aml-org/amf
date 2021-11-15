13ae80ec83462490b385e98969046ed81d7ed982
arielmirra <amirra@mulesoft.com>
2021-11-15 11:43:36 -0300

    APIMF-3311: add tests with json schema files

--- amf-cli/shared/src/test/scala/amf/validation/AMFClientTest.scala
+++ amf-cli/shared/src/test/scala/amf/validation/AMFClientTest.scala
@@ -3,7 +3,8 @@ package amf.validation
 import amf.apicontract.client.scala.RAMLConfiguration
 import amf.core.client.common.transform.PipelineId
 import amf.core.client.scala.config.RenderOptions
-import amf.shapes.client.scala.model.domain.SchemaShape
+import amf.core.client.scala.model.domain.ExternalSourceElement
+import amf.shapes.client.scala.model.domain.{NodeShape, SchemaShape}
 import org.scalatest.{AsyncFunSuite, Matchers}
 
 import scala.concurrent.ExecutionContext
@@ -48,6 +49,49 @@ class AMFClientTest extends AsyncFunSuite with Matchers {
         val restoredXml = restoredTypes.find(_.name.value() == xmlType)
         restoredXml.isDefined shouldBe true
         val restoredRaw = restoredXml.get.raw.value()
+        restoredRaw.isInstanceOf[String] shouldBe true
+        restoredRaw.isEmpty shouldBe false
+      }
+    }
+  }
+
+  test("AMF should persist and restore the raw json schema") {
+    val api           = "file://amf-cli/shared/src/test/resources/validations/raml/raml-with-json-schema/api.raml"
+    val ro            = RenderOptions().withCompactUris.withPrettyPrint.withSourceMaps
+    val configuration = RAMLConfiguration.RAML10().withRenderOptions(ro)
+    val client        = configuration.baseUnitClient()
+
+    client.parse(api) flatMap { parseResult =>
+      // parsing
+      parseResult.conforms shouldBe true
+      val NodeShapeType = "http://www.w3.org/ns/shacl#NodeShape"
+      val typeName      = "JsonSchema"
+      val bu            = parseResult.baseUnit
+      val nodeShapes = bu.findByType(NodeShapeType) map { element =>
+        element.asInstanceOf[NodeShape]
+      }
+      nodeShapes.length shouldBe 3
+      val definedType = nodeShapes.find(_.name.value() == typeName)
+      val jsonSchema  = definedType.get.inherits.head
+      // this jsonSchema node doesn't have the raw field, only the reference-id to it, and the raw value in the parsed-json-schema annotation.
+      val idToRaw = jsonSchema.asInstanceOf[ExternalSourceElement].referenceId
+      idToRaw.value().isEmpty shouldBe false
+
+      // serialization
+      val transformResult = client.transform(parseResult.baseUnit, PipelineId.Editing)
+      val rendered        = client.render(transformResult.baseUnit, "application/ld+json")
+
+      // restoring
+      val client2 = RAMLConfiguration.RAML10().baseUnitClient()
+      client2.parseContent(rendered) flatMap { parseResult =>
+        val restoredBu = parseResult.baseUnit
+        val restoredNodeShapes = restoredBu.findByType(NodeShapeType) map { element =>
+          element.asInstanceOf[NodeShape]
+        }
+        restoredNodeShapes.length shouldBe 2
+        val restoredType       = restoredNodeShapes.find(_.name.value() == typeName)
+        val restoredJsonSchema = restoredType.get
+        val restoredRaw        = restoredJsonSchema.raw.value()
         restoredRaw.isInstanceOf[String] shouldBe true
         restoredRaw.isEmpty shouldBe false
       }
