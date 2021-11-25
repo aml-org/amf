fe66a6f5817289faa6fae20c7cea93f7e4527d11
Loose <alejandro.loose@mulesoft.com>
2021-11-25 18:58:18 -0300

    Fix interface problem and update typings

--- amf-shapes/shared/src/main/scala/amf/shapes/client/platform/config/SemanticJsonSchemaConfiguration.scala
+++ amf-shapes/shared/src/main/scala/amf/shapes/client/platform/config/SemanticJsonSchemaConfiguration.scala
@@ -1,9 +1,8 @@
 package amf.shapes.client.platform.config
 
-import amf.aml.client.platform.AMLConfiguration
-import amf.aml.internal.convert.VocabulariesClientConverter._
-import amf.shapes.client.scala.ShapesConfiguration
+import amf.shapes.client.platform.ShapesConfiguration
 import amf.shapes.client.scala.config.{SemanticJsonSchemaConfiguration => InternalSemanticJsonSchemaConfiguration}
+import amf.shapes.internal.convert.ShapeClientConverters._
 
 import scala.scalajs.js.annotation.{JSExportAll, JSExportTopLevel}
 
