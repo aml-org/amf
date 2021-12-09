15a6e1a4f0474e8aca95303d826c13b4c9184f5a
Antonio Garrote <antoniogarrote@gmail.com>
2021-12-09 09:07:59 -0800

    Small GraphQL fixes

--- amf-shapes/shared/src/main/scala/amf/shapes/internal/entities/ShapeEntities.scala
+++ amf-shapes/shared/src/main/scala/amf/shapes/internal/entities/ShapeEntities.scala
@@ -4,6 +4,7 @@ import amf.core.internal.entities.Entities
 import amf.core.internal.metamodel.ModelDefaultBuilder
 import amf.core.internal.metamodel.domain.extensions.{PropertyShapeModel, ShapeExtensionModel}
 import amf.shapes.internal.domain.metamodel._
+import amf.shapes.internal.domain.metamodel.operations.{ShapeOperationModel, ShapeParameterModel, ShapePayloadModel, ShapeRequestModel, ShapeResponseModel}
 
 private[amf] object ShapeEntities extends Entities {
 
@@ -15,6 +16,11 @@ private[amf] object ShapeEntities extends Entities {
     FileShapeModel,
     NilShapeModel,
     NodeShapeModel,
+    ShapeOperationModel,
+    ShapeParameterModel,
+    ShapePayloadModel,
+    ShapeRequestModel,
+    ShapeResponseModel,
     PropertyShapeModel,
     PropertyDependenciesModel,
     ScalarShapeModel,
