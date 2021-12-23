828e3fbcfc5208cc98dd8d8ab4e864df91637611
Loose <alejandro.loose@mulesoft.com>
2021-12-23 10:21:40 -0300

    APIMF-3411: added parsing of @characteristics and iri validation

--- amf-shapes/shared/src/main/scala/amf/shapes/client/scala/model/domain/ShapeSemantics.scala
+++ amf-shapes/shared/src/main/scala/amf/shapes/client/scala/model/domain/ShapeSemantics.scala
@@ -5,24 +5,28 @@ import amf.core.client.scala.model.domain.{AmfScalar, DomainElement}
 import amf.core.internal.metamodel.Obj
 import amf.core.internal.parser.domain.{Annotations, Fields}
 import amf.core.internal.utils.AmfStrings
-import amf.shapes.internal.domain.metamodel.{BaseIRIModel, ContextElementWithIri, ContextMappingModel, CuriePrefixModel, DefaultVocabularyModel, SemanticContextModel}
+import amf.shapes.internal.domain.metamodel.{
+  BaseIRIModel,
+  ContextElementWithIri,
+  ContextMappingModel,
+  CuriePrefixModel,
+  DefaultVocabularyModel,
+  SemanticContextModel
+}
 import org.yaml.model.YPart
 
 import scala.collection.mutable
 
-trait WithContextIri {
-  this: DomainElement =>
-  def withIri(iri: String): this.type = set(ContextElementWithIri.IRI, AmfScalar(iri, Annotations()))
+trait WithContextIri { this: DomainElement =>
+  def withIri(iri: String): this.type                   = set(ContextElementWithIri.IRI, AmfScalar(iri, Annotations()))
   def withIri(iri: String, ann: Annotations): this.type = set(ContextElementWithIri.IRI, AmfScalar(iri, ann))
-  def iri: StrField = fields.field(ContextElementWithIri.IRI)
+  def iri: StrField                                     = fields.field(ContextElementWithIri.IRI)
 }
 
-class BaseIri(override val fields: Fields, val annotations: Annotations)
-  extends DomainElement
-    with WithContextIri {
+class BaseIri(override val fields: Fields, val annotations: Annotations) extends DomainElement with WithContextIri {
 
   def withNulled(nulled: Boolean): this.type = set(ContextMappingModel.Nulled, AmfScalar(nulled, Annotations()))
-  def nulled: BoolField = fields.field(ContextMappingModel.Nulled)
+  def nulled: BoolField                      = fields.field(ContextMappingModel.Nulled)
 
   override def meta: Obj = BaseIRIModel
 
@@ -39,16 +43,14 @@ object BaseIri {
     new BaseIri(Fields(), annotations)
 }
 
-
 class DefaultVocabulary(override val fields: Fields, val annotations: Annotations)
-  extends DomainElement
+    extends DomainElement
     with WithContextIri {
 
   override def meta: Obj = DefaultVocabularyModel
 
   override def componentId: String = "/@vocab".urlComponentEncoded
 }
-
 
 object DefaultVocabulary {
   def apply(): DefaultVocabulary = apply(Annotations())
@@ -59,19 +61,17 @@ object DefaultVocabulary {
     new DefaultVocabulary(Fields(), annotations)
 }
 
-
 class CuriePrefix(override val fields: Fields, val annotations: Annotations)
-  extends DomainElement
+    extends DomainElement
     with WithContextIri {
 
   def withAlias(alias: String): this.type = set(CuriePrefixModel.Alias, AmfScalar(alias, Annotations()))
-  def alias: StrField = fields.field(CuriePrefixModel.Alias)
+  def alias: StrField                     = fields.field(CuriePrefixModel.Alias)
 
   override def meta: Obj = CuriePrefixModel
 
   override def componentId: String = "/" + iri.value().urlComponentEncoded
 }
-
 
 object CuriePrefix {
   def apply(): CuriePrefix = apply(Annotations())
@@ -82,21 +82,20 @@ object CuriePrefix {
     new CuriePrefix(Fields(), annotations)
 }
 
-
 class ContextMapping(override val fields: Fields, val annotations: Annotations)
-  extends DomainElement
+    extends DomainElement
     with WithContextIri {
 
   override def meta: Obj = ContextMappingModel
 
   def withAlias(alias: String): this.type = set(ContextMappingModel.Alias, AmfScalar(alias, Annotations()))
-  def alias: StrField = fields.field(ContextMappingModel.Alias)
+  def alias: StrField                     = fields.field(ContextMappingModel.Alias)
 
   def withCoercion(coercion: String): this.type = set(ContextMappingModel.Coercion, AmfScalar(coercion, Annotations()))
-  def coercion: StrField = fields.field(ContextMappingModel.Coercion)
+  def coercion: StrField                        = fields.field(ContextMappingModel.Coercion)
 
   def withNulled(nulled: Boolean): this.type = set(ContextMappingModel.Nulled, AmfScalar(nulled, Annotations()))
-  def nulled: BoolField = fields.field(ContextMappingModel.Nulled)
+  def nulled: BoolField                      = fields.field(ContextMappingModel.Nulled)
 
   override def componentId: String = "/" + alias.value().urlComponentEncoded
 }
@@ -110,31 +109,30 @@ object ContextMapping {
     new ContextMapping(Fields(), annotations)
 }
 
-
 class SemanticContext(override val fields: Fields, val annotations: Annotations)
-  extends DomainElement
+    extends DomainElement
     with WithContextIri {
   override def meta: Obj = SemanticContextModel
 
-  def withBase(base: BaseIri): this.type  = set(SemanticContextModel.Base, base)
-  def base: Option[BaseIri] = Option(fields.field(SemanticContextModel.Base))
+  def withBase(base: BaseIri): this.type = set(SemanticContextModel.Base, base)
+  def base: Option[BaseIri]              = Option(fields.field(SemanticContextModel.Base))
 
-  def withVocab(vocab: DefaultVocabulary): this.type  = set(SemanticContextModel.Vocab, vocab)
-  def vocab: Option[DefaultVocabulary] = Option(fields.field(SemanticContextModel.Vocab))
+  def withVocab(vocab: DefaultVocabulary): this.type = set(SemanticContextModel.Vocab, vocab)
+  def vocab: Option[DefaultVocabulary]               = Option(fields.field(SemanticContextModel.Vocab))
 
-  def withCuries(curies: Seq[CuriePrefix]): this.type  = setArray(SemanticContextModel.Curies, curies)
-  def curies: Seq[CuriePrefix] = fields.field(SemanticContextModel.Curies)
+  def withCuries(curies: Seq[CuriePrefix]): this.type = setArray(SemanticContextModel.Curies, curies)
+  def curies: Seq[CuriePrefix]                        = fields.field(SemanticContextModel.Curies)
 
-  def withMapping(mapping: Seq[ContextMapping]): this.type  = setArray(SemanticContextModel.Mapping, mapping)
-  def mapping: Seq[ContextMapping] = fields.field(SemanticContextModel.Mapping)
+  def withMapping(mapping: Seq[ContextMapping]): this.type = setArray(SemanticContextModel.Mapping, mapping)
+  def mapping: Seq[ContextMapping]                         = fields.field(SemanticContextModel.Mapping)
 
   def withTypeMappings(typeMappings: Seq[String]): this.type = set(SemanticContextModel.TypeMapping, typeMappings)
-  def typeMappings: Seq[StrField] = fields.field(SemanticContextModel.TypeMapping)
+  def typeMappings: Seq[StrField]                            = fields.field(SemanticContextModel.TypeMapping)
 
   override def componentId: String = "/" + "@context".urlComponentEncoded
 
-  private def prefixMap(): Map[String,String] = {
-    var acc = Map[String,String]()
+  def prefixMap(): Map[String, String] = {
+    var acc = Map[String, String]()
     curies.foreach { curie =>
       acc += (curie.alias.value() -> curie.iri.value())
     }
@@ -148,8 +146,8 @@ class SemanticContext(override val fields: Fields, val annotations: Annotations)
     vocab match {
       case Some(v) =>
         newContext.withVocab(DefaultVocabulary().withId(v.id).withIri(expand(v.iri.value())))
-      case _       =>
-        // ignore
+      case _ =>
+      // ignore
     }
 
     newContext.withCuries(curies.map { curie =>
@@ -185,7 +183,7 @@ class SemanticContext(override val fields: Fields, val annotations: Annotations)
     vocab match {
       case Some(v) =>
         newContext.withVocab(DefaultVocabulary().withId(v.id).withIri(v.iri.value()))
-      case _       =>
+      case _ =>
       // ignore
     }
 
@@ -241,7 +239,7 @@ class SemanticContext(override val fields: Fields, val annotations: Annotations)
     merged.withCuries(acc.values.toList)
 
     // typings
-    val accTypings: mutable.Map[String,ContextMapping] = mutable.Map()
+    val accTypings: mutable.Map[String, ContextMapping] = mutable.Map()
     merged.mapping.foreach { mapping =>
       accTypings(mapping.alias.value()) = mapping
     }
@@ -249,6 +247,29 @@ class SemanticContext(override val fields: Fields, val annotations: Annotations)
       accTypings(mapping.alias.value()) = mapping
     }
     merged.withMapping(accTypings.values.toList)
+
+    merged
+  }
+
+  // Method to merge only relevant information from a parent context into a child one
+  def partialMerge(toMerge: SemanticContext): SemanticContext = {
+    val merged = copy()
+
+    // Base and Vocab are merged if not exists
+    if (merged.base.isEmpty) toMerge.base.foreach(merged.withBase)
+    if (merged.vocab.isEmpty) toMerge.vocab.foreach(merged.withVocab)
+
+    // Curies are merged
+    val acc: mutable.Map[String, CuriePrefix] = mutable.Map()
+    toMerge.curies.foreach { curie =>
+      acc(curie.alias.value()) = curie
+    }
+    merged.curies.foreach { curie =>
+      acc(curie.alias.value()) = curie
+    }
+    merged.withCuries(acc.values.toList)
+
+    // Other fields are ignored
 
     merged
   }
