e38af63fa78d5de65be26684df11f0a6b318db98
arielmirra <amirra@mulesoft.com>
2022-02-01 17:53:50 -0300

    APIMF-3386: fix example duplication when name is null or autogenerated

--- amf-cli/shared/src/test/resources/org/raml/parser/library/parameterized-references/output.txt.js
+++ amf-cli/shared/src/test/resources/org/raml/parser/library/parameterized-references/output.txt.js
@@ -1,7 +1,7 @@
 ModelId: file://amf-cli/shared/src/test/resources/org/raml/parser/library/parameterized-references/input.raml
 Profile: RAML 1.0
 Conforms: false
-Number of results: 4
+Number of results: 2
 
 Level: Violation
 
@@ -19,20 +19,4 @@ Level: Violation
   Target: file://amf-cli/shared/src/test/resources/org/raml/parser/library/parameterized-references/input.raml#/web-api/endpoint/%2Fuser/post/request/parameter/header/broken-example-param/scalar/schema/example/default-example
   Property: file://amf-cli/shared/src/test/resources/org/raml/parser/library/parameterized-references/input.raml#/web-api/endpoint/%2Fuser/post/request/parameter/header/broken-example-param/scalar/schema/example/default-example
   Range: [(29,29)-(29,47)]
-  Location: file://amf-cli/shared/src/test/resources/org/raml/parser/library/parameterized-references/liba.raml
-
-- Constraint: http://a.ml/vocabularies/amf/validation#example-validation-error
-  Message: should be equal to one of the allowed values
-  Severity: Violation
-  Target: file://amf-cli/shared/src/test/resources/org/raml/parser/library/parameterized-references/input.raml#/web-api/endpoint/%2Fuser/post/request/parameter/header/broken-type-param/scalar/schema/example/default-example
-  Property: file://amf-cli/shared/src/test/resources/org/raml/parser/library/parameterized-references/input.raml#/web-api/endpoint/%2Fuser/post/request/parameter/header/broken-type-param/scalar/schema/example/default-example
-  Range: [(32,29)-(32,34)]
-  Location: file://amf-cli/shared/src/test/resources/org/raml/parser/library/parameterized-references/liba.raml
-
-- Constraint: http://a.ml/vocabularies/amf/validation#example-validation-error
-  Message: should be equal to one of the allowed values
-  Severity: Violation
-  Target: file://amf-cli/shared/src/test/resources/org/raml/parser/library/parameterized-references/input.raml#/web-api/endpoint/%2Fuser/post/request/parameter/header/broken-all-params/scalar/schema/example/default-example
-  Property: file://amf-cli/shared/src/test/resources/org/raml/parser/library/parameterized-references/input.raml#/web-api/endpoint/%2Fuser/post/request/parameter/header/broken-all-params/scalar/schema/example/default-example
-  Range: [(35,29)-(35,47)]
   Location: file://amf-cli/shared/src/test/resources/org/raml/parser/library/parameterized-references/liba.raml
