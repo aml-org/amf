ec122547e3d22ba5c36243c6442f01489d3383fe
hghianni <hghianni@mulesoft.com>
2022-03-15 17:56:34 -0300

    W-10784090: Duplicate step to be consistent

--- amf-api-contract/shared/src/main/scala/amf/apicontract/internal/transformation/compatibility/Oas20CompatibilityPipeline.scala
+++ amf-api-contract/shared/src/main/scala/amf/apicontract/internal/transformation/compatibility/Oas20CompatibilityPipeline.scala
@@ -3,7 +3,6 @@ package amf.apicontract.internal.transformation.compatibility
 import amf.apicontract.internal.transformation.Oas20TransformationPipeline
 import amf.apicontract.internal.transformation.compatibility.common.SemanticFlattenFilter
 import amf.apicontract.internal.transformation.compatibility.oas._
-import amf.apicontract.internal.transformation.compatibility.oas3.CleanRepeatedOperationIds
 import amf.core.client.common.transform._
 import amf.core.client.scala.transform.{TransformationPipeline, TransformationStep}
 
