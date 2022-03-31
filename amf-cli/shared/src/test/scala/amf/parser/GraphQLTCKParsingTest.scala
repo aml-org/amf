3a85d9950ec59a6ba433e0474b4df9e6d1a0a805
Nicolas Schejtman <nschejtman93@gmail.com>
2022-03-31 16:29:26 -0300

    W-10671849: added GraphQL TCK & tests

---
+++ amf-cli/shared/src/test/scala/amf/parser/GraphQLTCKParsingTest.scala
@@ -0,0 +1,26 @@
+package amf.parser
+
+import amf.core.client.scala.config.RenderOptions
+import amf.core.internal.remote.{AmfJsonHint, GraphQLHint}
+import amf.cycle.GraphQLFunSuiteCycleTests
+
+class GraphQLTCKParsingTest extends GraphQLFunSuiteCycleTests {
+  override def basePath: String = s"amf-cli/shared/src/test/resources/graphql/tck/apis/valid/"
+
+
+
+  // Test valid APIs
+  fs.syncFile(s"$basePath").list.foreach { api =>
+    if (api.endsWith(".graphql") && !api.endsWith(".dumped.graphql")) {
+      ignore(s"GraphQL TCK > Apis > Valid > $api: dumped JSON matches golden") {
+        cycle(api, api.replace(".graphql", ".jsonld"), GraphQLHint, AmfJsonHint)
+      }
+
+      ignore(s"GraphQL TCK > Apis > Valid > $api: dumped GraphQL matches golden") {
+        cycle(api, api.replace(".graphql", ".dumped.graphql"), GraphQLHint, GraphQLHint)
+      }
+    }
+  }
+
+  override def renderOptions(): RenderOptions = RenderOptions().withoutFlattenedJsonLd.withPrettyPrint
+}
