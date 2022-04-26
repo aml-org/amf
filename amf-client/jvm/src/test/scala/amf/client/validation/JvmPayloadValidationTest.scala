616fc0736104ba891bd4e3a414c07d70b4559d09
hghianni <hghianni@mulesoft.com>
2022-04-26 17:17:29 -0300

    W-10983537: Add tests

--- amf-client/jvm/src/test/scala/amf/client/validation/JvmPayloadValidationTest.scala
+++ amf-client/jvm/src/test/scala/amf/client/validation/JvmPayloadValidationTest.scala
@@ -1,9 +1,13 @@
 package amf.client.validation
 
 import amf.client.model.DataTypes
-import amf.client.model.domain.ScalarShape
+import amf.client.model.document.Document
+import amf.client.model.domain.{AnyShape, ScalarShape}
+import amf.client.parse.RamlParser
+import amf.client.resolve.Raml10Resolver
 import amf.convert.NativeOpsFromJvm
 import amf.core.AMF
+import amf.core.resolution.pipelines.ResolutionPipeline
 import amf.plugins.document.webapi.validation.PayloadValidatorPlugin
 
 class JvmPayloadValidationTest extends ClientPayloadValidationTest with NativeOpsFromJvm {
@@ -47,6 +51,39 @@ class JvmPayloadValidationTest extends ClientPayloadValidationTest with NativeOp
       val zeroReport           = validator.syncValidate("application/json", "0")
       positiveNumberReport.results.asSeq.head.message shouldBe "Can't divide by 0"
       zeroReport.results.asSeq.head.message shouldBe "Can't divide by 0"
+    }
+  }
+
+  test("W-10983537: Regex hangs. Valid payload") {
+    AMF.init().flatMap { _ =>
+      amf.Core.registerPlugin(PayloadValidatorPlugin)
+      val path        = "file://amf-cli/shared/src/test/resources/regex-hangs/specification.raml"
+      val parseResult = new RamlParser().parseFileAsync(path).asFuture
+      parseResult.flatMap { bu =>
+        val resolutionResult  = new Raml10Resolver().resolve(bu, ResolutionPipeline.EDITING_PIPELINE)
+        val shape             = resolutionResult.asInstanceOf[Document].declares.asSeq.head.asInstanceOf[AnyShape]
+        val validator         = shape.payloadValidator("application/json").asOption.get
+        val payload           = "{\"URL\": \"https://www.google.com/\", \"EventTypeId\": 10}"
+        val report            = validator.syncValidate("application/json", payload)
+        report.conforms shouldBe true
+      }
+    }
+  }
+
+  test("W-10983537: Regex hangs. Invalid payload") {
+    AMF.init().flatMap { _ =>
+      amf.Core.registerPlugin(PayloadValidatorPlugin)
+      val path        = "file://amf-cli/shared/src/test/resources/regex-hangs/specification.raml"
+      val parseResult = new RamlParser().parseFileAsync(path).asFuture
+      parseResult.flatMap { bu =>
+        val resolutionResult  = new Raml10Resolver().resolve(bu, ResolutionPipeline.EDITING_PIPELINE)
+        val shape             = resolutionResult.asInstanceOf[Document].declares.asSeq.head.asInstanceOf[AnyShape]
+        val validator         = shape.payloadValidator("application/json").asOption.get
+        val payload           = "{\"URL\": \"httpshttps://www.google.com/\", \"EventTypeId\": 10}"
+        val report            = validator.syncValidate("application/json", payload)
+        println(report.results.asSeq.map(_.message))
+        report.conforms shouldBe false
+      }
     }
   }
 }
