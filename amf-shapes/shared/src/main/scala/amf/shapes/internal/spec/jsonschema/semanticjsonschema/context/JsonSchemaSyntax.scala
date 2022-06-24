2c1f28f92aa146cefa8f521ce520688a30d4ada3
Tomas Fernandez <tfernandez@mulesoft.com>
2022-06-24 09:38:29 -0300

    W-11178013 (refactor): changed and moved JsonSchemaContext to JsonLdSchemaContext as it had specific overrides for that type of document

---
+++ amf-shapes/shared/src/main/scala/amf/shapes/internal/spec/jsonschema/semanticjsonschema/context/JsonSchemaSyntax.scala
@@ -0,0 +1,54 @@
+package amf.shapes.internal.spec.jsonschema.semanticjsonschema.context
+
+import amf.shapes.internal.spec.common.parser.SpecSyntax
+
+object JsonSchemaSyntax extends SpecSyntax {
+  override val nodes: Map[String, Set[String]] = Map(
+    "schema" -> Set(
+      "$ref",
+      "$schema",
+      "format",
+      "title",
+      "description",
+      "maximum",
+      "exclusiveMaximum",
+      "minimum",
+      "exclusiveMinimum",
+      "maxLength",
+      "minLength",
+      "nullable",
+      "pattern",
+      "maxItems",
+      "minItems",
+      "uniqueItems",
+      "maxProperties",
+      "minProperties",
+      "required",
+      "enum",
+      "type",
+      "items",
+      "additionalItems",
+      "collectionFormat",
+      "allOf",
+      "properties",
+      "additionalProperties",
+      "discriminator",
+      "readOnly",
+      "writeOnly",
+      "xml",
+      "deprecated",
+      "externalDocs",
+      "allOf",
+      "anyOf",
+      "oneOf",
+      "not",
+      "dependencies",
+      "multipleOf",
+      "default",
+      "example",
+      "id",
+      "name",
+      "patternProperties"
+    )
+  )
+}
