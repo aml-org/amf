bf24fb4dedb5ca855abedbbf3d7f36992c1c14ab
Loose <alejandro.loose@mulesoft.com>
2022-09-14 16:47:08 -0300

    W-11558572 - Fixed cross-spec validation and added test using cache

--- amf-cli/shared/src/test/scala/amf/crosspec/CrossSpecValidationTest.scala
+++ amf-cli/shared/src/test/scala/amf/crosspec/CrossSpecValidationTest.scala
@@ -1,11 +1,20 @@
 package amf.crosspec
 
+import amf.apicontract.client.scala.configuration.OasComponentConfiguration
+import amf.apicontract.client.scala.{AMFConfiguration, OASConfiguration}
+import amf.cache.CustomUnitCache
+import amf.core.client.scala.config.CachedReference
+import amf.core.client.scala.errorhandling.UnhandledErrorHandler
 import amf.validation.UniquePlatformReportGenTest
+
+import scala.concurrent.Future
 
 class CrossSpecValidationTest extends UniquePlatformReportGenTest {
 
   override val basePath: String    = "file://amf-cli/shared/src/test/resources/cross-spec/"
   override val reportsPath: String = "amf-cli/shared/src/test/resources/cross-spec/reports/"
+
+  // With fallback plugin
 
   test("RAML 1.0 API with include of OAS 3.0 API") {
     validate("raml-point-oas/api-root.raml", Some("raml-point-oas.report"))
@@ -17,6 +26,37 @@ class CrossSpecValidationTest extends UniquePlatformReportGenTest {
 
   test("OAS 2.0 API with ref to OAS 3.0 Component Module") {
     validate("oas2-point-oas3component/api-root.json", Some("oas2-point-oas3component.report"))
+  }
+
+  // Without fallback plugin (with cache)
+
+  test("OAS 2.0 API with ref to cached OAS 3.0 Component Module") {
+    withCachedReference(
+      OASConfiguration.OAS20(),
+      basePath + "oas2-point-oas3component/component.yaml",
+      OasComponentConfiguration.OAS30Component()
+    ).flatMap(configuration =>
+      validate(
+        "oas2-point-oas3component/api-root.json",
+        Some("oas2-point-oas3component-cached.report"),
+        configOverride = Some(configuration)
+      )
+    )
+  }
+
+  private def withCachedReference(
+      baseConfig: AMFConfiguration,
+      referencePath: String,
+      referenceConfig: AMFConfiguration
+  ): Future[AMFConfiguration] = {
+    val client = referenceConfig.withErrorHandlerProvider(() => UnhandledErrorHandler).baseUnitClient()
+    for {
+      parsed <- client.parse(referencePath).map(_.baseUnit)
+    } yield {
+      val reference = CachedReference(referencePath, parsed)
+      val cache     = CustomUnitCache(Seq(reference))
+      baseConfig.withUnitCache(cache)
+    }
   }
 
 }
