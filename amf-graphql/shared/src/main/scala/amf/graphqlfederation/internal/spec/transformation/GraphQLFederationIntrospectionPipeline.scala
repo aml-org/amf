395d14c75682f1384f1dfd9268d94bcc7462189c
Tomas Fernandez <tfernandez@mulesoft.com>
2022-09-19 09:49:07 -0300

    (chore): use lowercase 'introspection' instead of 'Introspection' in GraphQLIntrospectionPipeline. Delegate to ENUM

--- amf-graphql/shared/src/main/scala/amf/graphqlfederation/internal/spec/transformation/GraphQLFederationIntrospectionPipeline.scala
+++ amf-graphql/shared/src/main/scala/amf/graphqlfederation/internal/spec/transformation/GraphQLFederationIntrospectionPipeline.scala
@@ -1,11 +1,12 @@
 package amf.graphqlfederation.internal.spec.transformation
 
 import amf.apicontract.internal.transformation.GraphQLEditingPipeline
+import amf.core.client.common.transform.PipelineId
 import amf.core.client.scala.transform.{TransformationPipeline, TransformationStep}
 import amf.core.internal.transform.stages.UrlShortenerStage
 
 object GraphQLFederationIntrospectionPipeline extends TransformationPipeline {
-  override val name: String = "Introspection"
+  override val name: String = PipelineId.Introspection
 
   override def steps: Seq[TransformationStep] =
     GraphQLEditingPipeline().steps.filterNot(_.isInstanceOf[UrlShortenerStage]) ++ List(
