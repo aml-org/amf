1140743394a2c4e3c3e4e55b45b0b90e9f964c45
Loose <alejandro.loose@mulesoft.com>
2022-10-13 15:43:23 -0300

    W-11756604: added doc to JsonLDInstanceDocumentModel

--- amf-shapes/shared/src/main/scala/amf/shapes/internal/document/metamodel/JsonLDInstanceDocumentModel.scala
+++ amf-shapes/shared/src/main/scala/amf/shapes/internal/document/metamodel/JsonLDInstanceDocumentModel.scala
@@ -4,11 +4,12 @@ import amf.core.client.scala.model.domain.AmfObject
 import amf.core.client.scala.vocabulary.Namespace.Document
 import amf.core.client.scala.vocabulary.ValueType
 import amf.core.internal.metamodel.Field
+import amf.core.internal.metamodel.Type.Array
 import amf.core.internal.metamodel.document.{BaseUnitModel, DocumentModel}
-import amf.core.internal.metamodel.domain.{DomainElementModel, ModelDoc, ModelVocabularies}
+import amf.core.internal.metamodel.domain.{ModelDoc, ModelVocabularies}
 import amf.shapes.client.scala.model.document.JsonLDInstanceDocument
 import amf.shapes.client.scala.model.domain.jsonldinstance.JsonLDElementModel
-import amf.core.internal.metamodel.Type.Array
+
 object JsonLDInstanceDocumentModel extends BaseUnitModel {
 
   val Encodes: Field = Field(
@@ -24,4 +25,10 @@ object JsonLDInstanceDocumentModel extends BaseUnitModel {
   override val `type`: List[ValueType]  = (Document + "JsonLDInstanceDocument") :: DocumentModel.`type`
   override def modelInstance: AmfObject = JsonLDInstanceDocument()
   override def fields: List[Field]      = Encodes +: BaseUnitModel.fields
+
+  override val doc: ModelDoc = ModelDoc(
+    ModelVocabularies.AmlDoc,
+    "JsonLDInstanceDocument",
+    "A Document that represents a JSON-LD instance document"
+  )
 }
