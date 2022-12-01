d7392ec43434965ad9e4ac61eb43164b37f5eef6
arielmirra <amirra@salesforce.com>
2022-12-01 12:45:42 -0300

    (refactor): refactor SourceMapsAnnotationsTest for better legibility

--- amf-cli/shared/src/test/scala/amf/parser/SourceMapsAnnotationsTest.scala
+++ amf-cli/shared/src/test/scala/amf/parser/SourceMapsAnnotationsTest.scala
@@ -18,51 +18,53 @@ import org.mulesoft.common.client.lexical.PositionRange
 import org.scalatest.Assertion
 import org.scalatest.funsuite.AsyncFunSuite
 
+import scala.collection.GenTraversableOnce
 import scala.concurrent.{ExecutionContext, Future}
 
+/** iterates the BaseUnit checking if both field and value have the SourceAST or a VirtualNode annotation. The value may
+  * not be checked if its synthesized or an expression.
+  */
 class SourceMapsAnnotationsTest extends AsyncFunSuite with PlatformSecrets {
 
-  override implicit val executionContext = ExecutionContext.Implicits.global
-  private val directory: String          = "amf-cli/shared/src/test/resources/parser/annotations/"
+  override implicit val executionContext: ExecutionContext = ExecutionContext.Implicits.global
+  private val directory: String                            = "amf-cli/shared/src/test/resources/parser/annotations/"
 
   test("Test raml 1.0 annotations") {
-    runTest("raml10.raml", Raml10YamlHint)
+    runTest("raml10.raml")
   }
 
   test("Test raml 0.8 annotations") {
-    runTest("raml08.raml", Raml08YamlHint)
+    runTest("raml08.raml")
   }
 
   test("Test oas 2.0 annotations") {
-    runTest("oas20.yaml", Oas20YamlHint)
+    runTest("oas20.yaml")
   }
 
   test("Test oas 3.0 annotations") {
-    runTest("oas30.yaml", Oas30YamlHint)
+    runTest("oas30.yaml")
   }
 
   test("Test Async 2.0 annotations") {
-    runTest("async2.yaml", Async20YamlHint)
+    runTest("async2.yaml")
   }
 
   ignore("Test GraphQL annotations") {
-    val graphqlConfig: AMFConfiguration = GraphQLConfiguration.GraphQL()
-    runTest("graphql.graphql", GraphQLHint, Some(graphqlConfig))
+    runTest("graphql.graphql", Some(GraphQLConfiguration.GraphQL()))
   }
 
   ignore("Test GraphQLFederation annotations") {
-    val federationConfig: AMFConfiguration = GraphQLFederationConfiguration.GraphQLFederation()
-    runTest("federation.graphql", GraphQLFederationHint, Some(federationConfig))
+    runTest("federation.graphql", Some(GraphQLFederationConfiguration.GraphQLFederation()))
   }
 
-  private def build(file: String, hint: Hint, config: Option[AMFConfiguration] = None): Future[BaseUnit] = {
+  private def parse(file: String, config: Option[AMFConfiguration] = None): Future[BaseUnit] = {
     val url           = s"file://$directory$file"
     val configuration = config.getOrElse(APIConfiguration.API())
     AMFParser.parse(url, configuration).map(_.baseUnit)
   }
 
-  private def runTest(file: String, hint: Hint, config: Option[AMFConfiguration] = None): Future[Assertion] =
-    build(file, hint, config).map { bu =>
+  private def runTest(file: String, config: Option[AMFConfiguration] = None): Future[Assertion] =
+    parse(file, config).map { bu =>
       val strings = checkUnit(bu)
       if (strings.isEmpty) succeed
       else {
@@ -70,17 +72,18 @@ class SourceMapsAnnotationsTest extends AsyncFunSuite with PlatformSecrets {
       }
     }
 
-  private def checkUnit(u: BaseUnit): Seq[String] = {
+  private def orderResults(results: Seq[String]): Set[String] = results.sorted.toSet
+
+  private def checkUnit(u: BaseUnit): Set[String] = {
     u.fields.filter({ case (f, _) => f == DocumentModel.Encodes || f == DocumentModel.Declares })
-    checkElement(u, RangesChecker(None))
+    val results = checkElement(u, RangesChecker(None))
+    orderResults(results)
   }
 
   private def checkElement(e: AmfElement, rangesChecker: RangesChecker): Seq[String] =
     e match {
       case o: AmfObject =>
-        if (rangesChecker.insideSynthetized)
-          // fail?
-          Seq()
+        if (rangesChecker.insideSynthesized) Seq() // fail?
         else
           rangesChecker.lastRange match {
             case Some(r) if o.range().forall(oR => r.contains(oR)) => checkObj(o, rangesChecker.on(o))
@@ -88,9 +91,7 @@ class SourceMapsAnnotationsTest extends AsyncFunSuite with PlatformSecrets {
             case r                                                 => Seq() // Seq(rangeNotContained(o, r, o.range()))
           }
       case a: AmfArray =>
-        if (rangesChecker.insideSynthetized)
-          // fail?
-          Seq()
+        if (rangesChecker.insideSynthesized) Seq() // fail?
         else
           rangesChecker.lastRange match {
             case Some(r) if a.range().forall(oR => r.contains(oR)) =>
@@ -102,35 +103,39 @@ class SourceMapsAnnotationsTest extends AsyncFunSuite with PlatformSecrets {
     }
 
   private def checkFeRange(fe: FieldEntry, meta: String, rangesChecker: RangesChecker): Seq[String] =
-    if (rangesChecker.insideSynthetized || rangesChecker.lastRange.isEmpty) Seq()
+    if (rangesChecker.insideSynthesized || rangesChecker.lastRange.isEmpty) Seq()
     else {
       if (!fe.range().forall(fR => rangesChecker.lastRange.exists(_.contains(fR))))
         Seq() // Seq(rangeNotContained(fe.field, meta, rangesChecker.lastRange, fe.range()))
       else Seq()
     }
 
-  private def checkObj(o: AmfObject, rangesChecker: RangesChecker) =
+  private def checkInValue(fe: FieldEntry, meta: String, rangesChecker: RangesChecker): Seq[String] = {
+    if (fe.isSynthesized) Nil
+    else checkFeRange(fe, meta, rangesChecker) ++ checkElement(fe.value.value, rangesChecker.on(fe.value.value))
+  }
+
+  private def checkObj(o: AmfObject, rangesChecker: RangesChecker): Seq[String] =
     o.fields
       .fields()
-      .flatMap { fe =>
+      .flatMap { fe: FieldEntry =>
         val meta = o.meta.`type`.head.iri()
-        checkInField(fe, meta) ++ (if (fe.isSynthesized) Nil
-                                   else
-                                     checkFeRange(fe, meta, rangesChecker) ++
-                                       checkElement(fe.value.value, rangesChecker.on(fe.value.value)))
+        checkInField(fe, meta) ++ checkInValue(fe, meta, rangesChecker)
       }
       .toSeq
 
   implicit class FieldEntryIm(fe: FieldEntry) {
-    def isSynthesized: Boolean = fe.value.annotations.contains(classOf[SynthesizedField])
+    def isSynthesized: Boolean = fe.value.annotations.isSynthesized
 
     def isExpression: Boolean = fe.value.annotations.find(_.isInstanceOf[ExpressionMember]).isDefined
 
-    def fieldHasMaps(): Boolean = checkAnnotations(fe.value.annotations)
+    def valueShouldHaveMaps(): Boolean = !fe.isSynthesized && !fe.isExpression
 
-    def valueHasMaps(): Boolean = checkAnnotations(fe.value.value.annotations)
+    def fieldHasMaps(): Boolean = hasAnnotations(fe.value.annotations)
 
-    private def checkAnnotations(a: Annotations): Boolean =
+    def valueHasMaps(): Boolean = hasAnnotations(fe.value.value.annotations)
+
+    private def hasAnnotations(a: Annotations): Boolean =
       a.contains(classOf[SourceAST]) || a.find(_.isInstanceOf[VirtualNode]).isDefined
 
     def range(): Option[PositionRange] =
@@ -148,11 +153,11 @@ class SourceMapsAnnotationsTest extends AsyncFunSuite with PlatformSecrets {
         (range.lineTo > other.lineTo || (range.lineTo == other.lineTo && range.columnTo >= other.columnTo))
   }
 
-  private def checkInField(fe: FieldEntry, meta: String) = {
-    val f = if (!fe.fieldHasMaps()) Some(missingField(fe.field, meta)) else None
-    val v =
-      if (!fe.valueHasMaps() && !fe.isSynthesized && !fe.isExpression) Some(missingValue(fe.field, meta)) else None
-    (f ++ v).toSeq
+  private def checkInField(fe: FieldEntry, meta: String): Seq[String] = {
+    val fieldStrings = if (!fe.fieldHasMaps()) Some(missingField(fe.field, meta)) else None
+    val valueStrings =
+      if (fe.valueShouldHaveMaps() && !fe.valueHasMaps()) Some(missingValue(fe.field, meta)) else None
+    (fieldStrings ++ valueStrings).toSeq
   }
 
   private def missingField(f: Field, meta: String) =
@@ -167,11 +172,11 @@ class SourceMapsAnnotationsTest extends AsyncFunSuite with PlatformSecrets {
   private def rangeNotContained(f: Field, meta: String, parent: Option[PositionRange], self: Option[PositionRange]) =
     s"range is not contained in parent [$parent - $self] of field ${f.value.iri()} at obj type $meta"
 
-  case class RangesChecker(lastRange: Option[PositionRange], insideSynthetized: Boolean = false) {
+  case class RangesChecker(lastRange: Option[PositionRange], insideSynthesized: Boolean = false) {
     def on(e: AmfElement): RangesChecker =
-      if (insideSynthetized) this
-      else if (e.annotations.contains(classOf[Inferred])) this
-      else if (e.annotations.contains(classOf[SynthesizedField])) RangesChecker(lastRange, true)
+      if (insideSynthesized) this
+      else if (e.annotations.isInferred) this
+      else if (e.annotations.isSynthesized) RangesChecker(lastRange, insideSynthesized = true)
       else RangesChecker(e.range())
   }
 }
