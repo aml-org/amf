da53c5e6b78ab923564822b27bf56c2f27b86b4d
Hernán Najles <50325041+pope1838@users.noreply.github.com>
2023-01-06 19:28:52 -0300

    Fix client list at jsonld object interfaces (#1698)

--- amf-shapes/shared/src/main/scala/amf/shapes/client/platform/model/domain/jsonldinstance/JsonLDObject.scala
+++ amf-shapes/shared/src/main/scala/amf/shapes/client/platform/model/domain/jsonldinstance/JsonLDObject.scala
@@ -6,8 +6,9 @@ import amf.shapes.internal.domain.metamodel.jsonldschema.JsonLDEntityModel
 import amf.shapes.internal.spec.jsonldschema.parser.JsonPath
 import amf.shapes.internal.convert.ShapeClientConverters._
 
+import scala.concurrent.ExecutionContext
 import scala.scalajs.js.annotation.{JSExportAll, JSExportTopLevel}
-
+import scala.concurrent.ExecutionContext.Implicits.global
 @JSExportAll
 class JsonLDObject(override private[amf] val _internal: InternalJsonLDObject) extends DomainElement with JsonLDElement {
   @JSExportTopLevel("JsonLDObject")
@@ -24,18 +25,18 @@ class JsonLDObject(override private[amf] val _internal: InternalJsonLDObject) ex
 
   def withProperty(property: String, value: JsonLDObject): JsonLDObject = _internal.withProperty(property, value)
 
-  def withStringPropertyCollection(property: String, values: Seq[String]): JsonLDObject =
-    _internal.withStringPropertyCollection(property, values)
+  def withStringPropertyCollection(property: String, values: ClientList[String]): JsonLDObject =
+    _internal.withStringPropertyCollection(property, values.asInternal)
 
-  def withIntPropertyCollection(property: String, values: Seq[Integer]): JsonLDObject =
-    _internal.withIntPropertyCollection(property, values)
+  def withIntPropertyCollection(property: String, values: ClientList[Int]): JsonLDObject =
+    _internal.withIntPropertyCollection(property, values.asInternal)
 
-  def withFloatPropertyCollection(property: String, values: Seq[Float]): JsonLDObject =
-    _internal.withFloatPropertyCollection(property, values)
+  def withFloatPropertyCollection(property: String, values: ClientList[Float]): JsonLDObject =
+    _internal.withFloatPropertyCollection(property, values.asInternal)
 
-  def withBoolPropertyCollection(property: String, values: Seq[Boolean]): JsonLDObject =
-    _internal.withBoolPropertyCollection(property, values)
+  def withBoolPropertyCollection(property: String, values: ClientList[Boolean]): JsonLDObject =
+    _internal.withBoolPropertyCollection(property, values.asInternal)
 
-  def withObjPropertyCollection(property: String, values: Seq[JsonLDObject]): JsonLDObject =
-    _internal.withObjPropertyCollection(property, values)
+  def withObjPropertyCollection(property: String, values: ClientList[JsonLDObject]): JsonLDObject =
+    _internal.withObjPropertyCollection(property, values.asInternal)
 }
