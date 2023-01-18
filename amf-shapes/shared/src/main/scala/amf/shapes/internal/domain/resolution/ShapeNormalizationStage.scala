6014a9bd10d30351971e581316e567180ef07443
nschejtman <nschejtman93@gmail.com>
2023-01-18 17:20:04 -0300

    Refactor > clarify names for reference updating classes

--- amf-shapes/shared/src/main/scala/amf/shapes/internal/domain/resolution/ShapeNormalizationStage.scala
+++ amf-shapes/shared/src/main/scala/amf/shapes/internal/domain/resolution/ShapeNormalizationStage.scala
@@ -6,28 +6,27 @@ import amf.core.client.scala.errorhandling.AMFErrorHandler
 import amf.core.client.scala.model.document.BaseUnit
 import amf.core.client.scala.model.domain.{DomainElement, Shape}
 import amf.core.client.scala.transform.TransformationStep
-import amf.core.internal.transform.stages.elements.resolution.ElementStageTransformer
 import amf.core.internal.transform.stages.selectors.ShapeSelector
 import amf.shapes.internal.domain.resolution.shape_normalization.{NormalizationContext, ShapeInheritanceResolver}
 
 /** Computes the canonical form for all the shapes in the model We are assuming certain pre-conditions in the state of
-  * the shape:
-  *   - All type references have been replaced by their expanded forms
-  */
+ * the shape:
+ *   - All type references have been replaced by their expanded forms
+ */
 class ShapeNormalizationStage(profile: ProfileName, val keepEditingInfo: Boolean) extends TransformationStep {
   override def transform(
-      model: BaseUnit,
-      errorHandler: AMFErrorHandler,
-      configuration: AMFGraphConfiguration
-  ): BaseUnit =
+                          model: BaseUnit,
+                          errorHandler: AMFErrorHandler,
+                          configuration: AMFGraphConfiguration
+                        ): BaseUnit =
     new ShapeNormalization(profile, keepEditingInfo)(errorHandler).transform(model, configuration)
 
   private class ShapeNormalization(profile: ProfileName, val keepEditingInfo: Boolean)(implicit
-      val errorHandler: AMFErrorHandler
+                                                                                       val errorHandler: AMFErrorHandler
   ) {
 
     protected var m: Option[BaseUnit] = None
-    protected val context             = new NormalizationContext(errorHandler, keepEditingInfo, profile)
+    private val context = new NormalizationContext(errorHandler, keepEditingInfo, profile)
 
     def transform[T <: BaseUnit](model: T, configuration: AMFGraphConfiguration): T = {
       m = Some(model)
@@ -35,22 +34,26 @@ class ShapeNormalizationStage(profile: ProfileName, val keepEditingInfo: Boolean
       // Performance?
       model.iterator().foreach {
         case s: Shape => ShapeInheritanceResolver(s, context)
-        case _        =>
+        case _ =>
       }
-      model.transform(ShapeSelector, transformReferences(_, _, configuration)).asInstanceOf[T]
+      model.transform(ShapeSelector, updateReferences(_, _, configuration)).asInstanceOf[T]
     }
 
-    protected def transformReferences(
-        element: DomainElement,
-        isCycle: Boolean,
-        configuration: AMFGraphConfiguration
-    ): Option[DomainElement] = {
+    private def updateReferences(
+                                  element: DomainElement,
+                                  isCycle: Boolean,
+                                  configuration: AMFGraphConfiguration
+                                ): Option[DomainElement] = {
       element match {
-        case shape: Shape => referencesTransformer.transform(shape, configuration)
-        case other        => Some(other)
+
+        case shape: Shape =>
+          val updater = new ShapeReferencesUpdaterTransformer(context)
+          updater.transform(shape, configuration)
+
+        case other => Some(other)
       }
     }
 
-    def referencesTransformer: ElementStageTransformer[Shape] = new ShapeTransformer(context)
   }
+
 }
