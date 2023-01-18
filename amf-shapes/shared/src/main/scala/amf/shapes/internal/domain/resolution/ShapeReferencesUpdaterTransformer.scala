91310b431e199d551a74da3994ae548bbcac511c
nschejtman <nschejtman93@gmail.com>
2023-01-18 17:20:04 -0300

    Refactor > clarify names for reference updating classes

---
+++ amf-shapes/shared/src/main/scala/amf/shapes/internal/domain/resolution/ShapeReferencesUpdaterTransformer.scala
@@ -0,0 +1,23 @@
+package amf.shapes.internal.domain.resolution
+
+import amf.core.client.common.validation.ProfileName
+import amf.core.client.scala.AMFGraphConfiguration
+import amf.core.client.scala.errorhandling.AMFErrorHandler
+import amf.core.client.scala.model.domain.Shape
+import amf.core.internal.transform.stages.elements.resolution.ElementStageTransformer
+import amf.shapes.internal.domain.resolution.shape_normalization.{NormalizationContext, ShapeReferencesUpdater, ShapeInheritanceResolver}
+
+class ShapeReferencesUpdaterTransformer(context: NormalizationContext) extends ElementStageTransformer[Shape] {
+
+  override def transform(shape: Shape, configuration: AMFGraphConfiguration): Option[Shape] = {
+    val updatedShape = ShapeReferencesUpdater()(context).update(shape)
+    Some(updatedShape)
+  }
+}
+
+object ShapeReferencesUpdaterTransformer {
+  def apply(errorHandler: AMFErrorHandler, keepEditingInfo: Boolean, profileName: ProfileName): ShapeReferencesUpdaterTransformer = {
+    val context = new NormalizationContext(errorHandler, keepEditingInfo, profileName)
+    new ShapeReferencesUpdaterTransformer(context)
+  }
+}
