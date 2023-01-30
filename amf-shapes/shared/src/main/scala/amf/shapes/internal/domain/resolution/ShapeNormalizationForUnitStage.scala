b49a43237af91f28b7572e01ade75570f138bbe1
nschejtman <nschejtman93@gmail.com>
2023-01-30 11:56:02 -0300

    Refactor Shape Normalization stages names (Unit & Element) + Fix Element stage not computing inheritance

--- amf-shapes/shared/src/main/scala/amf/shapes/internal/domain/resolution/ShapeNormalizationForUnitStage.scala
+++ amf-shapes/shared/src/main/scala/amf/shapes/internal/domain/resolution/ShapeNormalizationForUnitStage.scala
@@ -7,33 +7,38 @@ import amf.core.client.scala.model.document.BaseUnit
 import amf.core.client.scala.model.domain.{DomainElement, Shape}
 import amf.core.client.scala.transform.TransformationStep
 import amf.core.internal.transform.stages.selectors.ShapeSelector
-import amf.shapes.internal.domain.resolution.shape_normalization.{NormalizationContext, ShapeInheritanceResolver}
+import amf.shapes.internal.domain.resolution.shape_normalization.{
+  NormalizationContext,
+  ShapeInheritanceResolver,
+  ShapeReferencesUpdater
+}
 
 /** Computes the canonical form for all the shapes in the model We are assuming certain pre-conditions in the state of
- * the shape:
- *   - All type references have been replaced by their expanded forms
- */
-class ShapeNormalizationStage(profile: ProfileName, val keepEditingInfo: Boolean) extends TransformationStep {
+  * the shape:
+  *   - All type references have been replaced by their expanded forms
+  */
+class ShapeNormalizationForUnitStage(profile: ProfileName, val keepEditingInfo: Boolean) extends TransformationStep {
   override def transform(
-                          model: BaseUnit,
-                          eh: AMFErrorHandler,
-                          configuration: AMFGraphConfiguration
-                        ): BaseUnit = {
+      model: BaseUnit,
+      eh: AMFErrorHandler,
+      configuration: AMFGraphConfiguration
+  ): BaseUnit = {
     val context = new NormalizationContext(eh, keepEditingInfo, profile)
-
 
     // Step 1: resolve inheritance
     model.iterator().foreach {
       case s: Shape => ShapeInheritanceResolver()(context).normalize(s)
-      case _ =>
+      case _        =>
     }
 
-    def updateReferences(element: DomainElement, isCycle: Boolean, configuration: AMFGraphConfiguration): Option[DomainElement] = {
+    def updateReferences(
+        element: DomainElement,
+        isCycle: Boolean,
+        configuration: AMFGraphConfiguration
+    ): Option[DomainElement] = {
       element match {
-        case shape: Shape =>
-          val updater = new ShapeReferencesUpdaterTransformer(context)
-          updater.transform(shape, configuration)
-        case other => Some(other)
+        case shape: Shape => Some(ShapeReferencesUpdater()(context).update(shape))
+        case other        => Some(other)
       }
     }
 
