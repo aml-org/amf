a93a658d9153b7840ebc5f75bbd7004af94f95b9
nschejtman <nschejtman93@gmail.com>
2023-02-17 16:16:31 -0300

    Refactor: make explicit some implicit arguments

--- amf-shapes/shared/src/main/scala/amf/shapes/internal/domain/resolution/ShapeNormalizationForElementStage.scala
+++ amf-shapes/shared/src/main/scala/amf/shapes/internal/domain/resolution/ShapeNormalizationForElementStage.scala
@@ -3,20 +3,15 @@ package amf.shapes.internal.domain.resolution
 import amf.core.client.common.validation.ProfileName
 import amf.core.client.scala.AMFGraphConfiguration
 import amf.core.client.scala.errorhandling.AMFErrorHandler
-import amf.core.client.scala.model.document.Document
 import amf.core.client.scala.model.domain.Shape
 import amf.core.internal.transform.stages.elements.resolution.ElementStageTransformer
-import amf.shapes.internal.domain.resolution.shape_normalization.{
-  NormalizationContext,
-  ShapeInheritanceResolver,
-  ShapeReferencesUpdater
-}
+import amf.shapes.internal.domain.resolution.shape_normalization.{NormalizationContext, ShapeInheritanceResolver, ShapeReferencesUpdater}
 
 class ShapeNormalizationForElementStage(context: NormalizationContext) extends ElementStageTransformer[Shape] {
 
   override def transform(shape: Shape, configuration: AMFGraphConfiguration): Option[Shape] = {
-    val resolvedInheritance = ShapeInheritanceResolver()(context).normalize(shape)
-    val fixedReferences     = ShapeReferencesUpdater()(context).update(resolvedInheritance)
+    val resolvedInheritance = ShapeInheritanceResolver(context).normalize(shape)
+    val fixedReferences     = ShapeReferencesUpdater(context).update(resolvedInheritance)
     Some(fixedReferences)
   }
 }
