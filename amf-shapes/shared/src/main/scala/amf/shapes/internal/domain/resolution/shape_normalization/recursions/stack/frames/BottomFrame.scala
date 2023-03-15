b8645d777a5bb68de6ef09e6c52731dffad4f853
nschejtman <nschejtman93@gmail.com>
2023-03-15 11:22:19 -0300

    (fix): detect recursion in substack.

--- amf-shapes/shared/src/main/scala/amf/shapes/internal/domain/resolution/shape_normalization/recursions/stack/frames/BottomFrame.scala
+++ amf-shapes/shared/src/main/scala/amf/shapes/internal/domain/resolution/shape_normalization/recursions/stack/frames/BottomFrame.scala
@@ -2,6 +2,15 @@ package amf.shapes.internal.domain.resolution.shape_normalization.recursions.sta
 
 import amf.core.client.scala.model.domain.Shape
 
+object BottomFrame {
+  def toBottomFrame(frame: Frame): BottomFrame = {
+    frame match {
+      case b: BottomFrame => b
+      case f: Frame       => BottomFrame(f.shape)
+    }
+  }
+}
+
 case class BottomFrame(shape: Shape) extends Frame {
   override def toString: String = s"Frame($prettyPrintShape)"
 }
