5111268d07199cac296d07904aa826856b56a9e1
nschejtman <nschejtman93@gmail.com>
2023-03-27 12:12:37 -0300

    (performance): skip dead ends

--- amf-shapes/shared/src/main/scala/amf/shapes/internal/domain/resolution/AmfUpdaterIterator.scala
+++ amf-shapes/shared/src/main/scala/amf/shapes/internal/domain/resolution/AmfUpdaterIterator.scala
@@ -1,13 +1,26 @@
 package amf.shapes.internal.domain.resolution
 import amf.core.client.scala.model.document.BaseUnit
-import amf.core.client.scala.model.domain.{AmfArray, AmfElement, AmfObject}
+import amf.core.client.scala.model.domain.{AmfArray, AmfElement, AmfObject, DataNode}
 import amf.core.client.scala.traversal.iterator.{AmfIterator, InstanceCollector, VisitedCollector}
+import amf.shapes.client.scala.model.domain.Example
 
 import scala.collection.immutable.Queue
 
 case class AmfUpdaterIterator private (var buffer: Queue[AmfElement], updater: AmfElement => AmfElement)
     extends AmfIterator {
   private val visited: VisitedCollector = InstanceCollector()
+
+  /** This is a performance improvement. We are interested only in traversing AmfElements in which we could find a Shape
+    * that might need to be updated in their sub-tree. That said, we know (from the model) that some elements are
+    * dead-ends. This method hints the traversal to skip these elements to improve performance.
+    */
+  private def shouldSkip(o: AmfElement): Boolean = {
+    o match {
+      case _: Example  => true
+      case _: DataNode => true
+      case _           => false
+    }
+  }
 
   override def hasNext: Boolean = {
     if (buffer.nonEmpty) {
@@ -26,7 +39,7 @@ case class AmfUpdaterIterator private (var buffer: Queue[AmfElement], updater: A
     if (!visited.visited(current)) {
       val next = nextElements(current)
         .map(updater)
-        .filter(!visited.visited(_))
+        .filter(e => !visited.visited(e) && !shouldSkip(e))
       buffer = buffer ++ next
       visited += current
       current
