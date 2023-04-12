4882134959f28c6c529d6bf2a0da4ff4c8c4a3b9
Hernán Najles <50325041+pope1838@users.noreply.github.com>
2023-04-12 10:48:38 -0300

    W-12784127: add json ld instance document wrap fn converter (#1774)

---
+++ amf-shapes/jvm/src/test/scala/amf/shapes/client/jsonldschema/platform/JvmJsonLDInstancePlatformTest.scala
@@ -0,0 +1,5 @@
+package amf.shapes.client.jsonldschema.platform
+
+import amf.core.internal.convert.NativeOpsFromJvm
+
+class JvmJsonLDInstancePlatformTest extends JsonLDInstancePlatformTest with NativeOpsFromJvm
