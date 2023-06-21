5607ac4a7ca79d16a9c25463e764454ea571b287
nschejtman <nschejtman93@gmail.com>
2023-06-21 17:06:08 -0300

    Added tests

--- amf-cli/shared/src/test/scala/amf/validation/Raml10ValidationTest.scala
+++ amf-cli/shared/src/test/scala/amf/validation/Raml10ValidationTest.scala
@@ -15,4 +15,74 @@ class Raml10ValidationTest extends AbstractValidationTest {
     assertReport("api.raml", "api.report", s"$path/sub-schemas")
   }
 
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Lexicals ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // external example
+  test("Test failing external example in endpoint") {
+    assertReport("api.raml", "api.report", s"$path/lexicals/failing-external-example-in-endpoint")
+  }
+
+  test("Test failing external example in parametrized rt") {
+    assertReport("api.raml", "api.report", s"$path/lexicals/failing-external-example-in-parametrized-rt")
+  }
+
+  test("Test failing external example in rt") {
+    assertReport("api.raml", "api.report", s"$path/lexicals/failing-external-example-in-rt")
+  }
+
+  // external typed example
+
+  test("Test failing external typed example in endpoint") {
+    assertReport("api.raml", "api.report", s"$path/lexicals/failing-external-typed-example-in-endpoint")
+  }
+
+  test("Test failing external typed example in parametrized rt") {
+    assertReport("api.raml", "api.report", s"$path/lexicals/failing-external-typed-example-in-parametrized-rt")
+  }
+
+  test("Test failing external typed example in rt") {
+    assertReport("api.raml", "api.report", s"$path/lexicals/failing-external-typed-example-in-rt")
+  }
+
+  // inlined example
+
+  test("Test failing inline example in endpoint") {
+    assertReport("api.raml", "api.report", s"$path/lexicals/failing-inline-example-in-endpoint")
+  }
+
+  test("Test failing inline example in parametrized rt") {
+    assertReport("api.raml", "api.report", s"$path/lexicals/failing-inline-example-in-parametrized-rt")
+  }
+
+  test("Test failing inline example in rt") {
+    assertReport("api.raml", "api.report", s"$path/lexicals/failing-inline-example-in-rt")
+  }
+
+  // external type
+
+  test("Test failing external type in endpoint") {
+    assertReport("api.raml", "api.report", s"$path/lexicals/failing-external-type-in-endpoint")
+  }
+
+  test("Test failing external type in parametrized rt") {
+    assertReport("api.raml", "api.report", s"$path/lexicals/failing-external-type-in-parametrized-rt")
+  }
+
+  test("Test failing external type in rt") {
+    assertReport("api.raml", "api.report", s"$path/lexicals/failing-external-type-in-rt")
+  }
+
+  // inlined type
+  test("Test failing inline type in endpoint") {
+    assertReport("api.raml", "api.report", s"$path/lexicals/failing-inline-type-in-endpoint")
+  }
+
+  test("Test failing inline type in parametrized rt") {
+    assertReport("api.raml", "api.report", s"$path/lexicals/failing-inline-type-in-parametrized-rt")
+  }
+
+  test("Test failing inline type in rt") {
+    assertReport("api.raml", "api.report", s"$path/lexicals/failing-inline-type-in-rt")
+  }
+
+
 }
