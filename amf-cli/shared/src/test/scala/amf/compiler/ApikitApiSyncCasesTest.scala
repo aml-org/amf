ea06acb18db73d829cfdfcf3d90b2dbc56af7770
Tomas Fernandez <tfernandez@mulesoft.com>
2023-06-29 19:18:10 -0300

    (refactor): make test suite more readable

--- amf-cli/shared/src/test/scala/amf/compiler/ApikitApiSyncCasesTest.scala
+++ amf-cli/shared/src/test/scala/amf/compiler/ApikitApiSyncCasesTest.scala
@@ -1,193 +1,152 @@
 package amf.compiler
 
-import amf.apicontract.client.scala.{RAMLConfiguration, WebAPIConfiguration}
+import amf.apicontract.client.scala.{AMFBaseUnitClient, APIConfiguration}
 import amf.core.client.common.remote.Content
 import amf.core.client.common.transform.PipelineId
 import amf.core.client.scala.AMFGraphConfiguration
-import amf.core.client.scala.errorhandling.DefaultErrorHandler
 import amf.core.client.scala.resource.ResourceLoader
-import amf.core.internal.parser.{AMFCompiler, CompilerConfiguration}
-import amf.core.internal.remote.{Cache, Context, FileNotFound}
+import amf.core.internal.remote.Spec.{OAS20, OAS30, RAML10}
+import amf.core.internal.remote.{FileNotFound, Spec}
 import amf.core.internal.unsafe.PlatformSecrets
 import org.mulesoft.common.test.AsyncBeforeAndAfterEach
 import org.scalatest.matchers.should.Matchers
 
+import scala.collection.mutable
 import scala.concurrent.{ExecutionContext, Future}
 
 class ApikitApiSyncCasesTest extends AsyncBeforeAndAfterEach with PlatformSecrets with Matchers {
 
   override implicit val executionContext: ExecutionContext = ExecutionContext.Implicits.global
 
+  private val base = "file://amf-cli/shared/src/test/resources/compiler/apikit-apisync"
+
   test("Resource loader shouldn't have absolute path if ref and base aren't absolute") {
-    val mappings = Map(
-      "resource::really-cool-urn:1.0.0:raml:zip:main.raml" -> CustomContentResult(
-        "file://amf-cli/shared/src/test/resources/compiler/apikit-apisync/ref-base-not-absolute/main.raml"
-      ),
-      "something.raml" -> CustomContentResult(
-        "file://amf-cli/shared/src/test/resources/compiler/apikit-apisync/ref-base-not-absolute/something.raml"
-      ),
-      "examples/something/get-something-response.raml" -> CustomContentResult(
-        "file://amf-cli/shared/src/test/resources/compiler/apikit-apisync/ref-base-not-absolute/examples/something/get-something-response.raml"
-      ),
-      "examples/something/put-something-request.raml" -> CustomContentResult(
-        "file://amf-cli/shared/src/test/resources/compiler/apikit-apisync/ref-base-not-absolute/examples/something/put-something-request.raml"
-      ),
-      "examples/common/async-response.raml" -> CustomContentResult(
-        "file://amf-cli/shared/src/test/resources/compiler/apikit-apisync/ref-base-not-absolute/examples/common/async-response.raml"
-      ),
-      "libraries/resourceTypes.raml" -> CustomContentResult(
-        "file://amf-cli/shared/src/test/resources/compiler/apikit-apisync/ref-base-not-absolute/libraries/resourceTypes.raml"
-      )
-    )
-    val url = "resource::really-cool-urn:1.0.0:raml:zip:main.raml"
-    val eh  = DefaultErrorHandler()
-    AMFCompiler(
-      url,
-      base = Context(platform),
-      cache = Cache(),
-      CompilerConfiguration(
-        WebAPIConfiguration
-          .WebAPI()
-          .withResourceLoaders(List(new URNResourceLoader(mappings)))
-          .withErrorHandlerProvider(() => eh)
-      )
-    ).build()
-      .map { _ =>
-        eh.getResults should have size 0
-      }
+    val main = "resource::really-cool-urn:1.0.0:raml:zip:main.raml"
+    val builder = new UrnLoaderBuilder(s"$base/ref-base-not-absolute")
+      .addMapping(main, "main.raml")
+      .addMapping("something.raml")
+      .addMapping("examples/something/get-something-response.raml")
+      .addMapping("examples/something/get-something-request.raml")
+      .addMapping("examples/something/put-something-request.raml")
+      .addMapping("examples/common/async-response.raml")
+      .addMapping("libraries/resourceTypes.raml")
+    val loader = builder.build()
+    val client = getClient(loader, RAML10)
+    client.parse(main).map { result =>
+      result.results should have size 0
+    }
   }
 
   // APIMF-3118
   test("Resource loader should find nested references") {
-    val mappings = Map(
-      "resource::37fab092-be99-4538-b5ce-b004c5439f6d:refexample:1.0.1:oas:zip:example.json" -> CustomContentResult(
-        "file://amf-cli/shared/src/test/resources/compiler/apikit-apisync/uri-file-prefix/example.json"
-      ),
-      "exchange.json" -> CustomContentResult(
-        "file://amf-cli/shared/src/test/resources/compiler/apikit-apisync/uri-file-prefix/exchange.json"
-      ),
-      "components/schemas/okResponse.json" -> CustomContentResult(
-        "file://amf-cli/shared/src/test/resources/compiler/apikit-apisync/uri-file-prefix/components/schemas/okResponse.json"
-      ),
-      "components/schemas/item.json" -> CustomContentResult(
-        "file://amf-cli/shared/src/test/resources/compiler/apikit-apisync/uri-file-prefix/components/schemas/item.json"
-      ),
-      "components/schemas/nested.json" -> CustomContentResult(
-        "file://amf-cli/shared/src/test/resources/compiler/apikit-apisync/uri-file-prefix/components/schemas/nested.json"
-      )
-    )
-    val url = "resource::37fab092-be99-4538-b5ce-b004c5439f6d:refexample:1.0.1:oas:zip:example.json"
-    val client = WebAPIConfiguration
-      .WebAPI()
-      .withResourceLoaders(List(new URNResourceLoader(mappings)))
-      .baseUnitClient()
-    client.parse(url).map { parseResult =>
+    val main = "resource::37fab092-be99-4538-b5ce-b004c5439f6d:refexample:1.0.1:oas:zip:example.json"
+    val loader = new UrnLoaderBuilder(s"$base/uri-file-prefix")
+      .addMapping(main, "example.json")
+      .addMapping("exchange.json")
+      .addMapping("components/schemas/okResponse.json")
+      .addMapping("components/schemas/item.json")
+      .addMapping("components/schemas/nested.json")
+      .build()
+    val client = getClient(loader, OAS30)
+    client.parse(main).map { parseResult =>
       parseResult.results should have size 0
     }
   }
 
   test("Parsing context generated in extends resolution stage for raml traits") {
-    val mappings = Map(
-      "resource::really-cool-urn:1.0.0:raml:zip:townfile.raml" -> CustomContentResult(
-        "file://amf-cli/shared/src/test/resources/compiler/apikit-apisync/extends-stage-traits/townfile.raml"
-      ),
-      "some-modules/for-health-check.raml" -> CustomContentResult(
-        "file://amf-cli/shared/src/test/resources/compiler/apikit-apisync/extends-stage-traits/some-modules/for-health-check.raml"
-      ),
-      "some-modules/trait.raml" -> CustomContentResult(
-        "file://amf-cli/shared/src/test/resources/compiler/apikit-apisync/extends-stage-traits/some-modules/trait.raml"
-      )
-    )
-    val url = "resource::really-cool-urn:1.0.0:raml:zip:townfile.raml"
-    val eh  = DefaultErrorHandler()
-    val client = RAMLConfiguration
-      .RAML10()
-      .withResourceLoaders(List(new URNResourceLoader(mappings)))
-      .withErrorHandlerProvider(() => eh)
-      .baseUnitClient()
+    val main = "resource::really-cool-urn:1.0.0:raml:zip:townfile.raml"
+    val loader = new UrnLoaderBuilder(s"$base/extends-stage-traits")
+      .addMapping(main, "townfile.raml")
+      .addMapping("some-modules/for-health-check.raml")
+      .addMapping("some-modules/trait.raml")
+      .build()
+    val client = getClient(loader, RAML10)
     for {
-      parseResult <- client.parse(url)
-      _           <- Future.successful(client.transform(parseResult.baseUnit, PipelineId.Editing))
+      parseResult <- client.parse(main)
+      transformResult = client.transform(parseResult.baseUnit, PipelineId.Editing)
     } yield {
-      eh.getResults should have size 0
+      parseResult.results should have size 0
+      transformResult.results should have size 0
     }
   }
 
   // APIMF-3384
   test("should accept content URLs starting with 'jar:'") {
-    val mappings = Map(
-      "resource::com.mycompany:consumer-api:1.0.0:oas:zip:consumer.yaml" -> CustomContentResult(
-        "file://amf-cli/shared/src/test/resources/compiler/apikit-apisync/jar-protocol/consumer.yaml"
-      ),
-      "utility.yaml" -> CustomContentResult(
-        "file://amf-cli/shared/src/test/resources/compiler/apikit-apisync/jar-protocol/utility.yaml"
-      )
-    )
-    val url = "resource::com.mycompany:consumer-api:1.0.0:oas:zip:consumer.yaml"
-    val client = WebAPIConfiguration
-      .WebAPI()
-      .withResourceLoaders(List(new URNResourceLoader(mappings)))
-      .baseUnitClient()
-    client.parse(url).map { parseResult =>
+    val main = "resource::com.mycompany:consumer-api:1.0.0:oas:zip:consumer.yaml"
+    val loader = new UrnLoaderBuilder(s"$base/jar-protocol")
+      .addMapping(main, "consumer.yaml")
+      .addMapping("utility.yaml")
+      .build()
+    val client = getClient(loader, OAS30)
+    client.parse(main).map { parseResult =>
       parseResult.results should have size 0
     }
   }
 
   // APIMF-3600
   test("exchange modules with multiple zip sources") {
-    val mappings = Map(
-      "resource::8fe5354c-e64c-4eaa-addc-b50906a0b48c:se-23375:1.0.1:oas:zip:se-23375.json" -> CustomContentResult(
-        "file://amf-cli/shared/src/test/resources/compiler/apikit-apisync/exchange-modules/se-23375.json"
-      ),
-      "exchange_modules/8fe5354c-e64c-4eaa-addc-b50906a0b48c/datamodel-tmforum/1.0.0/4.1.0/Customer/Bucket.schema.json" -> CustomContentResult(
-        "file://amf-cli/shared/src/test/resources/compiler/apikit-apisync/exchange-modules/Bucket.schema.json"
+    val main = "resource::8fe5354c-e64c-4eaa-addc-b50906a0b48c:se-23375:1.0.1:oas:zip:se-23375.json"
+    val loader = new UrnLoaderBuilder(s"$base/exchange-modules")
+      .addMapping(main, "se-23375.json")
+      .addMapping(
+        "exchange_modules/8fe5354c-e64c-4eaa-addc-b50906a0b48c/datamodel-tmforum/1.0.0/4.1.0/Customer/Bucket.schema.json",
+        "Bucket.schema.json"
       )
-    )
-    val url = "resource::8fe5354c-e64c-4eaa-addc-b50906a0b48c:se-23375:1.0.1:oas:zip:se-23375.json"
-    val client = WebAPIConfiguration
-      .WebAPI()
-      .withResourceLoaders(List(new URNResourceLoader(mappings)))
-      .baseUnitClient()
-    client.parse(url).map { parseResult =>
+      .build()
+    val client: AMFBaseUnitClient = getClient(loader, OAS20)
+    client.parse(main).map { parseResult =>
       parseResult.results should have size 0
     }
   }
 
   // APIMF-3533
+  private def getClient(loader: ResourceLoader, spec: Spec) = getConfig(loader, spec).baseUnitClient()
+  private def getConfig(loader: ResourceLoader, spec: Spec) = {
+    APIConfiguration
+      .fromSpec(spec)
+      .withResourceLoaders(List(loader))
+  }
+
   test("references to external yaml using './'") {
-    val mappings = Map(
-      "resource::8fe5354c-e64c-4eaa-addc-b50906a0b48c:pure-member:1.0.0:oas:zip:pure-member-portal.yaml" -> CustomContentResult(
-        "file://amf-cli/shared/src/test/resources/compiler/apikit-apisync/dot-slash-ref/pure-member-portal.yaml"
-      ),
-      "responses.yaml" -> CustomContentResult(
-        "file://amf-cli/shared/src/test/resources/compiler/apikit-apisync/dot-slash-ref/responses.yaml"
-      )
-    )
-    val url = "resource::8fe5354c-e64c-4eaa-addc-b50906a0b48c:pure-member:1.0.0:oas:zip:pure-member-portal.yaml"
-    val client = WebAPIConfiguration
-      .WebAPI()
-      .withResourceLoaders(List(new URNResourceLoader(mappings)))
-      .baseUnitClient()
-    client.parse(url).map { parseResult =>
+    val main = "resource::8fe5354c-e64c-4eaa-addc-b50906a0b48c:pure-member:1.0.0:oas:zip:pure-member-portal.yaml"
+    val loader = new UrnLoaderBuilder(s"$base/dot-slash-ref")
+      .addMapping(main, "pure-member-portal.yaml")
+      .addMapping("responses.yaml")
+      .build()
+    val client = getClient(loader, OAS30)
+    client.parse(main).map { parseResult =>
       parseResult.results should have size 0
     }
   }
 
-  case class CustomContentResult(actualPath: String)
-
-  class URNResourceLoader(mappings: Map[String, CustomContentResult]) extends ResourceLoader {
+  class URNResourceLoader(mappings: Map[String, String]) extends ResourceLoader {
 
     override def fetch(resource: String): Future[Content] = {
       mappings
         .get(resource)
         .map(url => {
           platform
-            .fetchContent(url.actualPath, AMFGraphConfiguration.predefined())
+            .fetchContent(url, AMFGraphConfiguration.predefined())
             .map(content => Content(content.stream, resource)) // content result url provided by resource loader
         })
         .getOrElse(throw FileNotFound(new RuntimeException(s"Couldn't find resource $resource")))
     }
 
     override def accepts(resource: String): Boolean = true
+  }
+
+  class UrnLoaderBuilder(
+      private val base: String,
+      private val internal: mutable.Map[String, String] = mutable.Map.empty[String, String]
+  ) {
+
+    def addMapping(urn: String, fs: String): this.type = {
+      internal.put(urn, s"$base/$fs")
+      this
+    }
+
+    def addMapping(path: String): this.type = addMapping(path, path)
+
+    def build() = new URNResourceLoader(internal.toMap)
   }
 }
