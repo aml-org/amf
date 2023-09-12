f6abf8f2dc4c51d5c4497a97ae617823f6fef4eb
Hernan Najles <hernan.naj+github@gmail.com>
2023-09-12 16:14:02 -0300

    W-14105863: when overriding properties if the ranges are both uniones, leave the base range as it is

--- amf-cli/shared/src/test/scala/amf/validation/ValidationTest.scala
+++ amf-cli/shared/src/test/scala/amf/validation/ValidationTest.scala
@@ -317,6 +317,22 @@ class ValidationTest extends AsyncFunSuite with PlatformSecrets {
     }
 
   }
+
+  test("Test override property with same union range than super") {
+    val source = productionPath + "simple-union-override/library.raml"
+
+    val config = RAMLConfiguration.RAML10()
+    for {
+      report <- config.baseUnitClient().parse(source)
+      transform <- Future.successful(config.baseUnitClient().transform(report.baseUnit, PipelineId.Editing))
+      validation <- config.baseUnitClient().validate(transform.baseUnit)
+    } yield {
+      val parseReport = AMFValidationReport.unknownProfile(report)
+      val transformReport = AMFValidationReport.unknownProfile(transform)
+      println(transformReport.toString)
+      assert(validation.merge(transformReport).merge(parseReport).conforms)
+    }
+  }
   private def parseAndValidate(url: String, config: => AMFGraphConfiguration): Future[AMFValidationReport] = {
     val client = config.baseUnitClient()
     for {
