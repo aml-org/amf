c011d5b38de1dd8e4022dbf0f907a7c1d9cad490
Tomas Fernandez <tfernandez@mulesoft.com>
2024-02-07 17:12:39 -0300

    W-14991650 (fix): fixed declaration keys setting in Async 2.0

--- amf-api-contract/shared/src/main/scala/amf/apicontract/internal/spec/async/parser/domain/declarations/Async24DeclarationParser.scala
+++ amf-api-contract/shared/src/main/scala/amf/apicontract/internal/spec/async/parser/domain/declarations/Async24DeclarationParser.scala
@@ -1,11 +1,14 @@
 package amf.apicontract.internal.spec.async.parser.domain.declarations
 
 import amf.apicontract.internal.spec.async.parser.context.AsyncWebApiContext
+import amf.core.client.scala.model.document.Document
 import org.yaml.model.YMap
 
 object Async24DeclarationParser extends AsyncDeclarationParser {
-  override def parseDeclarations(map: YMap, parent: String)(implicit ctx: AsyncWebApiContext): Unit = {
-    Async23DeclarationParser.parseDeclarations(map, parent)
+  override def parseDeclarations(map: YMap, parent: String, document: Document)(implicit
+      ctx: AsyncWebApiContext
+  ): Unit = {
+    Async23DeclarationParser.parseDeclarations(map, parent, document)
     // TODO: add stuff....
   }
 }
