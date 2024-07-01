bcbab278cb83db7a966d4cecb4c3521daf07cf75
arielmirra <amirra@salesforce.com>
2024-07-01 16:12:13 -0300

    W-15633176: fix record fields parsing

--- amf-api-contract/shared/src/main/scala/amf/apicontract/internal/spec/avro/parser/domain/AvroShapeParser.scala
+++ amf-api-contract/shared/src/main/scala/amf/apicontract/internal/spec/avro/parser/domain/AvroShapeParser.scala
@@ -7,16 +7,16 @@ import org.yaml.model.{YMap, YNode, YScalar, YType}
 class AvroShapeParser(map: YMap)(implicit ctx: AvroSchemaContext) extends AvroKeyExtractor {
   val typeValue: Option[YNode] = map.typeValue
 
-  def parse(): Option[AnyShape] = typeValue.flatMap(parseTypeEntry(_))
+  def parse(): Option[AnyShape] = typeValue.flatMap(parseTypeEntry)
 
-  def parseTypeEntry(value: YNode, isRecordField: Boolean = false): Option[AnyShape] = {
+  def parseTypeEntry(value: YNode): Option[AnyShape] = {
     val (maybeShape, avroType) = value.tagType match {
       case YType.Seq =>
         val union = parseUnion(value.as[Seq[YNode]])
         (Some(union), "union")
       case YType.Str =>
         val specificType = value.as[YScalar].text
-        (Some(parseType(specificType, isRecordField)), specificType)
+        (Some(parseType(specificType)), specificType)
       case _ =>
         // todo: should validate invalid type when using the AVRO Validator
         (None, "invalid avro type")
@@ -27,14 +27,14 @@ class AvroShapeParser(map: YMap)(implicit ctx: AvroSchemaContext) extends AvroKe
 
   private def parseUnion(members: Seq[YNode]): AnyShape = AvroUnionShapeParser(members, map).parse()
 
-  private def parseType(name: String, isRecordField: Boolean = false): AnyShape = {
+  private def parseType(name: String): AnyShape = {
     name match {
       case "map"                 => parseMap()
       case "array"               => parseArray()
       case "record"              => parseRecord()
       case "enum"                => parseEnum()
       case "fixed"               => parseFixed()
-      case _ if name.isPrimitive => parsePrimitiveType(name, isRecordField)
+      case _ if name.isPrimitive => parsePrimitiveType(name)
       // todo: should validate invalid type when using the AVRO Validator, maybe save raw json in an annotation.
       case _ => AnyShape() // ignore
     }
@@ -42,8 +42,8 @@ class AvroShapeParser(map: YMap)(implicit ctx: AvroSchemaContext) extends AvroKe
 
   private def parseRecord()         = new AvroRecordParser(map).parse()
   private def parseEnum(): AnyShape = new AvroEnumParser(map).parse()
-  private def parsePrimitiveType(`type`: String, isRecordField: Boolean = false): AnyShape =
-    AvroTextTypeParser(`type`, Some(map), isRecordField).parse()
+  private def parsePrimitiveType(`type`: String): AnyShape =
+    AvroTextTypeParser(`type`, Some(map)).parse()
   private def parseFixed(): AnyShape = AvroFixedShapeParser(map).parse()
   private def parseMap(): AnyShape   = AvroMapShapeParser(map).parse()
   private def parseArray(): AnyShape = AvroArrayShapeParser(map).parse()
