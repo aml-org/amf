007ec51d830ec9e7cb64842c0e2f13bac3628c79
arielmirra <amirra@salesforce.com>
2024-07-01 16:12:13 -0300

    W-15633176: fix record fields parsing

--- amf-api-contract/shared/src/main/scala/amf/apicontract/internal/spec/avro/parser/domain/AvroTextTypeParser.scala
+++ amf-api-contract/shared/src/main/scala/amf/apicontract/internal/spec/avro/parser/domain/AvroTextTypeParser.scala
@@ -7,7 +7,7 @@ import amf.shapes.client.scala.model.domain.{AnyShape, NilShape, NodeShape, Scal
 import org.yaml.model.YMap
 
 /** parses primitive avro types such as null, boolean, int, long, float, double, bytes, string */
-case class AvroTextTypeParser(`type`: String, maybeMap: Option[YMap], isRecordField: Boolean = false)(implicit
+case class AvroTextTypeParser(`type`: String, maybeMap: Option[YMap])(implicit
     ctx: AvroSchemaContext
 ) extends AvroComplexShapeParser(maybeMap.getOrElse(YMap.empty)) {
   private val avroPrimitiveTypes = Set("null", "boolean", "int", "long", "float", "double", "bytes", "string")
@@ -23,10 +23,6 @@ case class AvroTextTypeParser(`type`: String, maybeMap: Option[YMap], isRecordFi
   override def parse(): AnyShape = `type` match {
     case "null" => NilShape(annotations).withName(`type`)
     case s if avroPrimitiveTypes.contains(s) =>
-      if (isRecordField) {
-        parseCommonFields()
-        parseDefault()
-      }
       shape.withDataType(DataType(`type`), typeAnnotations)
     case _ if ctx.globalSpace.contains(`type`) =>
       val originalShape = ctx.globalSpace(`type`).asInstanceOf[NodeShape]
