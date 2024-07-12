52ca00fd0a21bf0f4fea365040d9e454e2ddd0f4
arielmirra <amirra@salesforce.com>
2024-07-12 16:49:59 -0300

    W-16006973: add avro annotations for ALS

--- amf-api-contract/shared/src/main/scala/amf/apicontract/internal/spec/avro/parser/domain/AvroMapShapeParser.scala
+++ amf-api-contract/shared/src/main/scala/amf/apicontract/internal/spec/avro/parser/domain/AvroMapShapeParser.scala
@@ -1,13 +1,16 @@
 package amf.apicontract.internal.spec.avro.parser.domain
 
 import amf.apicontract.internal.spec.avro.parser.context.AvroSchemaContext
+import amf.core.internal.parser.domain.Annotations
 import amf.shapes.client.scala.model.domain.{AnyShape, NodeShape}
+import amf.shapes.internal.domain.metamodel.NodeShapeModel.AdditionalPropertiesSchema
 import org.yaml.model.YMap
 
 case class AvroMapShapeParser(map: YMap)(implicit ctx: AvroSchemaContext)
     extends AvroCollectionShapeParser[NodeShape](map, "values") {
-  override val shape: NodeShape                     = NodeShape(map)
-  override def setMembers(anyShape: AnyShape): Unit = shape.withAdditionalPropertiesSchema(anyShape)
+  override val shape: NodeShape = NodeShape(map)
+  override def setMembers(anyShape: AnyShape): Unit =
+    shape.setWithoutId(AdditionalPropertiesSchema, anyShape, Annotations(map))
 
   override def parseSpecificFields(): Unit = {}
 }
