a1aaca00c906ef2ca4e8be9e79db4d543b85d158
arielmirra <amirra@salesforce.com>
2024-07-12 17:58:33 -0300

    fix: add avro-schema annotation on collections (items/values) parsing

--- amf-cli/shared/src/test/scala/amf/avro/AvroSchemaTCKTest.scala
+++ amf-cli/shared/src/test/scala/amf/avro/AvroSchemaTCKTest.scala
@@ -7,18 +7,21 @@ import amf.core.internal.remote.{AmfJsonHint, AvroHint}
 class AvroSchemaValidTCKTest extends AvroSchemaCycleTest {
   override def basePath: String = s"amf-cli/shared/src/test/resources/upanddown/cycle/avro/valid/"
 
-
   fs.syncFile(s"$basePath").list.foreach { schema =>
     if (schema.endsWith(".json") && !schema.endsWith(".dumped.json")) {
 
       test(s"Avro Schema TCK > Schemas > Valid > $schema: JSON to JSON-LD matches golden") {
         cycle(schema, schema.replace(".json", ".jsonld"), AvroHint, AmfJsonHint)
       }
-      //Todo: des-ingnore in emission
-     ignore(s"Avro Schema TCK > Schemas > Valid > $schema: JSON to dumped JSON matches golden") {
+      // Todo: des-ingnore in emission
+      ignore(s"Avro Schema TCK > Schemas > Valid > $schema: JSON to dumped JSON matches golden") {
         cycle(schema, schema.replace(".json", ".dumped.json"), AvroHint, AvroHint)
       }
     }
+  }
+
+  test("array with an schema in items field") {
+    cycle("array-with-items-schema.json", "array-with-items-schema.jsonld", AvroHint, AmfJsonHint)
   }
 
   override def renderOptions(): RenderOptions = RenderOptions().withoutFlattenedJsonLd.withPrettyPrint
