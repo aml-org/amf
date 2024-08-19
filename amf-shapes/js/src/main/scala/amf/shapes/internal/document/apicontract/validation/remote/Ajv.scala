51a9765e4b3eed84421b1d2f73ea8e830bda8496
arielmirra <amirra@salesforce.com>
2024-08-19 14:38:53 -0300

    W-16459888: initial validation testing

--- amf-shapes/js/src/main/scala/amf/shapes/internal/document/apicontract/validation/remote/Ajv.scala
+++ amf-shapes/js/src/main/scala/amf/shapes/internal/document/apicontract/validation/remote/Ajv.scala
@@ -80,3 +80,19 @@ protected[amf] object LazyAjv {
   lazy val default: Ajv = AjvValidator()
   lazy val fast: Ajv    = AjvValidator.fast()
 }
+
+@js.native
+@JSImport("avro-js", JSImport.Default)
+protected[amf] class Avro extends js.Object {
+  def parse(schema: String): AvroSchema = js.native
+//  def parse(schema: js.Object): AvroSchema = js.native
+}
+
+@js.native
+trait AvroSchema extends js.Object {
+  def isValid(data: js.Dynamic): Boolean = js.native
+}
+
+protected[amf] object LazyAvro {
+  lazy val default: Avro = new Avro()
+}
