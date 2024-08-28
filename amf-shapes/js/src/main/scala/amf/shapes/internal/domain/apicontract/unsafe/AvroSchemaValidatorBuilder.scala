93e94850a3dfcfb102d2debfba2392e1fe757ded
arielmirra <amirra@salesforce.com>
2024-08-28 18:18:38 -0300

    W-15633281: add JS AVRO Schema validation dummy code

--- amf-shapes/js/src/main/scala/amf/shapes/internal/domain/apicontract/unsafe/AvroSchemaValidatorBuilder.scala
+++ amf-shapes/js/src/main/scala/amf/shapes/internal/domain/apicontract/unsafe/AvroSchemaValidatorBuilder.scala
@@ -1,8 +1,10 @@
 package amf.shapes.internal.domain.apicontract.unsafe
 
-import amf.core.client.common.validation.ValidationMode
+import amf.core.client.common.validation.{StrictValidationMode, ValidationMode}
 import amf.core.client.scala.model.domain.Shape
+import amf.core.client.scala.validation.AMFValidationResult
 import amf.core.client.scala.validation.payload.ShapeValidationConfiguration
+import amf.core.internal.remote.Mimes
 import amf.shapes.internal.document.apicontract.validation.remote.JsAvroShapePayloadValidator
 import amf.shapes.internal.validation.avro.BaseAvroSchemaPayloadValidator
 
@@ -23,4 +25,13 @@ object AvroSchemaValidatorBuilder {
       configuration: ShapeValidationConfiguration
   ): BaseAvroSchemaPayloadValidator =
     new JsAvroShapePayloadValidator(shape, mediaType, validationMode, configuration)
+
+  def validateSchema(
+      schema: Shape,
+      mediaType: String = Mimes.`application/json`,
+      validationMode: ValidationMode = StrictValidationMode,
+      configuration: ShapeValidationConfiguration = ShapeValidationConfiguration.predefined()
+  ): Seq[AMFValidationResult] = {
+    new JsAvroShapePayloadValidator(schema, mediaType, validationMode, configuration).validateAvroSchema()
+  }
 }
