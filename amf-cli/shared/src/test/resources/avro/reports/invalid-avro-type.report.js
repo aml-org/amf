06c51b1ebc44369da9d3ae95fd9af68a698c3898
arielmirra <amirra@salesforce.com>
2024-08-28 18:18:38 -0300

    W-15633281: add JS AVRO Schema validation dummy code

---
+++ amf-cli/shared/src/test/resources/avro/reports/invalid-avro-type.report.js
@@ -0,0 +1,4 @@
+ModelId: file://amf-cli/shared/src/test/resources/avro/schemas/invalid-avro-type.json
+Profile:
+Conforms: true
+Number of results: 0
---
+++ amf-cli/shared/src/test/resources/avro/reports/record-missing-name.report.js
@@ -0,0 +1,4 @@
+ModelId: file://amf-cli/shared/src/test/resources/avro/schemas/record-missing-name.json
+Profile: 
+Conforms: true
+Number of results: 0
