9dd8afa031d4d1a912fc2de697a2ed965c966fb7
Loose <alejandro.loose@mulesoft.com>
2024-09-20 15:31:38 -0300

    W-16702592 - Uncomment some tests that was ignored for JS

--- amf-cli/shared/src/test/scala/amf/avro/AvroSchemaLinkerReportTest.scala
+++ amf-cli/shared/src/test/scala/amf/avro/AvroSchemaLinkerReportTest.scala
@@ -9,26 +9,23 @@ class AvroSchemaLinkerReportTest extends UniquePlatformReportGenTest with AvroSc
   override val basePath: String    = "file://amf-cli/shared/src/test/resources/avro/doc/"
   override val reportsPath: String = "amf-cli/shared/src/test/resources/avro/doc/reports/"
 
-  // TODO This should work for JS the same. Now it is resolving and inlining the reference
-  if (platform.name == "jvm") {
-    test("invalid - RAML including AVRO Schema Document") {
-      withAvroSchema("schemas/schema-1.9.0.json", RAML10()).flatMap { case (config, _) =>
-        validate(
-          "apis/invalid-avro-ref.raml",
-          Some("invalid-avro-ref.raml.report"),
-          configOverride = Some(config)
-        )
-      }
+  test("invalid - RAML including AVRO Schema Document") {
+    withAvroSchema("schemas/schema-1.9.0.json", RAML10()).flatMap { case (config, _) =>
+      validate(
+        "apis/invalid-avro-ref.raml",
+        Some("invalid-avro-ref.raml.report"),
+        configOverride = Some(config)
+      )
     }
+  }
 
-    test("invalid - ASYNC referencing AVRO Schema Document invalid inner path") {
-      withAvroSchema("schemas/schema-1.9.0.json", Async20()).flatMap { case (config, _) =>
-        validate(
-          "apis/invalid-async-ref.yaml",
-          Some("invalid-async-ref.yaml.report"),
-          configOverride = Some(config)
-        )
-      }
+  test("invalid - ASYNC referencing AVRO Schema Document invalid inner path") {
+    withAvroSchema("schemas/schema-1.9.0.json", Async20()).flatMap { case (config, _) =>
+      validate(
+        "apis/invalid-async-ref.yaml",
+        Some("invalid-async-ref.yaml.report"),
+        configOverride = Some(config)
+      )
     }
   }
 }
