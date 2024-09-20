77362b067eb395a792f92db034d0f9b94db19a5a
Loose <alejandro.loose@mulesoft.com>
2024-09-20 15:13:20 -0300

    W-16702592 - Fix schema parse validation. Remove excluded errors (we have single error, is not possible to skip)

--- amf-cli/shared/src/test/resources/avro/reports/map-wrong-default.report.js
+++ amf-cli/shared/src/test/resources/avro/reports/map-wrong-default.report.js
@@ -1,4 +1,14 @@
 ModelId: file://amf-cli/shared/src/test/resources/avro/schemas/map-wrong-default.json
-Profile: 
-Conforms: true
-Number of results: 0
+Profile: Avro
+Conforms: false
+Number of results: 1
+
+Level: Violation
+
+- Constraint: http://a.ml/vocabularies/amf/validation#example-validation-error
+  Message: 'wrong default' is not a valid value (of type '{"type":"map","values":"long"}') for ''
+  Severity: Violation
+  Target: file://amf-cli/shared/src/test/resources/avro/schemas/map-wrong-default.json#/shape/default-node/scalar_1
+  Property: file://amf-cli/shared/src/test/resources/avro/schemas/map-wrong-default.json#/shape/default-node/scalar_1
+  Range: [(4,13)-(4,28)]
+  Location: file://amf-cli/shared/src/test/resources/avro/schemas/map-wrong-default.json
