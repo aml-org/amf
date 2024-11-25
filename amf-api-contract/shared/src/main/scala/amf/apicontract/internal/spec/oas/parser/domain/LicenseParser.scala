5877f3212fd8a279c9424d5000e305e13a49c89a
Leopoldo Chalub <lchalub@salesforce.com>
2024-11-25 15:22:48 -0300

    W-10548497 - Adding Identifier Field to License and Validation only for Oas 3.1

--- amf-api-contract/shared/src/main/scala/amf/apicontract/internal/spec/oas/parser/domain/LicenseParser.scala
+++ amf-api-contract/shared/src/main/scala/amf/apicontract/internal/spec/oas/parser/domain/LicenseParser.scala
@@ -6,6 +6,7 @@ import amf.apicontract.internal.spec.common.parser.{SpecParserOps, WebApiContext
 import amf.apicontract.internal.spec.spec.toOas
 import amf.apicontract.internal.validation.definitions.ParserSideValidations.ExclusiveLicenseIdentifierError
 import amf.core.internal.parser.YMapOps
+import amf.core.internal.remote.Spec
 import amf.shapes.internal.spec.common.parser.AnnotationParser
 import org.yaml.model.{YMap, YNode}
 
@@ -24,15 +25,18 @@ class LicenseParser(node: YNode)(implicit ctx: WebApiContext) extends SpecParser
     val map = node.as[YMap]
     map.key("url", LicenseModel.Url in license)
     map.key("name", LicenseModel.Name in license)
-    map.key("identifier", LicenseModel.Identifier in license)
 
-    if (license.identifier.option().isDefined && license.url.option().isDefined) {
-      ctx.eh.violation(
-        ExclusiveLicenseIdentifierError,
-        license.id,
-        ExclusiveLicenseIdentifierError.message,
-        license.annotations
-      )
+    if (ctx.spec == Spec.OAS31) {
+      map.key("identifier", LicenseModel.Identifier in license)
+
+      if (license.identifier.option().isDefined && license.url.option().isDefined) {
+        ctx.eh.violation(
+          ExclusiveLicenseIdentifierError,
+          license.id,
+          ExclusiveLicenseIdentifierError.message,
+          license.annotations
+        )
+      }
     }
 
     AnnotationParser(license, map).parse()
