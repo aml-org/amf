098bff442ff3cd1bedbca648549d0d3bf719875c
looseale <alejandro.loose@mulesoft.com>
2025-06-25 20:00:56 -0300

    W-18540194 - Added basic resolution pipelines

--- amf-grpc/shared/src/main/scala/amf/grpc/client/scala/GRPCConfiguration.scala
+++ amf-grpc/shared/src/main/scala/amf/grpc/client/scala/GRPCConfiguration.scala
@@ -1,13 +1,25 @@
 package amf.grpc.client.scala
 
-import amf.antlr.internal.plugins.syntax.{AntlrSyntaxRenderPlugin, GrpcSyntaxParsePlugin, SyamlForJsonLDSyntaxParsePlugin}
+import amf.antlr.internal.plugins.syntax.{
+  AntlrSyntaxRenderPlugin,
+  GrpcSyntaxParsePlugin,
+  SyamlForJsonLDSyntaxParsePlugin
+}
 import amf.apicontract.client.scala.{AMFConfiguration, APIConfigurationBuilder}
 import amf.grpc.internal.plugins.parse.GrpcParsePlugin
 import amf.grpc.internal.plugins.render.GrpcRenderPlugin
+import amf.grpc.internal.transformation.{GrpcCachePipeline, GrpcEditingPipeline, GrpcTransformationPipeline}
 
 object GRPCConfiguration extends APIConfigurationBuilder {
   def GRPC(): AMFConfiguration =
     common()
       .withPlugins(List(GrpcParsePlugin, GrpcSyntaxParsePlugin, GrpcRenderPlugin, AntlrSyntaxRenderPlugin))
       .withPlugin(SyamlForJsonLDSyntaxParsePlugin) // override SYAML
+      .withTransformationPipelines(
+        List(
+          GrpcTransformationPipeline(),
+          GrpcEditingPipeline(),
+          GrpcCachePipeline()
+        )
+      )
 }
