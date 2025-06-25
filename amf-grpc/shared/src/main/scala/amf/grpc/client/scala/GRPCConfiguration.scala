d3a46ee4b4684bc0146cd284837657187f17f7ae
looseale <alejandro.loose@mulesoft.com>
2025-06-25 20:22:44 -0300

    W-18540246 - Added empty gRPC validation plugin and validations

--- amf-grpc/shared/src/main/scala/amf/grpc/client/scala/GRPCConfiguration.scala
+++ amf-grpc/shared/src/main/scala/amf/grpc/client/scala/GRPCConfiguration.scala
@@ -1,20 +1,27 @@
 package amf.grpc.client.scala
 
+import amf.antlr.client.scala.parse.AntlrBasedDocumentsFallbackPlugin
 import amf.antlr.internal.plugins.syntax.{
   AntlrSyntaxRenderPlugin,
   GrpcSyntaxParsePlugin,
   SyamlForJsonLDSyntaxParsePlugin
 }
 import amf.apicontract.client.scala.{AMFConfiguration, APIConfigurationBuilder}
+import amf.apicontract.internal.validation.model.ApiEffectiveValidations.GrpcEffectiveValidations
+import amf.apicontract.internal.validation.model.ApiValidationProfiles.GrpcValidationProfile
+import amf.core.client.common.validation.SeverityLevels
+import amf.core.internal.remote.Spec
 import amf.grpc.internal.plugins.parse.GrpcParsePlugin
 import amf.grpc.internal.plugins.render.GrpcRenderPlugin
+import amf.grpc.internal.plugins.validation.GrpcValidationPlugin
 import amf.grpc.internal.transformation.{GrpcCachePipeline, GrpcEditingPipeline, GrpcTransformationPipeline}
 
 object GRPCConfiguration extends APIConfigurationBuilder {
   def GRPC(): AMFConfiguration =
     common()
-      .withPlugins(List(GrpcParsePlugin, GrpcSyntaxParsePlugin, GrpcRenderPlugin, AntlrSyntaxRenderPlugin))
-      .withPlugin(SyamlForJsonLDSyntaxParsePlugin) // override SYAML
+      .withPlugins(
+        List(GrpcParsePlugin, GrpcSyntaxParsePlugin, GrpcRenderPlugin, AntlrSyntaxRenderPlugin, GrpcValidationPlugin())
+      )
       .withTransformationPipelines(
         List(
           GrpcTransformationPipeline(),
@@ -22,4 +29,7 @@ object GRPCConfiguration extends APIConfigurationBuilder {
           GrpcCachePipeline()
         )
       )
+      .withValidationProfile(GrpcValidationProfile, GrpcEffectiveValidations)
+      .withPlugin(SyamlForJsonLDSyntaxParsePlugin) // override SYAML
+      .withFallback(AntlrBasedDocumentsFallbackPlugin(false, false, SeverityLevels.WARNING, Spec.GRPC))
 }
