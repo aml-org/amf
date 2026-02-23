0702aba631e26b2b779deb6fc15bd4316561d66d
aloose_sfemu <aloose@salesforce.com>
2026-02-23 19:04:13 -0300

    W-21336243 - Update AgentGraph (AgentNetworkV2) schema and test instances

--- amf-agent-graph/shared/src/main/scala/amf/agentgraph/internal/plugins/parse/schema/AgentGraphSchema.scala
+++ amf-agent-graph/shared/src/main/scala/amf/agentgraph/internal/plugins/parse/schema/AgentGraphSchema.scala
@@ -1,22 +1,11 @@
 package amf.agentgraph.internal.plugins.parse.schema
 
 import amf.shapes.internal.plugins.parser.schema.JsonSchemaBasedSpecSchema
+import org.mulesoft.common.io.Fs
 
 object AgentGraphSchema extends JsonSchemaBasedSpecSchema {
 
   override def schema: String =
-    """
-      |{
-      |  "$schema": "http://json-schema.org/draft-07/schema#",
-      |  "type": "object",
-      |  "required": [ "agent-graph" ],
-      |  "properties": {
-      |    "agent-graph": {
-      |      "type": "string",
-      |      "pattern": "^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?$"
-      |    }
-      |  }
-      |}
-      |""".stripMargin
+    Fs.syncFile("amf-agent-graph/shared/src/main/resources/schema_agent_graph.json").read().toString
 
 }
