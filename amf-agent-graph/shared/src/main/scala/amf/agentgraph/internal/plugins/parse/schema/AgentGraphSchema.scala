b843571aa4b89fbe4b4b68fc7df94df61a3f0ac5
aloose_sfemu <aloose@salesforce.com>
2026-02-24 09:48:01 -0300

    W-21336243 - Use a SBT source and task to load AgentGraph schema content

--- amf-agent-graph/shared/src/main/scala/amf/agentgraph/internal/plugins/parse/schema/AgentGraphSchema.scala
+++ amf-agent-graph/shared/src/main/scala/amf/agentgraph/internal/plugins/parse/schema/AgentGraphSchema.scala
@@ -1,11 +1,10 @@
 package amf.agentgraph.internal.plugins.parse.schema
 
+import amf.agentgraph.internal.spec.AgentGraphSchemaContent
 import amf.shapes.internal.plugins.parser.schema.JsonSchemaBasedSpecSchema
-import org.mulesoft.common.io.Fs
 
 object AgentGraphSchema extends JsonSchemaBasedSpecSchema {
 
-  override def schema: String =
-    Fs.syncFile("amf-agent-graph/shared/src/main/resources/schema_agent_graph.json").read().toString
+  override def schema: String = AgentGraphSchemaContent.content
 
 }
